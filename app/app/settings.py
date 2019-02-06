30e3e93f3e092cd03a88d8e2b42aacfaba5f34a5
Edwin van de Ven <edwin@simplycode.nl>
2019-02-06 17:52:38 +0100

    Improve settings structure

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,9 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'costasiella.apps.CostasiellaConfig',
+
+    # 3rd party
     'graphene_django',
     'webpack_loader',
+    
+    # local apps
+    'costasiella.apps.CostasiellaConfig',
 ]
 
 MIDDLEWARE = [
