559ccc0cfc4904f4e2d2d9ca9fa6b5d0655625bc
Edwin van de Ven <edwin@simplycode.nl>
2019-02-06 18:15:49 +0100

    Don't return any data when user is not authenticated

--- app/costasiella/schema.py
+++ app/costasiella/schema.py
@@ -13,4 +13,11 @@ class Query(graphene.ObjectType):
     school_locations = graphene.List(SchoolLocationType)
 
     def resolve_school_locations(self, info, **kwargs):
-        return SchoolLocation.objects.all()
+        print('user authenticated:')
+        print(info.context.user.is_authenticated)
+        if not info.context.user.is_authenticated:
+            return SchoolLocation.objects.none()
+        else:
+            return SchoolLocation.objects.all()
+        ## return everything:
+        # return SchoolLocation.objects.all()
