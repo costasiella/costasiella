fddcbbe9c6226b6c41ce81c69cc5d46670093c59
Edwin van de Ven <edwin@simplycode.nl>
2019-02-08 17:38:52 +0100

    Add customizable templates for allauth

--- app/app/settings.py
+++ app/app/settings.py
@@ -84,7 +84,7 @@ ROOT_URLCONF = 'app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, "templates"), ],
+        'DIRS': [os.path.join(BASE_DIR, "templates"), os.path.join(BASE_DIR, "templates", "allauth")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
