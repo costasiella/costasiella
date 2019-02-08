9a05132880167071ffc2259dde56bba7e775826c
Edwin van de Ven <edwin@simplycode.nl>
2019-02-08 20:43:35 +0100

    Add permission and group to schema
    Query user for group permissions

--- app/users/schema.py
+++ app/users/schema.py
@@ -1,4 +1,6 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group, Permission
+from django.db.models import Q
 
 import graphene
 from graphene_django import DjangoObjectType
@@ -7,6 +9,16 @@ from graphene_django import DjangoObjectType
 class UserType(DjangoObjectType):
     class Meta:
         model = get_user_model()
+
+
+class GroupType(DjangoObjectType):
+    class Meta:
+        model = Group
+
+
+class PermissionType(DjangoObjectType):
+    class Meta:
+        model = Permission
 
 
 class CreateUser(graphene.Mutation):
@@ -31,9 +43,12 @@ class CreateUser(graphene.Mutation):
 class Mutation(graphene.ObjectType):
     create_user = CreateUser.Field()
 
+
 class Query(graphene.AbstractType):
     me = graphene.Field(UserType)
     users = graphene.List(UserType)
+    group = graphene.List(GroupType, search=graphene.String())
+    permission = graphene.List(PermissionType)
 
     def resolve_users(self, info):
         return get_user_model().objects.all()
@@ -44,3 +59,23 @@ class Query(graphene.AbstractType):
             raise Exception('Not logged in!')
 
         return user
+
+    def resolve_group(self, info, search=None):
+        if search:
+            filter = (
+                Q(name__icontains=search)
+            )
+            return Groups.objects.filter(filter)
+
+        return Group.objects.all()
+
+
+    def resolve_permission(self, info):
+        return Permission.objects.all()
+        # user = info.context.user
+        # if user.is_anonymous:
+        #     raise Exception('Not logged in!')
+        # if user:
+        #     return Permission.objects.filter(user=user)
+
+        
