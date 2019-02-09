d289e6f25f09820c438dd20275963419100be2ce
Edwin van de Ven <edwin@simplycode.nl>
2019-02-09 16:21:23 +0100

    Add bootstrap admin & styling for allauth login

--- app/app/settings.py
+++ app/app/settings.py
@@ -93,6 +93,9 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'libraries': {
+                'addcss': 'templatetags.addcss'
+            }
         },
     },
 ]
@@ -152,6 +155,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "assets", "bootstrapadmin"),
+]
 
 # Email configuration
 
