1b17ebcb874ef7ecab113f91f09683e7aa3f7c97
Edwin van de Ven <edwin@simplycode.nl>
2019-02-09 16:55:28 +0100

    Only show school locations when a user is logged in

--- app/costasiella/schema.py
+++ app/costasiella/schema.py
@@ -13,8 +13,11 @@ class Query(graphene.ObjectType):
     school_locations = graphene.List(SchoolLocationType)
 
     def resolve_school_locations(self, info, **kwargs):
+        user = info.context.user
         print('user authenticated:')
-        print(info.context.user.is_authenticated)
+        print(user.is_authenticated)
+        if user.is_anonymous:
+            raise Exception('Not logged in!')
         # if not info.context.user.is_authenticated:
             # return SchoolLocation.objects.none()
         # else:
