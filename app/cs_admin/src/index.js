9dffec6ba25d8e8e78e578a373a16dd320e4fd94
Edwin van de Ven <edwin@simplycode.nl>
2019-03-02 08:59:44 +0100

    Code cleanup

--- app/cs_admin/src/index.js
+++ app/cs_admin/src/index.js
@@ -6,39 +6,6 @@ import App from './App'
 import * as serviceWorker from './serviceWorker'
 
 
-import ApolloClient from "apollo-boost"
-import gql from "graphql-tag"
-
-const client = new ApolloClient({
-    uri: "http://localhost:8000/graphql/"
-  });
-
-// client
-// .query({
-// query: gql`
-//     {
-//         schoolLocations {
-//             name
-//         }
-//     }
-// `
-// })
-// .then(result => console.log(result));
-
-
-// client
-// .query({
-// mutation: gql`
-//     mutation TokenAuth($username: String!, $password: String!) {
-//         tokenAuth(username: $username, password: $password) {
-//         token
-//         }
-//     }
-// `
-// })
-// .then(result => console.log(result));
-
-
 ReactDOM.render(<App />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
