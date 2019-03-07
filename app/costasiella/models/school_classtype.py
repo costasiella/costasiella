ddac4b3a19f0c8d6e995a4e74b4503a5ca4d4890
Edwin van de Ven <edwin@simplycode.nl>
2019-03-07 20:57:24 +0100

    Add school classtype model

--- app/costasiella/models/school_classtype.py
+++ app/costasiella/models/school_classtype.py
@@ -4,11 +4,14 @@ from django.db import models
 
 # Create your models here.
 
-class SchoolLocation(models.Model):
+class SchoolClasstype(models.Model):
     # id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     archived = models.BooleanField(default=False)
     display_public = models.BooleanField(default=True)
     name = models.CharField(max_length=255)
+    description = models.TextField()
+    link = models.URLField()
+    image = models.ImageField(upload_to='uploads/')
     
     def __str__(self):
         return self.name
