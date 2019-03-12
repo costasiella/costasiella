b953de5d3390cf5cbd4be28aafcce189c2314f84
Edwin van de Ven <edwin@simplycode.nl>
2019-03-12 19:44:56 +0100

    Fix validation for classtype urlWebsite

--- app/cs_admin/src/components/school/classtypes/yupSchema.js
+++ app/cs_admin/src/components/school/classtypes/yupSchema.js
@@ -6,6 +6,6 @@ export const CLASSTYPE_SCHEMA = Yup.object().shape({
       .min(3, t('form_field_min_len_3'))
       .required(t('form_field_required')),
     description: Yup.string(),
-    link: Yup.string()
+    urlWebsite: Yup.string()
       .url(),
   })
