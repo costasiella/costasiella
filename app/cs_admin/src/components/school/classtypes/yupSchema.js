7abbb4828320fbeac48526c78517bbcf55b3fac5
Edwin van de Ven <edwin@simplycode.nl>
2019-03-12 21:21:49 +0100

    Update Yup messages

--- app/cs_admin/src/components/school/classtypes/yupSchema.js
+++ app/cs_admin/src/components/school/classtypes/yupSchema.js
@@ -3,9 +3,9 @@ import * as Yup from 'yup'
 
 export const CLASSTYPE_SCHEMA = Yup.object().shape({
     name: Yup.string()
-      .min(3, t('form_field_min_len_3'))
-      .required(t('form_field_required')),
+      .min(3, t('yup.min_len_3'))
+      .required(t('yup.field_required')),
     description: Yup.string(),
     urlWebsite: Yup.string()
-      .url(),
+      .url(t("yup.url")),
   })
