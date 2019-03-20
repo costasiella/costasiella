ee2d559ee4084917229508510597d48693b9a6b8
Edwin van de Ven <edwin@simplycode.nl>
2019-03-20 09:50:48 +0100

    Finance costcenter model

--- app/costasiella/models/__init__.py
+++ app/costasiella/models/__init__.py
@@ -1,4 +1,5 @@
 from .finance_glaccount import FinanceGLAccount
+from .finance_costcenter import FinanceCostCenter
 
 from .school_location import SchoolLocation
 from .school_classtype import SchoolClasstype
