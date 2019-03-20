9602f4f5050c62bf8daa7ea2b151696a7ed76975
Edwin van de Ven <edwin@simplycode.nl>
2019-03-20 10:24:30 +0100

    school discovery model

--- app/costasiella/models/__init__.py
+++ app/costasiella/models/__init__.py
@@ -1,6 +1,8 @@
 from .finance_glaccount import FinanceGLAccount
 from .finance_costcenter import FinanceCostCenter
 
-from .school_location import SchoolLocation
 from .school_classtype import SchoolClasstype
+from .school_discovery import SchoolDiscovery
+from .school_location import SchoolLocation
+
 
