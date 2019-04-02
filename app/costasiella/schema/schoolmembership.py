4e79f4f9d234d2ee3f5272b40f5a8900e83de0e2
Edwin van de Ven <edwin@simplycode.nl>
2019-04-02 17:04:57 +0900

    update school membership schema

--- app/costasiella/schema/schoolmembership.py
+++ app/costasiella/schema/schoolmembership.py
@@ -109,7 +109,7 @@ class CreateSchoolMembership(graphene.relay.ClientIDMutation):
         # Validate input
         result = validate_create_update_input(input, update=False)
 
-        school_membership = SchoolMembership(
+        membership = SchoolMembership(
             display_public=input['display_public'],
             display_shop=input['display_shop'],
             name=input['name'], 
@@ -122,23 +122,30 @@ class CreateSchoolMembership(graphene.relay.ClientIDMutation):
         )
 
         if 'finance_glaccount' in result:
-            school_membership.finance_glaccount = result['finance_glaccount']
+            membership.finance_glaccount = result['finance_glaccount']
 
         if 'finance_costcenter' in result:
-            school_membership.finance_costcenter = result['finance_costcenter']
+            membership.finance_costcenter = result['finance_costcenter']
 
-        school_membership.save()
+        membership.save()
 
-        return CreateSchoolMembership(school_membership = school_membership)
+        return CreateSchoolMembership(school_membership = membership)
 
 
 class UpdateSchoolMembership(graphene.relay.ClientIDMutation):
     class Input:
         id = graphene.ID(required=True)
+        display_public = graphene.Boolean(required=True, default_value=True)
+        display_shop = graphene.Boolean(required=True, default_value=True)
         name = graphene.String(required=True)
         description = graphene.String(required=False, default_value="")
-        display_public = graphene.Boolean(required=True, default_value=True)
-        url_website = graphene.String(required=False, default_value="")
+        price = graphene.Float(rquired=True, default_value=0)
+        finance_tax_rate = graphene.ID(required=True)
+        validity = graphene.Int(required=True, default_value=1)
+        validity_unit = graphene.String(required=True)
+        terms_and_conditions = graphene.String(required=False, default_value="")
+        finance_glaccount = graphene.ID(required=False, default_value="")
+        finance_costcenter = graphene.ID(required=False, default_value="")
 
     school_membership = graphene.Field(SchoolMembershipNode)
 
@@ -147,68 +154,31 @@ class UpdateSchoolMembership(graphene.relay.ClientIDMutation):
         user = info.context.user
         require_login_and_permission(user, 'costasiella.change_schoolmembership')
 
+    
         rid = get_rid(input['id'])
         membership = SchoolMembership.objects.filter(id=rid.id).first()
         if not membership:
             raise Exception('Invalid School Membership ID!')
 
-        url_website = input['url_website']
-        if url_website:
-            if not validators.url(url_website, public=True):
-                raise GraphQLError(_('Invalid URL, make sure it starts with "http"'))
-            else:
-                membership.url_website = url_website
+        result = validate_create_update_input(input, update=True)
 
-        membership.name = input['name']
-        membership.description = input['description']
-        membership.display_public = input['display_public']
+        membership.display_public=input['display_public'],
+        membership.display_shop=input['display_shop'],
+        membership.name=input['name'], 
+        membership.description=input['description'],
+        membership.price=input['price'],
+        membership.finance_tax_rate=result['finance_tax_rate'],
+        membership.validity=input['validity'],
+        membership.validity_unit=input['validity_unit'],
+        membership.terms_and_conditions=input['terms_and_conditions']
+
+        if 'finance_glaccount' in result:
+            membership.finance_glaccount = result['finance_glaccount']
+
+        if 'finance_costcenter' in result:
+            membership.finance_costcenter = result['finance_costcenter']
+
         membership.save(force_update=True)
-
-        return UpdateSchoolMembership(school_membership=membership)
-
-
-class UploadSchoolMembershipImage(graphene.relay.ClientIDMutation):
-    class Input:
-        id = graphene.ID(required=True)
-        image = graphene.String(required=True)
-
-
-    school_membership = graphene.Field(SchoolMembershipNode)
-
-    @classmethod
-    def mutate_and_get_payload(self, root, info, **input):
-        user = info.context.user
-        require_login_and_permission(user, 'costasiella.change_schoolmembership')
-
-        import base64
-        from django.core.files.base import ContentFile
-
-        def base64_file(data, name=None):
-            _format, _img_str = data.split(';base64,')
-            _name, ext = _format.split('/')
-            if not name:
-                name = _name.split(":")[-1]
-            return ContentFile(base64.b64decode(_img_str), name='{}.{}'.format(name, ext))
-
-        rid = get_rid(input['id'])
-        membership = SchoolMembership.objects.filter(id=rid.id).first()
-        if not membership:
-            raise Exception('Invalid School Membership ID!')
-
-        b64_enc_image = input['image']
-        # print(b64_enc_image)
-        (image_type, image_file) = b64_enc_image.split(',')
-        # print(image_type)
-
-        
-        membership.image = base64_file(data=b64_enc_image)
-        membership.save(force_update=True)
-
-        print('new image')
-        img = membership.image
-        print(img.name)
-        print(img.path)
-        print(img.url)
 
         return UpdateSchoolMembership(school_membership=membership)
 
@@ -239,4 +209,4 @@ class ArchiveSchoolMembership(graphene.relay.ClientIDMutation):
 class SchoolMembershipMutation(graphene.ObjectType):
     archive_school_membership = ArchiveSchoolMembership.Field()
     create_school_membership = CreateSchoolMembership.Field()
-    # update_school_membership = UpdateSchoolMembership.Field()
+    update_school_membership = UpdateSchoolMembership.Field()
