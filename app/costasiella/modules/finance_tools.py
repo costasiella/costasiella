f46264439b1ef342ab14a9c41d6b5603fca047ec
Edwin van de Ven <edwin@simplycode.nl>
2019-04-08 19:15:12 +0900

    Fixed tests for memberships

--- app/costasiella/modules/finance_tools.py
+++ app/costasiella/modules/finance_tools.py
@@ -3,4 +3,4 @@
 def display_float_as_amount(amount):
     from constance import config
 
-    return ' '.join([config.CURRENCY_SYMBOL, str(amount)])
+    return ' '.join([config.CURRENCY_SYMBOL, format(amount, '.2f')])
