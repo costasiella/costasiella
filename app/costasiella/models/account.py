9a6c2e72d9aa597d7875a00807728ef014db3131
Edwin van de Ven <edwin@simplycode.nl>
2019-04-16 20:03:13 +0900

    Update model

--- app/costasiella/models/account.py
+++ app/costasiella/models/account.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-class CustomUser(AbstractUser):
+class Account(AbstractUser):
     # add additional fields in here
 
     def __str__(self):
