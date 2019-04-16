c25eab37e2d198b0849d585896bd21d2f7b7d600
Edwin van de Ven <edwin@simplycode.nl>
2019-04-16 20:05:44 +0900

    admin & forms

--- app/costasiella/forms.py
+++ app/costasiella/forms.py
@@ -1,21 +1,22 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth import get_user_model
 from django.utils.translation import gettext as _
 
-from .models import CustomUser
+from .models import Account
 
 
-class CustomUserCreationForm(UserCreationForm):
+class AccountCreationForm(UserCreationForm):
 
     class Meta(UserCreationForm):
-        model = CustomUser
+        model = get_user_model()
         fields = ('email',)
 
 
-class CustomUserChangeForm(UserChangeForm):
+class AccountChangeForm(UserChangeForm):
 
     class Meta:
-        model = CustomUser
+        model = get_user_model()
         fields = ('email',)
 
 
