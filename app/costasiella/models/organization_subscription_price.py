e0e8e71d3fb9593f8a87ac98fb2afdc00aef63f2
Edwin van de Ven <edwin@simplycode.nl>
2019-04-23 21:04:38 +0900

    Subscription Price model

---
+++ app/costasiella/models/organization_subscription_price.py
@@ -0,0 +1,18 @@
+from django.utils.translation import gettext as _
+
+from django.db import models
+
+from .finance_taxrate import FinanceTaxRate
+from .organization_subscription import OrganizationSubscription
+
+
+class OrganizationSubscriptionPrice(models.Model):
+    organization_subscription = models.ForeignKey(OrganizationSubscription, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    finance_tax_rate = models.ForeignKey(FinanceTaxRate, on_delete=models.CASCADE)
+    date_start = models.DateField()
+    date_end = models.DateField()
+    
+    def __str__(self):
+        return self.organization_subscription.name + ' ' + self.date_start + ' - ' + self.price
+    
