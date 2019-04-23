e22b730aad2855b8bff599b7adfa5c4c219eca73
Edwin van de Ven <edwin@simplycode.nl>
2019-04-23 22:40:02 +0900

    Start on schema

--- app/costasiella/models/organization_subscription_price.py
+++ app/costasiella/models/organization_subscription_price.py
@@ -11,7 +11,7 @@ class OrganizationSubscriptionPrice(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     finance_tax_rate = models.ForeignKey(FinanceTaxRate, on_delete=models.CASCADE)
     date_start = models.DateField()
-    date_end = models.DateField()
+    date_end = models.DateField(null=True)
     
     def __str__(self):
         return self.organization_subscription.name + ' ' + self.date_start + ' - ' + self.price
