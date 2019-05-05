71e163c6c6ee8d75e49a548396527dcdfac139e3
Edwin van de Ven <edwin@simplycode.nl>
2019-05-05 22:33:19 +0900

    Add relations to main menu

--- app/cs_admin/src/components/SiteWrapper.js
+++ app/cs_admin/src/components/SiteWrapper.js
@@ -47,86 +47,6 @@ type navItem = {|
   +useExact?: boolean,
 |};
 
-const navBarItems: Array<navItem> = [
-  {
-    value: "Home",
-    to: "/",
-    icon: "home",
-    LinkComponent: withRouter(NavLink),
-    useExact: true,
-  },
-  // {
-  //   value: "Interface",
-  //   icon: "box",
-  //   subItems: [
-  //     {
-  //       value: "Cards Design",
-  //       to: "/cards",
-  //       LinkComponent: withRouter(NavLink),
-  //     },
-  //     { value: "Charts", to: "/charts", LinkComponent: withRouter(NavLink) },
-  //     {
-  //       value: "Pricing Cards",
-  //       to: "/pricing-cards",
-  //       LinkComponent: withRouter(NavLink),
-  //     },
-  //   ],
-  // },
-  // {
-  //   value: "Components",
-  //   icon: "calendar",
-  //   subItems: [
-  //     { value: "Maps", to: "/maps", LinkComponent: withRouter(NavLink) },
-  //     { value: "Icons", to: "/icons", LinkComponent: withRouter(NavLink) },
-  //     { value: "Store", to: "/store", LinkComponent: withRouter(NavLink) },
-  //     { value: "Blog", to: "/blog", LinkComponent: withRouter(NavLink) },
-  //   ],
-  // },
-  // {
-  //   value: "Pages",
-  //   icon: "file",
-  //   subItems: [
-  //     { value: "Profile", to: "/profile", LinkComponent: withRouter(NavLink) },
-  //     { value: "Login", to: "/login", LinkComponent: withRouter(NavLink) },
-  //     {
-  //       value: "Register",
-  //       to: "/register",
-  //       LinkComponent: withRouter(NavLink),
-  //     },
-  //     {
-  //       value: "Forgot password",
-  //       to: "/forgot-password",
-  //       LinkComponent: withRouter(NavLink),
-  //     },
-  //     { value: "400 error", to: "/400", LinkComponent: withRouter(NavLink) },
-  //     { value: "401 error", to: "/401", LinkComponent: withRouter(NavLink) },
-  //     { value: "403 error", to: "/403", LinkComponent: withRouter(NavLink) },
-  //     { value: "404 error", to: "/404", LinkComponent: withRouter(NavLink) },
-  //     { value: "500 error", to: "/500", LinkComponent: withRouter(NavLink) },
-  //     { value: "503 error", to: "/503", LinkComponent: withRouter(NavLink) },
-  //     { value: "Email", to: "/email", LinkComponent: withRouter(NavLink) },
-  //     {
-  //       value: "Empty page",
-  //       to: "/empty-page",
-  //       LinkComponent: withRouter(NavLink),
-  //     },
-  //     { value: "RTL", to: "/rtl", LinkComponent: withRouter(NavLink) },
-  //   ],
-  // },
-  {
-    value: "School",
-    to: "/organization",
-    icon: "book",
-    LinkComponent: withRouter(NavLink),
-  },
-  {
-    value: "Finance",
-    to: "/finance",
-    icon: "dollar-sign",
-    LinkComponent: withRouter(NavLink),
-  },
-];
-
 
 const check_permission = (permissions, permission, resource) => {
   let you_shall_not_pass = true
@@ -153,6 +73,17 @@ const getNavBarItems = (t, user) => {
     LinkComponent: withRouter(NavLink),
     useExact: true,
   })
+
+  if (
+    (check_permission(permissions, 'view', 'account'))
+  ){
+    items.push({
+      value: t("relations.title"),
+      to: "/relations",
+      icon: "users",
+      LinkComponent: withRouter(NavLink),
+    })
+  }
 
   if (
     (check_permission(permissions, 'view', 'financecostcenter')) ||
