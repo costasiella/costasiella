72acdf0665be4c8b20276e10dee0aaa734671b68
Edwin van de Ven <edwin@simplycode.nl>
2019-05-07 20:32:09 +0900

    Code cleanup

--- app/cs_admin/src/components/general/ContentCard.jsx
+++ app/cs_admin/src/components/general/ContentCard.jsx
@@ -72,34 +72,3 @@ ContentCard.defaultProps = {
 }
   
 export default withTranslation()(ContentCard)
-
-
-
-  // class SchoolLocatons extends Component {
-  //   componentDidMount() {
-  //     window.addEventListener("scroll", this.handleOnScroll);
-  //   }
-  
-  //   componentWillUnmount() {
-  //     window.removeEventListener("scroll", this.handleOnScroll);
-  //   }
-  
-  //   handleOnScroll = () => {
-  //     // http://stackoverflow.com/questions/9439725/javascript-how-to-detect-if-browser-window-is-scrolled-to-bottom
-  //     var scrollTop =
-  //       (document.documentElement && document.documentElement.scrollTop) ||
-  //       document.body.scrollTop;
-  //     var scrollHeight =
-  //       (document.documentElement && document.documentElement.scrollHeight) ||
-  //       document.body.scrollHeight;
-  //     var clientHeight =
-  //       document.documentElement.clientHeight || window.innerHeight;
-  //     var scrolledToBottom = Math.ceil(scrollTop + clientHeight) >= scrollHeight;
-  //     if (scrolledToBottom) {
-  //       this.props.onLoadMore()
-  //     }
-  //   }
-    
-  
-  // }
-  
