78fa2a2fc70b0f53e41d38f05731d48ef1009b7e
Edwin van de Ven <edwin@simplycode.nl>
2019-05-07 21:53:35 +0900

    Move accounts form to separate component

--- app/cs_admin/src/components/relations/accounts/RelationsAccountAdd.jsx
+++ app/cs_admin/src/components/relations/accounts/RelationsAccountAdd.jsx
@@ -25,6 +25,8 @@ import {
 import SiteWrapper from "../../SiteWrapper"
 import HasPermissionWrapper from "../../HasPermissionWrapper"
 
+import RelationsAccountForm from "./RelationsAccountForm"
+
 import RelationsMenu from '../RelationsMenu'
 
 
@@ -84,44 +86,11 @@ const RelationsAccountAdd = ({ t, history }) => (
                         }}
                         >
                         {({ isSubmitting, errors }) => (
-                            <FoForm>
-                                <Card.Body>
-                                    <Form.Group label={t('general.first_name')}>
-                                      <Field type="text" 
-                                              name="firstName" 
-                                              className={(errors.firstName) ? "form-control is-invalid" : "form-control"} 
-                                              autoComplete="off" />
-                                      <ErrorMessage name="firstName" component="span" className="invalid-feedback" />
-                                    </Form.Group>
-                                    <Form.Group label={t('general.last_name')}>
-                                      <Field type="text" 
-                                              name="lastName" 
-                                              className={(errors.lastName) ? "form-control is-invalid" : "form-control"} 
-                                              autoComplete="off" />
-                                      <ErrorMessage name="lastName" component="span" className="invalid-feedback" />
-                                    </Form.Group>
-                                    <Form.Group label={t('general.email')}>
-                                      <Field type="text" 
-                                              name="email" 
-                                              className={(errors.email) ? "form-control is-invalid" : "form-control"} 
-                                              autoComplete="off" />
-                                      <ErrorMessage name="email" component="span" className="invalid-feedback" />
-                                    </Form.Group>
-                                </Card.Body>
-                                <Card.Footer>
-                                    <Button 
-                                      color="primary"
-                                      className="pull-right" 
-                                      type="submit" 
-                                      disabled={isSubmitting}
-                                    >
-                                      {t('general.submit')}
-                                    </Button>
-                                    <Button color="link" onClick={() => history.push(return_url)}>
-                                        {t('general.cancel')}
-                                    </Button>
-                                </Card.Footer>
-                            </FoForm>
+                            <RelationsAccountForm
+                              isSubmitting={isSubmitting}
+                              errors={errors}
+                              return_url={return_url}
+                            />
                         )}
                     </Formik>
                 )}
