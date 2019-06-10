35c7cc86e34cfdefa4dd17f6b9cbc21394648f3f
Edwin van de Ven <edwin@simplycode.nl>
2019-06-10 20:09:14 +0200

    Fix fixtures names and queries

---
+++ app/cs_admin/src/components/finance/payment_methods/queries.js
@@ -0,0 +1,33 @@
+import gql from "graphql-tag"
+
+export const GET_PAYMENT_METHODS_QUERY = gql`
+  query FinancePaymentMethods($after: String, $before: String, $archived: Boolean) {
+    financePaymentMethods(first: 15, before: $before, after: $after, archived: $archived) {
+      pageInfo {
+        startCursor
+        endCursor
+        hasNextPage
+        hasPreviousPage
+      }
+      edges {
+        node {
+          id,
+          archived,
+          name,
+          code
+        }
+      }
+    }
+  }
+`
+
+export const GET_PAYMENT_METHOD_QUERY = gql`
+  query FinancePaymentMethod($id: ID!) {
+    financePaymentMethod(id:$id) {
+      id
+      name
+      code
+      archived
+    }
+  }
+`
