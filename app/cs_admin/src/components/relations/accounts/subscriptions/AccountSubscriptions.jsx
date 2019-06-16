6862d87278e535b2d170de20f3adb0388e69b9c9
Edwin van de Ven <edwin@simplycode.nl>
2019-06-16 16:31:54 +0200

    Code cleanup

--- app/cs_admin/src/components/relations/accounts/subscriptions/AccountSubscriptions.jsx
+++ app/cs_admin/src/components/relations/accounts/subscriptions/AccountSubscriptions.jsx
@@ -41,43 +41,6 @@ const DELETE_ACCOUNT_SUBSCRIPTION = gql`
 `
 
 
-// const confirm_delete = ({t, msgConfirm, msgDescription, msgSuccess, deleteFunction, functionVariables}) => {
-//   confirmAlert({
-//     customUI: ({ onClose }) => {
-//       return (
-//         <div className='custom-ui'>
-//           <h1>{t('general.confirm_delete')}</h1>
-//           {msgConfirm}
-//           {msgDescription}
-//           <button className="btn btn-link pull-right" onClick={onClose}>{t('general.confirm_delete_no')}</button>
-//           <button
-//             className="btn btn-danger"
-//             onClick={() => {
-//               deleteFunction(functionVariables)
-//                 .then(({ data }) => {
-//                   console.log('got data', data);
-//                   toast.success(
-//                     msgSuccess, {
-//                       position: toast.POSITION.BOTTOM_RIGHT
-//                     })
-//                 }).catch((error) => {
-//                   toast.error((t('general.toast_server_error')) + ': ' +  error, {
-//                       position: toast.POSITION.BOTTOM_RIGHT
-//                     })
-//                   console.log('there was an error sending the query', error);
-//                 })
-//               onClose()
-//             }}
-//           >
-//             <Icon name="trash-2" /> {t('general.confirm_delete_yes')}
-//           </button>
-//         </div>
-//       )
-//     }
-//   })
-// }
-
-
 const AccountSubscriptions = ({ t, history, match, archived=false }) => (
   <SiteWrapper>
     <div className="my-3 my-md-5">
