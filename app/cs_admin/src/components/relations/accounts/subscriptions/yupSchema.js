dbd75cca35a24b565d268b2d4a66f2214ecade39
Edwin van de Ven <edwin@simplycode.nl>
2019-06-17 11:11:07 +0200

    Yup Schema for add account subscription

--- app/cs_admin/src/components/relations/accounts/subscriptions/yupSchema.js
+++ app/cs_admin/src/components/relations/accounts/subscriptions/yupSchema.js
@@ -3,20 +3,12 @@ import * as Yup from 'yup'
 import { yupToFormErrors } from 'formik';
 
 export const SUBSCRIPTION_SCHEMA = Yup.object().shape({
-    // name: Yup.string()
-    //   .min(3, t('yup.min_len_3'))
-    //   .required(t('yup.field_required')),
-    // description: Yup.string(),
-    // sortOrder: Yup.number()
-    //   .min(0),
-    // minDuration: Yup.number()
-    //   .min(0),
-    // classes: Yup.number()
-    //   .min(0),
-    // subscriptionUnit: Yup.string(),
-    // creditValidity: Yup.number()
-    //   .min(0),
-    // quickStatsAmount: Yup.number()
-    //   .min(0),
-    // termsAndConditions: Yup.string()
-  })
+  organizationSubscription: Yup.string()
+    .required(t('yup.field_required')),
+  financePaymentMethod: Yup.string(),
+  dateStart: Yup.date()
+    .required(t('yup.field_required')),
+  dateEnd: Yup.date(),
+  registrationFeePaid: Yup.boolean(),
+  note: Yup.string(),
+})
