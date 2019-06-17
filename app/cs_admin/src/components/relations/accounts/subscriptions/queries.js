3f9870b123a4faeef59b38d40981c2a82efd7358
Edwin van de Ven <edwin@simplycode.nl>
2019-06-17 11:27:38 +0200

    Update AccountSubscription edit query

--- app/cs_admin/src/components/relations/accounts/subscriptions/queries.js
+++ app/cs_admin/src/components/relations/accounts/subscriptions/queries.js
@@ -40,39 +40,24 @@ export const GET_ACCOUNT_SUBSCRIPTIONS_QUERY = gql`
 `
 
 export const GET_SUBSCRIPTION_QUERY = gql`
-  query OrganizationSubscription($id: ID!, $after: String, $before: String, $archived: Boolean!) {
-    organizationSubscription(id:$id) {
+  query AccountSubscription($id: ID!, $accountId: ID!, $after: String, $before: String, $archived: Boolean!) {
+    accountSubscription(id:$id) {
       id
-      archived
-      displayPublic
-      displayShop
-      name
-      description
-      sortOrder
-      minDuration
-      classes
-      subscriptionUnit
-      subscriptionUnitDisplay
-      reconciliationClasses
-      creditValidity
-      unlimited
-      termsAndConditions
-      registrationFee
-      organizationMembership {
+      organizationSubscription {
         id
         name
       }
-      quickStatsAmount
-      financeGlaccount {
-        id 
-        name
-      }
-      financeCostcenter {
+      financePaymentMethod {
         id
         name
       }
+      dateStart
+      dateEnd
+      note
+      registrationFeePaid
+      createdAt
     }
-    organizationMemberships(first: 15, before: $before, after: $after, archived: $archived) {
+    organizationSubscriptions(first: 100, before: $before, after: $after, archived: $archived) {
       pageInfo {
         startCursor
         endCursor
@@ -87,7 +72,7 @@ export const GET_SUBSCRIPTION_QUERY = gql`
         }
       }
     }
-    financeGlaccounts(first: 15, before: $before, after: $after, archived: $archived) {
+    financePaymentMethods(first: 15, before: $before, after: $after, archived: $archived) {
       pageInfo {
         startCursor
         endCursor
@@ -103,22 +88,16 @@ export const GET_SUBSCRIPTION_QUERY = gql`
         }
       }
     }
-    financeCostcenters(first: 15, before: $before, after: $after, archived: $archived) {
-      pageInfo {
-        startCursor
-        endCursor
-        hasNextPage
-        hasPreviousPage
-      }
-      edges {
-        node {
-          id
-          archived
-          name
-          code
-        }
-      }
+    account(id:$accountId) {
+      id
+      firstName
+      lastName
+      email
+      phone
+      mobile
+      isActive
     }
+  }
   }
 `
 
