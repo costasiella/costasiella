9662d3cadbc5b88b18df84f48c00f6202bcc61ac
Edwin van de Ven <edwin@simplycode.nl>
2019-06-22 19:10:24 +0200

    Add code to get_query_vars to add orderBy, if set

--- app/cs_admin/src/components/schedule/classes/tools.js
+++ app/cs_admin/src/components/schedule/classes/tools.js
@@ -1,10 +1,18 @@
 import CSLS from "../../../tools/cs_local_storage"
 
 export function get_list_query_variables() {
-    return {
-      dateFrom: localStorage.getItem(CSLS.SCHEDULE_CLASSES_DATE_FROM), 
-      dateUntil: localStorage.getItem(CSLS.SCHEDULE_CLASSES_DATE_UNTIL),
-    //   orderBy: "lo"
-    }
+  let orderBy
+
+  let queryVars = {
+    dateFrom: localStorage.getItem(CSLS.SCHEDULE_CLASSES_DATE_FROM), 
+    dateUntil: localStorage.getItem(CSLS.SCHEDULE_CLASSES_DATE_UNTIL)
   }
+
+  orderBy = localStorage.getItem(CSLS.SCHEDULE_CLASSES_ORDER_BY)
+  if (orderBy) {
+    queryVars.orderBy = orderBy
+  }
+
+  return queryVars
+}
 
