925d85f30ab4c4b2eaf6200a837b96338576b1ac
Edwin van de Ven <edwin@simplycode.nl>
2019-06-28 16:35:36 +0200

    Add migrations

--- app/costasiella/models/schedule_item_teachers.py
+++ app/costasiella/models/schedule_item_teachers.py
@@ -9,25 +9,25 @@ from .account import Account
 # Create your models here.
 
 class ScheduleItemTeachers(models.Model):
-    class Meta:
-        permissions = [
-            ('view_scheduleitemteachers', _("Can view schedule class teachers")),
-            ('add_scheduleitemteachers', _("Can add schedule class teachers")),
-            ('change_scheduleitemteachers', _("Can change schedule class teachers")),
-            ('delete_scheduleitemteachers', _("Can delete schedule class teachers")),
-        ]
+    # class Meta:
+    #     permissions = [
+    #         ('view_scheduleitemteachers', _("Can view schedule class teachers")),
+    #         ('add_scheduleitemteachers', _("Can add schedule class teachers")),
+    #         ('change_scheduleitemteachers', _("Can change schedule class teachers")),
+    #         ('delete_scheduleitemteachers', _("Can delete schedule class teachers")),
+    #     ]
 
     TEACHER_ROLES = [
-        [NORMAL, _("Normal")],
-        [SUB, _("Subteacher")],
-        [ASSISTANT, _("Assistant")],
-        [KARMA, _("Karma")]
+        ['NORMAL', _("Normal")],
+        ['SUB', _("Subteacher")],
+        ['ASSISTANT', _("Assistant")],
+        ['KARMA', _("Karma")]
     ]
 
     schedule_item = models.ForeignKey(ScheduleItem, on_delete=models.CASCADE)
     account = models.ForeignKey(Account, on_delete=models.CASCADE)
     role = models.CharField(max_length=50, choices=TEACHER_ROLES)
-    account_2 = models.ForeignKey(Account, on_delete=models.CASCADE, null=True)
+    account_2 = models.ForeignKey(Account, on_delete=models.CASCADE, null=True, related_name="account_2")
     role_2 = models.CharField(default="", max_length=50, choices=TEACHER_ROLES)
     date_start = models.DateField()
     date_end = models.DateField(default=None, null=True)
@@ -35,5 +35,5 @@ class ScheduleItemTeachers(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.schedule_item + ' [' + self.account.full_name + " - " + str(date_start)']'
+        return self.schedule_item + ' [' + self.account.full_name + " - " + str(date_start) + ']'
     
