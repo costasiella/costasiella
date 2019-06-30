95e4781522e06d08ef621f6c5489f963fb38bdde
Edwin van de Ven <edwin@simplycode.nl>
2019-06-30 12:45:12 +0200

    Add teachers menu link

--- app/cs_admin/src/components/schedule/classes/all/ClassEditMenu.jsx
+++ app/cs_admin/src/components/schedule/classes/all/ClassEditMenu.jsx
@@ -11,10 +11,12 @@ import {
 // import HasPermissionWrapper from "../HasPermissionWrapper"
 
 let edit_active
+let teachers_active
 
 const ClassEditMenu = ({ t, active_link, classId }) => (
     <List.Group transparent={true}>
         {(active_link === 'edit') ? edit_active = true: edit_active = false}
+        {(active_link === 'teachers') ? teachers_active = true: teachers_active = false}
         
 
         <List.GroupItem
@@ -25,6 +27,15 @@ const ClassEditMenu = ({ t, active_link, classId }) => (
             active={edit_active}
             >
             {t('general.edit')}
+        </List.GroupItem>
+        <List.GroupItem
+            key={v4()}
+            className="d-flex align-items-center"
+            to={"#/schedule/classes/all/teachers/" + classId}
+            icon="users"
+            active={teachers_active}
+            >
+            {t('general.teachers')}
         </List.GroupItem>
     </List.Group>
 );
