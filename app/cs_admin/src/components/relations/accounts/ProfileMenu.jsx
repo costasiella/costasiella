cde4ad2292e9ac760f087f21b2b44604d8618eb7
Edwin van de Ven <edwin@simplycode.nl>
2019-07-08 20:12:01 +0200

    Add classpasses to profile menu

--- app/cs_admin/src/components/relations/accounts/ProfileMenu.jsx
+++ app/cs_admin/src/components/relations/accounts/ProfileMenu.jsx
@@ -12,12 +12,14 @@ import HasPermissionWrapper from "../../HasPermissionWrapper"
 
 let profile_active
 let subscriptions_active
+let classpasses_active
 
 
 const ProfileMenu = ({ t, account_id, active_link }) => (
     <List.Group transparent={true}>
         {(active_link === 'profile') ? profile_active = true: profile_active = false}
         {(active_link === 'subscriptions') ? subscriptions_active = true: subscriptions_active = false}
+        {(active_link === 'classpasses') ? classpasses_active = true: classpasses_active = false}
         
 
         <List.GroupItem
@@ -40,6 +42,19 @@ const ProfileMenu = ({ t, account_id, active_link }) => (
                 active={subscriptions_active}
                 >
             {t('relations.account.subscriptions.title')}
+            </List.GroupItem>
+        </HasPermissionWrapper>
+        <HasPermissionWrapper 
+            permission="view"
+            resource="accountclasspass">
+            <List.GroupItem
+                key={v4()}
+                className="d-flex align-items-center"
+                to={"#/relations/accounts/" + account_id + "/classpasses"}
+                icon="credit-card"
+                active={classpasses_active}
+                >
+            {t('relations.account.classpasses.title')}
             </List.GroupItem>
         </HasPermissionWrapper>
     </List.Group>
