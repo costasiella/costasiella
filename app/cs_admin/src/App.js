85fabc76d893ec07df16c31b3356a49a45461bf3
Edwin van de Ven <edwin@simplycode.nl>
2019-07-09 11:18:27 +0200

    Classpass Edit component route

--- app/cs_admin/src/App.js
+++ app/cs_admin/src/App.js
@@ -76,6 +76,7 @@ import AccountSubscriptionAdd from './components/relations/accounts/subscription
 import AccountSubscriptionEdit from './components/relations/accounts/subscriptions/AccountSubscriptionEdit'
 import AccountClasspasses from './components/relations/accounts/classpasses/AccountClasspasses'
 import AccountClasspassAdd from './components/relations/accounts/classpasses/AccountClasspassAdd'
+import AccountClasspassEdit from './components/relations/accounts/classpasses/AccountClasspassEdit'
 
 import ScheduleHome from './components/schedule/home/ScheduleHome'
 import ScheduleClasses from './components/schedule/classes/ScheduleClasses'
@@ -194,6 +195,7 @@ class App extends Component {
             <Route exact path="/relations/accounts/:account_id/subscriptions/edit/:id" component={AccountSubscriptionEdit} />
             <Route exact path="/relations/accounts/:account_id/classpasses" component={AccountClasspasses} />
             <Route exact path="/relations/accounts/:account_id/classpasses/add" component={AccountClasspassAdd} />
+            <Route exact path="/relations/accounts/:account_id/classpasses/edit/:id" component={AccountClasspassEdit} />
             <Route exact path="/schedule" component={ScheduleHome} />
             <Route exact path="/schedule/classes" component={ScheduleClasses} />
             <Route exact path="/schedule/classes/add/" component={ScheduleClassAdd} />
