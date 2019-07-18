373cb06e2b0d1d0374c7ad0c4e43f599a9b38e36
Edwin van de Ven <edwin@simplycode.nl>
2019-07-18 17:58:10 +0200

    Fix org appointment model

--- app/costasiella/models/organization_appointment.py
+++ app/costasiella/models/organization_appointment.py
@@ -1,16 +1,14 @@
-from django.utils.translation import gettext as _
-
 from django.db import models
-from sorl.thumbnail import ImageField
+from .organization_appointment_category import OrganizationAppointmentCategory
+
+# Create your models here.
 
 class OrganizationAppointment(models.Model):
+    organization_appointment_category = models.ForeignKey(OrganizationAppointmentCategory, on_delete=models.CASCADE)
     archived = models.BooleanField(default=False)
     display_public = models.BooleanField(default=True)
     name = models.CharField(max_length=255)
-    description = models.TextField()
-    url_website = models.URLField()
-    image = ImageField(upload_to='organization_appointment_images', default=None)
 
     def __str__(self):
-        return self.name
+        return self.organization_appointment_category.name + ' ' + 'Appointment' + ': ' + self.name
     
