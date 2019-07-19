23724656030b5aff029bd9306f5e33a2970f9f18
Edwin van de Ven <edwin@simplycode.nl>
2019-07-19 18:54:39 +0200

    Add account to query

--- app/cs_admin/src/components/relations/accounts/teacher_profile/queries.js
+++ app/cs_admin/src/components/relations/accounts/teacher_profile/queries.js
@@ -2,7 +2,7 @@ import gql from "graphql-tag"
 
 
 export const GET_ACCOUNT_TEACHER_PROFILE_QUERY = gql`
-  query AccountTeacherQuery($id: ID!) {
+  query AccountTeacherQuery($id: ID!, accountId: ID!) {
     accountTeacherProfile(id:$id) {
       id
       classes
@@ -12,6 +12,15 @@ export const GET_ACCOUNT_TEACHER_PROFILE_QUERY = gql`
       bio
       urlBio
       urlWebsite
+    }
+    account(id:$accountId) {
+      id
+      firstName
+      lastName
+      email
+      phone
+      mobile
+      isActive
     }
   }
 `
