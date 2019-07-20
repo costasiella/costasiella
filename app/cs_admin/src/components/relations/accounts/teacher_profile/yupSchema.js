ad6f1bb231471a177c3aa3e1227fc44942bd386a
Edwin van de Ven <edwin@simplycode.nl>
2019-07-20 14:12:15 +0200

    Yup for teacher profile form

--- app/cs_admin/src/components/relations/accounts/teacher_profile/yupSchema.js
+++ app/cs_admin/src/components/relations/accounts/teacher_profile/yupSchema.js
@@ -2,13 +2,14 @@ import { t } from 'i18next'
 import * as Yup from 'yup'
 
 export const ACCOUNT_TEACHER_PROFILE_SCHEMA = Yup.object().shape({
-    // firstName: Yup.string()
-    //   .min(2, t('yup.min_len_2'))
-    //   .required(t('yup.field_required')),
-    // lastName: Yup.string()
-    //   .min(2, t('yup.min_len_2'))
-    //   .required(t('yup.field_required')),
-    // email: Yup.string()
-    //   .email(t('yup.email'))
-    //   .required(t('yup.field_required')),
+  classes: Yup.boolean(),  
+  appointments: Yup.boolean(),  
+  events: Yup.boolean(),  
+  role: Yup.string(),
+  education: Yup.string(),
+  bio: Yup.string(),
+  urlBio: Yup.string()
+    .url(t('yup.url')),
+  urlWebsite: Yup.string()
+    .url(t('yup.url')),
   })
