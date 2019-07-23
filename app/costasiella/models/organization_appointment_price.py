02be7e7c1808a87712eb996522015db502fa1ef6
Edwin van de Ven <edwin@simplycode.nl>
2019-07-23 19:03:26 +0200

    Add unique constraint to model for appointment price
    unique on account & appointment

--- app/costasiella/models/organization_appointment_price.py
+++ app/costasiella/models/organization_appointment_price.py
@@ -10,12 +10,13 @@ from .organization_appointment import OrganizationAppointment
 
 
 class OrganizationAppointmentPrice(models.Model):
+    class Meta:
+        unique_together = ('account', 'organization_appointment',)
+
     account = models.ForeignKey(Account, on_delete=models.CASCADE)
     organization_appointment = models.ForeignKey(OrganizationAppointment, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=20, decimal_places=2)
     finance_tax_rate = models.ForeignKey(FinanceTaxRate, on_delete=models.CASCADE)
-    finance_glaccount = models.ForeignKey(FinanceGLAccount, on_delete=models.CASCADE, null=True)
-    finance_costcenter = models.ForeignKey(FinanceCostCenter, on_delete=models.CASCADE, null=True)
     
     def __str__(self):
         return self.organization_appointment.name + ' ' + self.account.full_name + ' - ' + self.price
