cf221628bfcc4c2bf842b58a540db41a18b39e65
Edwin van de Ven <edwin@simplycode.nl>
2019-07-29 12:30:44 +0200

    Save default invoice groups

--- app/cs_admin/src/components/finance/invoices/groups/defaults/FinanceInvoiceGroupDefaultForm.jsx
+++ app/cs_admin/src/components/finance/invoices/groups/defaults/FinanceInvoiceGroupDefaultForm.jsx
@@ -7,6 +7,7 @@ import { Form as FoForm, Field, ErrorMessage } from 'formik'
 import { v4 } from "uuid"
 
 import {
+  Dimmer,
   Button,
   Card,
   Form,
@@ -16,23 +17,25 @@ import {
 
 const FinanceInvoiceGroupDefaultForm = ({ t, history, inputData, isSubmitting, setFieldTouched, setFieldValue, errors, values, submitForm }) => (
   <FoForm>
-    <Form.Group label={t('finance.invoice_groups_defaults.default_group')}>
-      <Field component="select" 
-            name="financeInvoiceGroup" 
-            className={(errors.financeInvoiceGroup) ? "form-control is-invalid" : "form-control"} 
-            // value={values.financeInvoiceGroup}
-            onChange={() => {
-              // setFieldValue('financeInvoiceGroup', values.financeInvoiceGroup)
-              // setFieldTouched('financeInvoiceGroup', true)
-              setTimeout(() => {submitForm()}, 200)
-            }}
-            autoComplete="off">
-        {inputData.financeInvoiceGroups.edges.map(({ node }) =>
-          <option value={node.id} key={v4()}>{node.name}</option>
-        )}
-      </Field>
-      <ErrorMessage name="financeInvoiceGroup" component="span" className="invalid-feedback" />
-    </Form.Group>  
+    <Dimmer active={isSubmitting} loader={isSubmitting} >
+      <Form.Group label={t('finance.invoice_groups_defaults.default_group')}>
+        <Field component="select" 
+              name="financeInvoiceGroup" 
+              className={(errors.financeInvoiceGroup) ? "form-control is-invalid" : "form-control"} 
+              onChange={(e) => {
+                console.log(e.target.value)
+                setFieldValue('financeInvoiceGroup', e.target.value)
+                setFieldTouched('financeInvoiceGroup', true)
+                setTimeout(() => {submitForm()}, 200)
+              }}
+              autoComplete="off">
+          {inputData.financeInvoiceGroups.edges.map(({ node }) =>
+            <option value={node.id} key={v4()}>{node.name}</option>
+          )}
+        </Field>
+        <ErrorMessage name="financeInvoiceGroup" component="span" className="invalid-feedback" />
+      </Form.Group>  
+    </Dimmer>
   </FoForm>
 )
 
