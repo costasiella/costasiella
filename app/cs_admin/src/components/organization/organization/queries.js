df9ea663b0055be16d177f239b274eff9b20ccd9
Edwin van de Ven <edwin@simplycode.nl>
2019-08-05 11:20:42 +0200

    Organization query

---
+++ app/cs_admin/src/components/organization/organization/queries.js
@@ -0,0 +1,16 @@
+import gql from "graphql-tag"
+
+
+export const GET_ORGANIZATION_QUERY = gql`
+  query Organization($id: ID!) {
+    organization(id:$id) {
+      id
+      name
+      address
+      phone
+      email
+      registration
+      taxRegistration
+    }
+  }
+`
