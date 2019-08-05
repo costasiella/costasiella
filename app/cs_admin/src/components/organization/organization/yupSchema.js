cb41b1b6377d2a835f7a0bfe69c422c7346e55dd
Edwin van de Ven <edwin@simplycode.nl>
2019-08-05 11:39:45 +0200

    Add organization Yup schema

--- app/cs_admin/src/components/organization/organization/yupSchema.js
+++ app/cs_admin/src/components/organization/organization/yupSchema.js
@@ -5,4 +5,10 @@ export const ORGANIZATION_SCHEMA = Yup.object().shape({
     name: Yup.string()
       .min(3, t('yup.min_len_3'))
       .required(t('yup.field_required')),
+    address: Yup.string(),
+    phone: Yup.string(),
+    email: Yup.string()
+      .email(t('yup.email')),
+    registration: Yup.string(),
+    taxRegistration: Yup.string(),
   })
