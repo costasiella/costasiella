c07ea9a3d897bbe6256e66653ab9fd6832ebf478
Edwin van de Ven <edwin@simplycode.nl>
2019-08-14 17:57:36 +0200

    Update appointment schedule query vars

--- app/cs_admin/src/components/schedule/appointments/tools.js
+++ app/cs_admin/src/components/schedule/appointments/tools.js
@@ -2,35 +2,27 @@ import CSLS from "../../../tools/cs_local_storage"
 
 export function get_list_query_variables() {
   let orderBy
-  let organizationClasstype
-  let organizationLevel
+  let organizationAppointment
   let organizationLocation
 
   let queryVars = {
-    dateFrom: localStorage.getItem(CSLS.SCHEDULE_CLASSES_DATE_FROM), 
-    dateUntil: localStorage.getItem(CSLS.SCHEDULE_CLASSES_DATE_UNTIL)
+    dateFrom: localStorage.getItem(CSLS.SCHEDULE_APPOINTMENTS_DATE_FROM), 
+    dateUntil: localStorage.getItem(CSLS.SCHEDULE_APPOINTMENTS_DATE_UNTIL)
   }
 
-  orderBy = localStorage.getItem(CSLS.SCHEDULE_CLASSES_ORDER_BY)
+  orderBy = localStorage.getItem(CSLS.SCHEDULE_APPOINTMENTS_ORDER_BY)
   if (orderBy) {
     queryVars.orderBy = orderBy
   }
 
-  organizationClasstype = localStorage.getItem(CSLS.SCHEDULE_CLASSES_FILTER_CLASSTYPE)
-  if (organizationClasstype) {
-    queryVars.organizationClasstype = organizationClasstype
+  organizationAppointment = localStorage.getItem(CSLS.SCHEDULE_APPOINTMENTS_FILTER_CLASSTYPE)
+  if (organizationAppointment) {
+    queryVars.organizationAppointment = organizationAppointment
   } else {
-    queryVars.organizationClasstype = ""
+    queryVars.organizationAppointment = ""
   }
 
-  organizationLevel = localStorage.getItem(CSLS.SCHEDULE_CLASSES_FILTER_LEVEL)
-  if (organizationLevel) {
-    queryVars.organizationLevel = organizationLevel
-  } else {
-    queryVars.organizationLevel = ""
-  }
-
-  organizationLocation = localStorage.getItem(CSLS.SCHEDULE_CLASSES_FILTER_LOCATION)
+  organizationLocation = localStorage.getItem(CSLS.SCHEDULE_APPOINTMENTS_FILTER_LOCATION)
   if (organizationLocation) {
     queryVars.organizationLocation = organizationLocation
   } else {
