cc88623a2afeb220671d08cca75a7ec82c9651ad
Edwin van de Ven <edwin@simplycode.nl>
2019-08-15 12:55:41 +0200

    get appointment query

--- app/cs_admin/src/components/schedule/appointments/queries.js
+++ app/cs_admin/src/components/schedule/appointments/queries.js
@@ -92,7 +92,7 @@ export const GET_APPOINTMENTS_QUERY = gql`
   }
 `
 
-export const GET_CLASS_QUERY = gql`
+export const GET_APPOINTMENT_QUERY = gql`
   query ScheduleItem($id: ID!, $before: String, $after: String, $archived: Boolean!) {
     scheduleItem(id:$id) {
       id
@@ -105,14 +105,6 @@ export const GET_CLASS_QUERY = gql`
           id
           name
         }
-      }
-      organizationAppointment {
-        id
-        name
-      }
-      organizationLevel {
-        id
-        name
       }
       dateStart
       dateEnd
@@ -136,51 +128,6 @@ export const GET_CLASS_QUERY = gql`
             id
             name
           }
-        }
-      }
-    }
-    organizationAppointments(first: 100, before: $before, after: $after, archived: $archived) {
-      pageInfo {
-        startCursor
-        endCursor
-        hasNextPage
-        hasPreviousPage
-      }
-      edges {
-        node {
-          id
-          archived
-          name
-        }
-      }
-    }
-    organizationLevels(first: 100, before: $before, after: $after, archived: $archived) {
-      pageInfo {
-        startCursor
-        endCursor
-        hasNextPage
-        hasPreviousPage
-      }
-      edges {
-        node {
-          id
-          archived
-          name
-        }
-      }
-    }
-    financePaymentMethods(first: 100, before: $before, after: $after, archived: $archived) {
-      pageInfo {
-        startCursor
-        endCursor
-        hasNextPage
-        hasPreviousPage
-      }
-      edges {
-        node {
-          id
-          archived
-          name
         }
       }
     }
