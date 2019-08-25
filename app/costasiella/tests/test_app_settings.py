832850d74c01186e08de82ed49ce67865ba56723
Edwin van de Ven <edwin@simplycode.nl>
2019-08-26 01:09:46 +0200

    tests for appSettings

--- app/costasiella/tests/test_app_settings.py
+++ app/costasiella/tests/test_app_settings.py
@@ -27,22 +27,22 @@ class GQLAppSettings(TestCase):
         self.admin_user = f.AdminUserFactory.create()
         self.anon_user = AnonymousUser()
 
-        self.permission_view = 'view_financeappsettings'
-        self.permission_add = 'add_financeappsettings'
-        self.permission_change = 'change_financeappsettings'
-        self.permission_delete = 'delete_financeappsettings'
+        self.permission_view = 'view_appsettings'
+        self.permission_add = 'add_appsettings'
+        self.permission_change = 'change_appsettings'
+        self.permission_delete = 'delete_appsettings'
 
 
         self.variables_update = {
             "input": {
-                "name": "Updated appsettings",
-                "code" : "9000"
+                "dateFormat": "DD-MM-YYYY",
+                "timeFormat" : "12"
             }
         }
 
         self.appsettings_query = '''
-  query AppSettings($id: ID!) {
-    appSettings(id:$id) {
+  query AppSettings {
+    appSettings(id: "QXBwU2V0dGluZ3NOb2RlOjE=") {
       dateFormat
       timeFormat
     }
@@ -50,12 +50,12 @@ class GQLAppSettings(TestCase):
 '''
 
         self.appsettings_update_mutation = '''
-  mutation UpdateFinanceCostCenter($input: UpdateFinanceCostCenterInput!) {
-    updateFinanceCostcenter(input: $input) {
-      financeCostcenter {
+  mutation UpdateAppSettings($input: UpdateAppSettingsInput!) {
+    updateAppSettings(input: $input) {
+      appSettings {
         id
-        name
-        code
+        dateFormat
+        timeFormat
       }
     }
   }
@@ -69,284 +69,100 @@ class GQLAppSettings(TestCase):
 
     def test_query_one(self):
         """ Query appsettings as admin """   
-        print(to_global_id('AppSettingsNode', 1))
+        # print(to_global_id('AppSettingsNode', 1))
 
-        # # First query appsettingss to get node id easily
-        # node_id = self.get_node_id_of_first_appsettings()
+        executed = execute_test_client_api_query(self.appsettings_query, self.admin_user)
+        data = executed.get('data')
 
-        # # Now query single appsettings and check
-        # executed = execute_test_client_api_query(self.appsettings_query, self.admin_user, variables={"id": node_id})
-        # data = executed.get('data')
-        # self.assertEqual(data['financeCostcenter']['name'], appsettings.name)
-        # self.assertEqual(data['financeCostcenter']['archived'], appsettings.archived)
-        # self.assertEqual(data['financeCostcenter']['code'], appsettings.code)
+        self.assertEqual(data['appSettings']['dateFormat'], "YYYY-MM-DD")
+        self.assertEqual(data['appSettings']['timeFormat'], "24")
 
 
-    # def test_query_one_anon_user(self):
-    #     """ Deny permission for anon users Query one glacount """   
-    #     appsettings = f.FinanceCostCenterFactory.create()
-
-    #     # First query appsettingss to get node id easily
-    #     node_id = self.get_node_id_of_first_appsettings()
-
-    #     # Now query single appsettings and check
-    #     executed = execute_test_client_api_query(self.appsettings_query, self.anon_user, variables={"id": node_id})
-    #     errors = executed.get('errors')
-    #     self.assertEqual(errors[0]['message'], 'Not logged in!')
+    def test_query_one_anon_user(self):
+        """ Deny permission for anon users """   
+        executed = execute_test_client_api_query(self.appsettings_query, self.anon_user)
+        errors = executed.get('errors')
+        self.assertEqual(errors[0]['message'], 'Not logged in!')
 
 
-    # def test_query_one_permission_denied(self):
-    #     """ Permission denied message when user lacks authorization """   
-    #     # Create regular user
-    #     user = f.RegularUserFactory.create()
-    #     appsettings = f.FinanceCostCenterFactory.create()
+    def test_query_one_permission_regular_user(self):
+        """ Permission granted when logged in """   
+        # Create regular user
+        user = f.RegularUserFactory.create()
 
-    #     # First query appsettingss to get node id easily
-    #     node_id = self.get_node_id_of_first_appsettings()
-
-    #     # Now query single appsettings and check
-    #     executed = execute_test_client_api_query(self.appsettings_query, user, variables={"id": node_id})
-    #     errors = executed.get('errors')
-    #     self.assertEqual(errors[0]['message'], 'Permission denied!')
-
-
-    # def test_query_one_permission_granted(self):
-    #     """ Respond with data when user has permission """   
-    #     user = f.RegularUserFactory.create()
-    #     permission = Permission.objects.get(codename='view_financeappsettings')
-    #     user.user_permissions.add(permission)
-    #     user.save()
-    #     appsettings = f.FinanceCostCenterFactory.create()
-
-    #     # First query appsettingss to get node id easily
-    #     node_id = self.get_node_id_of_first_appsettings()
-
-    #     # Now query single location and check   
-    #     executed = execute_test_client_api_query(self.appsettings_query, user, variables={"id": node_id})
-    #     data = executed.get('data')
-    #     self.assertEqual(data['financeCostcenter']['name'], appsettings.name)
-
-
-    # def test_create_appsettings(self):
-    #     """ Create a appsettings """
-    #     query = self.appsettings_create_mutation
-    #     variables = self.variables_create
-
-    #     executed = execute_test_client_api_query(
-    #         query, 
-    #         self.admin_user, 
-    #         variables=variables
-    #     )
-    #     data = executed.get('data')
-    #     self.assertEqual(data['createFinanceCostcenter']['financeCostcenter']['name'], variables['input']['name'])
-    #     self.assertEqual(data['createFinanceCostcenter']['financeCostcenter']['archived'], False)
-    #     self.assertEqual(data['createFinanceCostcenter']['financeCostcenter']['code'], variables['input']['code'])
-
-
-    # def test_create_appsettings_anon_user(self):
-    #     """ Don't allow creating appsettingss for non-logged in users """
-    #     query = self.appsettings_create_mutation
-    #     variables = self.variables_create
-
-    #     executed = execute_test_client_api_query(
-    #         query, 
-    #         self.anon_user, 
-    #         variables=variables
-    #     )
-    #     data = executed.get('data')
-    #     errors = executed.get('errors')
-    #     self.assertEqual(errors[0]['message'], 'Not logged in!')
-
-
-    # def test_create_location_permission_granted(self):
-    #     """ Allow creating appsettingss for users with permissions """
-    #     query = self.appsettings_create_mutation
-    #     variables = self.variables_create
-
-    #     # Create regular user
-    #     user = f.RegularUserFactory.create()
-    #     permission = Permission.objects.get(codename=self.permission_add)
-    #     user.user_permissions.add(permission)
-    #     user.save()
-
-    #     executed = execute_test_client_api_query(
-    #         query, 
-    #         user, 
-    #         variables=variables
-    #     )
-    #     data = executed.get('data')
-    #     self.assertEqual(data['createFinanceCostcenter']['financeCostcenter']['name'], variables['input']['name'])
-    #     self.assertEqual(data['createFinanceCostcenter']['financeCostcenter']['archived'], False)
-    #     self.assertEqual(data['createFinanceCostcenter']['financeCostcenter']['code'], variables['input']['code'])
-
-
-    # def test_create_appsettings_permission_denied(self):
-    #     """ Check create appsettings permission denied error message """
-    #     query = self.appsettings_create_mutation
-    #     variables = self.variables_create
-
-    #     # Create regular user
-    #     user = f.RegularUserFactory.create()
-
-    #     executed = execute_test_client_api_query(
-    #         query, 
-    #         user, 
-    #         variables=variables
-    #     )
-    #     data = executed.get('data')
-    #     errors = executed.get('errors')
-    #     self.assertEqual(errors[0]['message'], 'Permission denied!')
-
-
-    # def test_update_appsettings(self):
-    #     """ Update a appsettings """
-    #     query = self.appsettings_update_mutation
-    #     appsettings = f.FinanceCostCenterFactory.create()
-    #     variables = self.variables_update
-    #     variables['input']['id'] = self.get_node_id_of_first_appsettings()
-
-    #     executed = execute_test_client_api_query(
-    #         query, 
-    #         self.admin_user, 
-    #         variables=variables
-    #     )
-    #     data = executed.get('data')
-    #     self.assertEqual(data['updateFinanceCostcenter']['financeCostcenter']['name'], variables['input']['name'])
-    #     self.assertEqual(data['updateFinanceCostcenter']['financeCostcenter']['code'], variables['input']['code'])
-
-
-    # def test_update_appsettings_anon_user(self):
-    #     """ Don't allow updating appsettingss for non-logged in users """
-    #     query = self.appsettings_update_mutation
-    #     appsettings = f.FinanceCostCenterFactory.create()
-    #     variables = self.variables_update
-    #     variables['input']['id'] = self.get_node_id_of_first_appsettings()
-
-    #     executed = execute_test_client_api_query(
-    #         query, 
-    #         self.anon_user, 
-    #         variables=variables
-    #     )
-    #     data = executed.get('data')
-    #     errors = executed.get('errors')
-    #     self.assertEqual(errors[0]['message'], 'Not logged in!')
-
-
-    # def test_update_appsettings_permission_granted(self):
-    #     """ Allow updating appsettingss for users with permissions """
-    #     query = self.appsettings_update_mutation
-    #     appsettings = f.FinanceCostCenterFactory.create()
-    #     variables = self.variables_update
-    #     variables['input']['id'] = self.get_node_id_of_first_appsettings()
-
-    #     # Create regular user
-    #     user = f.RegularUserFactory.create()
-    #     permission = Permission.objects.get(codename=self.permission_change)
-    #     user.user_permissions.add(permission)
-    #     user.save()
-
-    #     executed = execute_test_client_api_query(
-    #         query, 
-    #         user, 
-    #         variables=variables
-    #     )
-    #     data = executed.get('data')
-    #     self.assertEqual(data['updateFinanceCostcenter']['financeCostcenter']['name'], variables['input']['name'])
-    #     self.assertEqual(data['updateFinanceCostcenter']['financeCostcenter']['code'], variables['input']['code'])
-
-
-    # def test_update_appsettings_permission_denied(self):
-    #     """ Check update appsettings permission denied error message """
-    #     query = self.appsettings_update_mutation
-    #     appsettings = f.FinanceCostCenterFactory.create()
-    #     variables = self.variables_update
-    #     variables['input']['id'] = self.get_node_id_of_first_appsettings()
-
-    #     # Create regular user
-    #     user = f.RegularUserFactory.create()
-
-    #     executed = execute_test_client_api_query(
-    #         query, 
-    #         user, 
-    #         variables=variables
-    #     )
-    #     data = executed.get('data')
-    #     errors = executed.get('errors')
-    #     self.assertEqual(errors[0]['message'], 'Permission denied!')
-
-
-    # def test_archive_appsettings(self):
-    #     """ Archive a appsettings """
-    #     query = self.appsettings_archive_mutation
-    #     appsettings = f.FinanceCostCenterFactory.create()
-    #     variables = self.variables_archive
-    #     variables['input']['id'] = self.get_node_id_of_first_appsettings()
-
-    #     executed = execute_test_client_api_query(
-    #         query, 
-    #         self.admin_user, 
-    #         variables=variables
-    #     )
-    #     data = executed.get('data')
-    #     print(data)
-    #     self.assertEqual(data['archiveFinanceCostcenter']['financeCostcenter']['archived'], variables['input']['archived'])
-
-
-    # def test_archive_appsettings_anon_user(self):
-    #     """ Archive appsettings denied for anon user """
-    #     query = self.appsettings_archive_mutation
-    #     appsettings = f.FinanceCostCenterFactory.create()
-    #     variables = self.variables_archive
-    #     variables['input']['id'] = self.get_node_id_of_first_appsettings()
-
-    #     executed = execute_test_client_api_query(
-    #         query, 
-    #         self.anon_user, 
-    #         variables=variables
-    #     )
-    #     data = executed.get('data')
-    #     errors = executed.get('errors')
-    #     self.assertEqual(errors[0]['message'], 'Not logged in!')
-
-
-    # def test_archive_appsettings_permission_granted(self):
-    #     """ Allow archiving appsettingss for users with permissions """
-    #     query = self.appsettings_archive_mutation
-    #     appsettings = f.FinanceCostCenterFactory.create()
-    #     variables = self.variables_archive
-    #     variables['input']['id'] = self.get_node_id_of_first_appsettings()
-
-    #     # Create regular user
-    #     user = f.RegularUserFactory.create()
-    #     permission = Permission.objects.get(codename=self.permission_delete)
-    #     user.user_permissions.add(permission)
-    #     user.save()
-
-    #     executed = execute_test_client_api_query(
-    #         query, 
-    #         user,
-    #         variables=variables
-    #     )
-    #     data = executed.get('data')
-    #     self.assertEqual(data['archiveFinanceCostcenter']['financeCostcenter']['archived'], variables['input']['archived'])
-
-
-    # def test_archive_appsettings_permission_denied(self):
-    #     """ Check archive appsettings permission denied error message """
-    #     query = self.appsettings_archive_mutation
-    #     appsettings = f.FinanceCostCenterFactory.create()
-    #     variables = self.variables_archive
-    #     variables['input']['id'] = self.get_node_id_of_first_appsettings()
+        # Now query single appsettings and check
+        executed = execute_test_client_api_query(self.appsettings_query, user)
+        data = executed.get('data')
         
-    #     # Create regular user
-    #     user = f.RegularUserFactory.create()
+        self.assertEqual(data['appSettings']['dateFormat'], "YYYY-MM-DD")
 
-    #     executed = execute_test_client_api_query(
-    #         query, 
-    #         user, 
-    #         variables=variables
-    #     )
-    #     data = executed.get('data')
-    #     errors = executed.get('errors')
-    #     self.assertEqual(errors[0]['message'], 'Permission denied!')
 
+    def test_update_appsettings(self):
+        """ Update appsettings """
+        query = self.appsettings_update_mutation
+        variables = self.variables_update
+
+        executed = execute_test_client_api_query(
+            query, 
+            self.admin_user, 
+            variables=variables
+        )
+        data = executed.get('data')
+        self.assertEqual(data['updateAppSettings']['appSettings']['dateFormat'], variables['input']['dateFormat'])
+        self.assertEqual(data['updateAppSettings']['appSettings']['timeFormat'], variables['input']['timeFormat'])
+
+
+    def test_update_appsettings_anon_user(self):
+        """ Don't allow updating appsettingss for non-logged in users """
+        query = self.appsettings_update_mutation
+        appsettings = f.FinanceCostCenterFactory.create()
+        variables = self.variables_update
+
+        executed = execute_test_client_api_query(
+            query, 
+            self.anon_user, 
+            variables=variables
+        )
+        data = executed.get('data')
+        errors = executed.get('errors')
+        self.assertEqual(errors[0]['message'], 'Not logged in!')
+
+
+    def test_update_appsettings_permission_granted(self):
+        """ Allow updating appsettingss for users with permissions """
+        query = self.appsettings_update_mutation
+        appsettings = f.FinanceCostCenterFactory.create()
+        variables = self.variables_update
+
+        # Create regular user
+        user = f.RegularUserFactory.create()
+        permission = Permission.objects.get(codename=self.permission_change)
+        user.user_permissions.add(permission)
+        user.save()
+
+        executed = execute_test_client_api_query(
+            query, 
+            user, 
+            variables=variables
+        )
+        data = executed.get('data')
+        self.assertEqual(data['updateAppSettings']['appSettings']['dateFormat'], variables['input']['dateFormat'])
+
+
+    def test_update_appsettings_permission_denied(self):
+        """ Check update appsettings permission denied error message """
+        query = self.appsettings_update_mutation
+        appsettings = f.FinanceCostCenterFactory.create()
+        variables = self.variables_update
+
+        # Create regular user
+        user = f.RegularUserFactory.create()
+
+        executed = execute_test_client_api_query(
+            query, 
+            user, 
+            variables=variables
+        )
+        data = executed.get('data')
+        errors = executed.get('errors')
+        self.assertEqual(errors[0]['message'], 'Permission denied!')
