aeba756d9661c7344202392de4def1fbbd6bb70e
Edwin van de Ven <edwin@simplycode.nl>
2019-08-27 11:21:23 +0200

    Add edit to class menu

--- app/cs_admin/src/components/schedule/classes/class/ClassMenu.jsx
+++ app/cs_admin/src/components/schedule/classes/class/ClassMenu.jsx
@@ -10,30 +10,52 @@ import {
 } from "tabler-react";
 import HasPermissionWrapper from "../../../HasPermissionWrapper"
 
-let attendance_active
+let attendance_active = false
+let edit_active = false
 
+function ClassMenu ({ t, scheduleItemId, class_date, active_link }) {
+  switch (active_link) {
+    case "attendance":
+      attendance_active = true
+      break
+    case "edit":
+      edit_active = true
+      break
+  }
 
-const ClassMenu = ({ t, scheduleItemId, class_date, active_link }) => (
-  <List.Group transparent={true}>
-    {(active_link === "attendance") ? attendance_active = true: attendance_active = false}
-    
+  return (
+    <List.Group transparent={true}>   
+      <HasPermissionWrapper 
+          resource="scheduleitemattendance"
+          permission="view" 
+      >
+        <List.GroupItem
+            key={v4()}
+            className="d-flex align-items-center"
+            to={"#/schedule/classes/class/attendance/" + scheduleItemId + "/" + class_date}
+            icon="check-circle"
+            active={attendance_active}
+            >
+            {t("general.attendance")}
+        </List.GroupItem>
+      </HasPermissionWrapper>
+      <HasPermissionWrapper 
+          resource="scheduleclassweeklyotc"
+          permission="change" 
+      >
+        <List.GroupItem
+            key={v4()}
+            className="d-flex align-items-center"
+            to={"#/schedule/classes/class/edit/" + scheduleItemId + "/" + class_date}
+            icon="edit-3"
+            active={edit_active}
+            >
+            {t("general.edit")}
+        </List.GroupItem>
+      </HasPermissionWrapper>
 
-    <HasPermissionWrapper 
-        resource="scheduleitemattendance"
-        permission="view" 
-    >
-      <List.GroupItem
-          key={v4()}
-          className="d-flex align-items-center"
-          to={"#/schedule/classes/class/attendance/" + scheduleItemId + "/" + class_date}
-          icon="check-circle"
-          active={attendance_active}
-          >
-          {t("general.attendance")}
-      </List.GroupItem>
-    </HasPermissionWrapper>
-
-  </List.Group>
-);
+    </List.Group>
+  )
+}
 
 export default withTranslation()(ClassMenu)
