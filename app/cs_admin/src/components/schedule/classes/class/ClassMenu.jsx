5ea3302b0759a5f897eb02a00a78a3e1b58db4fe
Edwin van de Ven <edwin@simplycode.nl>
2019-09-03 13:51:39 +0200

    Add header to class menu

--- app/cs_admin/src/components/schedule/classes/class/ClassMenu.jsx
+++ app/cs_admin/src/components/schedule/classes/class/ClassMenu.jsx
@@ -25,37 +25,40 @@ function ClassMenu ({ t, scheduleItemId, class_date, active_link }) {
   }
 
   return (
-    <List.Group transparent={true}>   
-      <HasPermissionWrapper 
-          resource="scheduleitemattendance"
-          permission="view" 
-      >
-        <List.GroupItem
-            key={v4()}
-            className="d-flex align-items-center"
-            to={"#/schedule/classes/class/attendance/" + scheduleItemId + "/" + class_date}
-            icon="check-circle"
-            active={attendance_active}
-            >
-            {t("general.attendance")}
-        </List.GroupItem>
-      </HasPermissionWrapper>
-      <HasPermissionWrapper 
-          resource="scheduleclassweeklyotc"
-          permission="change" 
-      >
-        <List.GroupItem
-            key={v4()}
-            className="d-flex align-items-center"
-            to={"#/schedule/classes/class/edit/" + scheduleItemId + "/" + class_date}
-            icon="edit-3"
-            active={edit_active}
-            >
-            {t("general.edit")}
-        </List.GroupItem>
-      </HasPermissionWrapper>
+    <div>
+      <h5>{t("general.menu")}</h5>
+      <List.Group transparent={true}>   
+        <HasPermissionWrapper 
+            resource="scheduleitemattendance"
+            permission="view" 
+        >
+          <List.GroupItem
+              key={v4()}
+              className="d-flex align-items-center"
+              to={"#/schedule/classes/class/attendance/" + scheduleItemId + "/" + class_date}
+              icon="check-circle"
+              active={attendance_active}
+              >
+              {t("general.attendance")}
+          </List.GroupItem>
+        </HasPermissionWrapper>
+        <HasPermissionWrapper 
+            resource="scheduleclassweeklyotc"
+            permission="change" 
+        >
+          <List.GroupItem
+              key={v4()}
+              className="d-flex align-items-center"
+              to={"#/schedule/classes/class/edit/" + scheduleItemId + "/" + class_date}
+              icon="edit-3"
+              active={edit_active}
+              >
+              {t("general.edit")}
+          </List.GroupItem>
+        </HasPermissionWrapper>
 
-    </List.Group>
+      </List.Group>
+    </div>
   )
 }
 
