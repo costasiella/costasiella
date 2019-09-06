2796ae9cad56bb6c1f92e4f70166644a0b1e6679
Edwin van de Ven <edwin@simplycode.nl>
2019-09-06 15:35:32 +0200

    Import timedelta

--- app/app/settings.py
+++ app/app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from datetime import timedelta
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
