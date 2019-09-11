e760fa41b13ef7322788a6e88c371012a89e1f2f
Edwin van de Ven <edwin@simplycode.nl>
2019-09-11 18:45:44 +0200

    Add revoke token query & schema endpoint

--- app/costasiella/schema/__init__.py
+++ app/costasiella/schema/__init__.py
@@ -138,4 +138,5 @@ class Mutation(AccountMutation,
     token_auth = graphql_jwt.ObtainJSONWebToken.Field()
     verify_token = graphql_jwt.Verify.Field()
     refresh_token = graphql_jwt.Refresh.Field()
+    revoke_token = graphql_jwt.Revoke.Field()
 
