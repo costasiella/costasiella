3bfb49003cca95376adb302e9c8aaa5f479310bb
Edwin van de Ven <edwin@simplycode.nl>
2019-09-17 10:53:50 +0200

    Add b64 encoded upload file to input vars

--- app/cs_admin/src/components/organization/organization/documents/OrganizationDocumentAdd.jsx
+++ app/cs_admin/src/components/organization/organization/documents/OrganizationDocumentAdd.jsx
@@ -1,18 +1,23 @@
 // @flow
 
-import React from 'react'
+import React, { useState, useRef } from 'react'
 import { useMutation } from "react-apollo";
 import { withTranslation } from 'react-i18next'
 import { withRouter } from "react-router"
-import { Formik } from 'formik'
+import { 
+  Formik,
+  Form as FoForm, 
+  Field, 
+  ErrorMessage 
+} from 'formik'
 import { toast } from 'react-toastify'
 import { Link } from 'react-router-dom'
 
 import { ADD_DOCUMENT, GET_DOCUMENTS_QUERY } from "./queries"
 import { DOCUMENT_SCHEMA } from './yupSchema'
 import { dateToLocalISO } from "../../../../tools/date_tools"
-import OrganizationDocumentForm from './OrganizationDocumentForm'
-
+// import OrganizationDocumentForm from './OrganizationDocumentForm'
+import CSDatePicker from "../../../ui/CSDatePicker"
 
 import {
   Page,
@@ -30,11 +35,28 @@ import OrganizationMenu from '../../OrganizationMenu'
 import OrganizationDocumentsBase from "./OrganizationDocumentsBase"
 
 
-const return_url = "/organization/levels"
+const customFileInputLabelStyle = {
+  whiteSpace: "nowrap",
+  display: "block",
+  overflow: "hidden"
+}  
+
 
 function OrganizationDocumentAdd({ t, match, history }) {
   const organizationId = match.params.organization_id
   const documentType = match.params.document_type
+
+  // Vars for document file input field start
+  const [fileName, setFileName] = useState("")
+  const inputFileName = useRef(null)
+  const fileInputLabel = fileName || t("general.custom_file_input_inner_label")
+
+  const handleFileInputChange = (event) => {
+    console.log('on change triggered')
+    setFileName(event.target.files[0].name)
+  }
+
+  // Vars for document file input field end
   
   const returnUrl = `/organization/documents/${organizationId}/${documentType}`
   const back = <Link to={returnUrl}>
@@ -90,35 +112,99 @@ function OrganizationDocumentAdd({ t, match, history }) {
               inputVars.dateEnd = dateToLocalISO(values.dateEnd)
             }
 
+            let reader = new FileReader()
+            reader.onload = function(reader_event) {
+              console.log(reader_event.target.result)
+              let b64_enc_file = reader_event.target.result
+              console.log(b64_enc_file)
+              // Add uploaded document b64 encoded blob to input vars
+              inputVars.document = b64_enc_file
 
-            addDocument({ variables: {
-              input: inputVars
-            }, refetchQueries: [
-                {query: GET_DOCUMENTS_QUERY, variables: {documentType: documentType}}
-            ]})
-            .then(({ data }) => {
-                console.log('got data', data);
-                toast.success((t('organization.documents.toast_add_success')), {
-                    position: toast.POSITION.BOTTOM_RIGHT
-                  })
-              }).catch((error) => {
-                toast.error((t('general.toast_server_error')) + ': ' +  error, {
-                    position: toast.POSITION.BOTTOM_RIGHT
-                  })
-                console.log('there was an error sending the query', error)
-                setSubmitting(false)
-              })
+              addDocument({ variables: {
+                input: inputVars
+              }, refetchQueries: [
+                  {query: GET_DOCUMENTS_QUERY, variables: {documentType: documentType}}
+              ]})
+              .then(({ data }) => {
+                  console.log('got data', data);
+                  toast.success((t('organization.documents.toast_add_success')), {
+                      position: toast.POSITION.BOTTOM_RIGHT
+                    })
+                }).catch((error) => {
+                  toast.error((t('general.toast_server_error')) + ': ' +  error, {
+                      position: toast.POSITION.BOTTOM_RIGHT
+                    })
+                  console.log('there was an error sending the query', error)
+                  setSubmitting(false)
+                })
+
+            }
+            
+            let file = inputFileName.current.files[0]
+            reader.readAsDataURL(file)
           }}
           >
           {({ isSubmitting, errors, values, setFieldTouched, setFieldValue }) => (
-              <OrganizationDocumentForm 
-                isSubmitting={isSubmitting}
-                values={values}
-                errors={errors}
-                setFieldTouched={setFieldTouched}
-                setFieldValue={setFieldValue}
-                return_url={return_url}
-              />
+            <FoForm>
+            <Card.Body>
+              <Grid.Row>
+                <Grid.Col>
+                  <Form.Group label={t('general.version')}>
+                    <Field type="text" 
+                          name="version" 
+                          className={(errors.version) ? "form-control is-invalid" : "form-control"} 
+                          autoComplete="off" />
+                    <ErrorMessage name="version" component="span" className="invalid-feedback" />
+                  </Form.Group>
+                </Grid.Col>
+                <Grid.Col>
+                  <Form.Group label={t('general.custom_file_input_label')}>
+                    <div className="custom-file">
+                      <input type="file" ref={inputFileName} className="custom-file-input" onChange={handleFileInputChange} />
+                      <label className="custom-file-label" style={customFileInputLabelStyle}>
+                        {fileInputLabel}
+                      </label>
+                    </div>
+                  </Form.Group>
+                </Grid.Col>
+              </Grid.Row>
+              <Grid.Row>
+                <Grid.Col>
+                  <Form.Group label={t('general.date_start')}>
+                    <CSDatePicker 
+                      selected={values.dateStart}
+                      onChange={(date) => setFieldValue("dateStart", date)}
+                      onBlur={() => setFieldTouched("dateStart", true)}
+                    />
+                    <ErrorMessage name="dateStart" component="span" className="invalid-feedback" />
+                  </Form.Group>
+                </Grid.Col>
+                <Grid.Col>
+                  <Form.Group label={t('general.date_end')}>
+                    <CSDatePicker 
+                      selected={values.dateEnd}
+                      onChange={(date) => setFieldValue("dateEnd", date)}
+                      onBlur={() => setFieldTouched("dateEnd", true)}
+                    />
+                    <ErrorMessage name="dateEnd" component="span" className="invalid-feedback" />
+                  </Form.Group>
+                </Grid.Col>
+              </Grid.Row>
+            </Card.Body>
+            <Card.Footer>
+              <Button 
+                color="primary"
+                className="pull-right" 
+                type="submit" 
+                disabled={isSubmitting}
+              >
+                {t('general.submit')}
+              </Button>
+              <Button color="link" onClick={() => history.push(returnUrl)}>
+                {t('general.cancel')}
+              </Button>
+            </Card.Footer>
+          </FoForm>
           )}
         </Formik>
       </Card>
