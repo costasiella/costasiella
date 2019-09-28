5b762a1990527d0175371537c9bd855b4a3cb442
Edwin van de Ven <edwin@simplycode.nl>
2019-09-28 20:00:09 +0200

    Split settings into multiple files

---
+++ app/app/settings/development.py
@@ -0,0 +1,9 @@
+"""
+Django settings for development
+"""
+
+from app.settings.common import *
+
+# Media files (User uploads)
+# https://docs.djangoproject.com/en/2.1/topics/files/
+MEDIA_ROOT = '/home/edwin/Development/costasiella/app/costasiella/media_test'
