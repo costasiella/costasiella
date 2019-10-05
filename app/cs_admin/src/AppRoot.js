01eda1ec66fe8fac8ba05dac23e0b290dae1a0c8
Edwin van de Ven <edwin@simplycode.nl>
2019-10-05 18:05:03 +0200

    Activate accepted documents route

--- app/cs_admin/src/AppRoot.js
+++ app/cs_admin/src/AppRoot.js
@@ -100,6 +100,7 @@ import RelationsHome from './components/relations/home/RelationsHome'
 import RelationsAccounts from './components/relations/accounts/RelationsAccounts'
 import RelationsAccountAdd from './components/relations/accounts/RelationsAccountAdd'
 import RelationsAccountProfile from './components/relations/accounts/RelationsAccountProfile'
+import AccountAcceptedDocuments from './components/relations/accounts/accepted_documents/AcceptedDocuments.jsx'
 import AccountClasspasses from './components/relations/accounts/classpasses/AccountClasspasses'
 import AccountClasspassAdd from './components/relations/accounts/classpasses/AccountClasspassAdd'
 import AccountClasspassEdit from './components/relations/accounts/classpasses/AccountClasspassEdit'
@@ -270,6 +271,7 @@ function AppRoot({ t }) {
           <PrivateRoute exact path="/relations/accounts" component={RelationsAccounts} />
           <PrivateRoute exact path="/relations/accounts/add" component={RelationsAccountAdd} />
           <PrivateRoute exact path="/relations/accounts/:account_id/profile" component={RelationsAccountProfile} />
+          <PrivateRoute exact path="/relations/accounts/:account_id/accepted_documents" component={AccountAcceptedDocuments} />
           <PrivateRoute exact path="/relations/accounts/:account_id/classpasses" component={AccountClasspasses} />
           <PrivateRoute exact path="/relations/accounts/:account_id/classpasses/add" component={AccountClasspassAdd} />
           <PrivateRoute exact path="/relations/accounts/:account_id/classpasses/edit/:id" component={AccountClasspassEdit} />
