e70549edab42e778e430a38cd4ece33dc5899780
Edwin van de Ven <edwin@simplycode.nl>
2019-10-05 10:29:38 +0200

    Fix query symbols

--- app/cs_admin/src/components/relations/accounts/accepted_documents/queries.js
+++ app/cs_admin/src/components/relations/accounts/accepted_documents/queries.js
@@ -1,28 +1,29 @@
 import gql from "graphql-tag"
 
 export const GET_ACCOUNT_ACCEPTED_DOCUMENTS_QUERY = gql`
-query AccountAcceptedDocuments($after: String, $before: String, $account: ID!) {
-  accountAcceptedDocuments(first: 15, before:$before, after:$after, account: $account) {
-    pageInfo {
-      hasNextPage
-      hasPreviousPage
-      startCursor
-      endCursor
-    }
-    edges {
-      node {
-        id
-        account {
-          email
-        }
-        document {
+  query AccountAcceptedDocuments($after: String, $before: String, $account: ID!) {
+    accountAcceptedDocuments(first: 15, before:$before, after:$after, account: $account) {
+      pageInfo {
+        hasNextPage
+        hasPreviousPage
+        startCursor
+        endCursor
+      }
+      edges {
+        node {
           id
-          documentType
-          version
-          urlDocument
+          account {
+            email
+          }
+          document {
+            id
+            documentType
+            version
+            urlDocument
+          }
+          dateAccepted
+          clientIp
         }
-        dateAccepted
-        clientIp
       }
     }
     account(id:$account) {
