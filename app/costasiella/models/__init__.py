f7692d239d9716d2d971f96163706b273f8d4031
Edwin van de Ven <edwin@simplycode.nl>
2019-11-16 15:45:10 +0100

    Add model for schedule item price

--- app/costasiella/models/__init__.py
+++ app/costasiella/models/__init__.py
@@ -41,4 +41,5 @@ from .schedule_item_teacher import ScheduleItemTeacher
 from .schedule_item_teacher_available import ScheduleItemTeacherAvailable
 from .schedule_item_organization_classpass_group import ScheduleItemOrganizationClasspassGroup
 from .schedule_item_organization_subscription_group import ScheduleItemOrganizationSubscriptionGroup
+from .schedule_item_price import ScheduleItemPrice
 from .schedule_item_weekly_otc import ScheduleItemWeeklyOTC
