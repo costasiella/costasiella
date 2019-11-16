77a5a683b6303fe7b0352bf20e0315a03b50fa05
Edwin van de Ven <edwin@simplycode.nl>
2019-11-16 15:59:38 +0100

    Add schema for price based on teacher

---
+++ app/costasiella/schema/schedule_item_price.py
@@ -0,0 +1,217 @@
+from django.utils.translation import gettext as _
+
+import graphene
+from graphene_django import DjangoObjectType
+from graphene_django.filter import DjangoFilterConnectionField
+from graphql import GraphQLError
+
+from ..models import Account, ScheduleItem, ScheduleItemPrice, OrganizationClasspass
+from ..modules.gql_tools import require_login_and_permission, get_rid
+from ..modules.messages import Messages
+
+m = Messages()
+
+class ScheduleItemPriceNode(DjangoObjectType):
+    # Disable output like "A_3" by graphene automatically converting model choices
+    # to an Enum field
+    role = graphene.Field(graphene.String, source='role')
+    role_2 = graphene.Field(graphene.String, source='role_2')
+
+    class Meta:
+        model = ScheduleItemPrice
+        filter_fields = ['schedule_item']
+        interfaces = (graphene.relay.Node, )
+
+    @classmethod
+    def get_node(self, info, id):
+        user = info.context.user
+        require_login_and_permission(user, 'costasiella.view_scheduleitemprice')
+
+        # Return only public non-archived location rooms
+        return self._meta.model.objects.get(id=id)
+
+
+class ScheduleItemPriceQuery(graphene.ObjectType):
+    schedule_item_prices = DjangoFilterConnectionField(ScheduleItemPriceNode)
+    schedule_item_price = graphene.relay.Node.Field(ScheduleItemPriceNode)
+
+    def resolve_schedule_item_prices(self, info, **kwargs):
+        user = info.context.user
+        require_login_and_permission(user, 'costasiella.view_scheduleitemprice')
+
+        return ScheduleItemPrice.objects.order_by('-date_start')
+            
+
+def validate_schedule_item_price_create_update_input(input):
+    """
+    Validate input
+    """ 
+    result = {}
+
+    # Check Account
+    if 'account' in input:
+        if input['account']:
+            rid = get_rid(input['account'])
+            account = Account.objects.filter(id=rid.id).first()
+            result['account'] = account
+            if not account:
+                raise Exception(_('Invalid Account ID!'))            
+
+    # Check Account
+    if 'account_2' in input:
+        if input['account_2']:
+            rid = get_rid(input['account_2'])
+            account_2 = Account.objects.filter(id=rid.id).first()
+            result['account_2'] = account_2
+            if not account_2:
+                raise Exception(_('Invalid Account ID!'))            
+
+    # Check Schedule item
+    if 'schedule_item' in input:
+        if input['schedule_item']:
+            rid = get_rid(input['schedule_item'])
+            schedule_item = ScheduleItem.objects.get(id=rid.id)
+            result['schedule_item'] = schedule_item
+            if not schedule_item:
+                raise Exception(_('Invalid Schedule Item (class) ID!'))   
+
+    # Check Organization Classpass Dropin
+    if 'organization_classpass_dropin' in input:
+        if input['organization_classpass_dropin']:
+            rid = get_rid(input['organization_classpass_dropion'])
+            organization_classpass = OrganizationClasspass.get(id=rid.id)
+            result['organization_classpass_dropin'] = organization_classpass
+            if not organization_classpass:
+                raise Exception(_('Invalid Organization Classpass ID!'))   
+
+    # Check Organization Classpass Trial
+    if 'organization_classpass_trial' in input:
+        if input['organization_classpass_trial']:
+            rid = get_rid(input['organization_classpass_dropion'])
+            organization_classpass = OrganizationClasspass.get(id=rid.id)
+            result['organization_classpass_trial'] = organization_classpass
+            if not organization_classpass:
+                raise Exception(_('Invalid Organization Classpass ID!'))   
+
+
+    return result
+
+
+class CreateScheduleItemPrice(graphene.relay.ClientIDMutation):
+    class Input:
+        schedule_item = graphene.ID(required=True)
+        account = graphene.ID(required=True)
+        role = graphene.String(required=False, default_value="")
+        account_2 = graphene.ID(required=False, defailt_value="")
+        role_2 = graphene.String(required=False, default_value="")
+        date_start = graphene.types.datetime.Date(required=True)
+        date_end = graphene.types.datetime.Date(required=False, default_value=None)
+
+    schedule_item_price = graphene.Field(ScheduleItemPriceNode)
+
+    @classmethod
+    def mutate_and_get_payload(self, root, info, **input):
+        user = info.context.user
+        require_login_and_permission(user, 'costasiella.add_scheduleitemprice')
+
+        validation_result = validate_schedule_item_price_create_update_input(input)
+
+        schedule_item_price = ScheduleItemPrice(
+            schedule_item = validation_result['schedule_item'],
+            account=validation_result['account'],
+            date_start=input['date_start']
+        )
+
+        # Optional fields
+        date_end = input.get('date_end', None)
+        if date_end:
+            schedule_item_price.date_end = date_end
+
+        role = input.get('role', None)
+        if role:
+            schedule_item_price.role = role
+
+        account_2 = validation_result.get('account_2', None)
+        if account_2:
+            schedule_item_price.account_2 = account_2
+
+        role_2 = input.get('role_2', None)
+        if role_2:
+            schedule_item_price.role_2 = role_2
+
+        schedule_item_price.save()
+
+        return CreateScheduleItemPrice(schedule_item_price=schedule_item_price)
+
+
+class UpdateScheduleItemPrice(graphene.relay.ClientIDMutation):
+    class Input:
+        id = graphene.ID(required=True)
+        account = graphene.ID(required=True)
+        role = graphene.String(required=False, default_value="")
+        account_2 = graphene.ID(required=False)
+        role_2 = graphene.String(required=False, default_value="")
+        date_start = graphene.types.datetime.Date(required=True)
+        date_end = graphene.types.datetime.Date(required=False, default_value=None)
+        
+    schedule_item_price = graphene.Field(ScheduleItemPriceNode)
+
+    @classmethod
+    def mutate_and_get_payload(self, root, info, **input):
+        user = info.context.user
+        require_login_and_permission(user, 'costasiella.change_scheduleitemprice')
+
+        rid = get_rid(input['id'])
+        schedule_item_price = ScheduleItemPrice.objects.filter(id=rid.id).first()
+        if not schedule_item_price:
+            raise Exception('Invalid Schedule Item Teacher ID!')
+
+        validation_result = validate_schedule_item_price_create_update_input(input)
+
+        schedule_item_price.account=validation_result['account']
+        schedule_item_price.date_start=input['date_start']
+        
+        # Optional fields
+        if 'date_end' in input:
+            schedule_item_price.date_end = input['date_end']
+
+        if 'role' in input:
+            schedule_item_price.role = input['role']
+
+        if 'account_2' in validation_result:
+            schedule_item_price.account_2 = validation_result['account_2']
+
+        if 'role_2' in input:
+            schedule_item_price.role_2 = input['role_2']
+
+        schedule_item_price.save()
+
+        return UpdateScheduleItemPrice(schedule_item_price=schedule_item_price)
+
+
+class DeleteScheduleItemPrice(graphene.relay.ClientIDMutation):
+    class Input:
+        id = graphene.ID(required=True)
+
+    ok = graphene.Boolean()
+
+    @classmethod
+    def mutate_and_get_payload(self, root, info, **input):
+        user = info.context.user
+        require_login_and_permission(user, 'costasiella.delete_scheduleitemprice')
+
+        rid = get_rid(input['id'])
+        schedule_item_price = ScheduleItemPrice.objects.filter(id=rid.id).first()
+        if not schedule_item_price:
+            raise Exception('Invalid Schedule Item Teacher ID!')
+
+        ok = schedule_item_price.delete()
+
+        return DeleteScheduleItemPrice(ok=ok)
+
+
+class ScheduleItemPriceMutation(graphene.ObjectType):
+    delete_schedule_item_price = DeleteScheduleItemPrice.Field()
+    create_schedule_item_price = CreateScheduleItemPrice.Field()
+    update_schedule_item_price = UpdateScheduleItemPrice.Field()
+    
