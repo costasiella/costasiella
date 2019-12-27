bdc5c1997e9a4953788a8fadac315f17c37ec637
Edwin van de Ven <edwin@simplycode.nl>
2019-12-27 18:08:34 +0100

    Add preview view for invoice pdf exports

--- app/costasiella/views/__init__.py
+++ app/costasiella/views/__init__.py
@@ -1,2 +1,2 @@
 from .organization_document import privacy_policy, terms_and_conditions
-from .export_finance_invoice import invoice_pdf
+from .export_finance_invoice import invoice_pdf, invoice_pdf_preview
--- app/costasiella/views/export_finance_invoice.py
+++ app/costasiella/views/export_finance_invoice.py
@@ -1,8 +1,13 @@
 from django.utils.translation import gettext as _
 from django.utils import timezone
 from django.shortcuts import redirect, render
-from django.http import Http404
+from django.http import Http404, HttpResponse
 from django.db.models import Q
+from django.template.loader import get_template, render_to_string
+
+
+# from django.template.loader import render_to_string
+# rendered = render_to_string('my_template.html', {'foo': 'bar'})
 
 from ..models import FinanceInvoice, FinanceInvoiceGroup, FinanceInvoiceGroupDefault, FinanceInvoiceItem
 from ..modules.gql_tools import require_login_and_permission, get_rid
@@ -24,6 +29,43 @@ def invoice_pdf(request, node_id):
     finance_invoice = FinanceInvoice.objects.filter(id=rid.id).first()
 
     print(finance_invoice)
+
+
+    template_path = 'system/invoices/export_invoice_pdf.html'
+    t = get_template(template_path)
+    print(t)
+    rendered_template = render_to_string(template_path)
+
+    return HttpResponse(rendered_template)
+
+
+def invoice_pdf_preview(request, node_id):
+    """
+    Preview for invoice_pdf
+
+    :param: POST: node_id - FinanceInvoiceNode ID
+    """
+    print("InvoiceID:")
+    print(node_id)
+    # print(request.POST.node_id)
+
+    rid = get_rid(node_id)
+    print(rid)
+    finance_invoice = FinanceInvoice.objects.get(id=rid.id)
+
+    if not finance_invoice:
+        raise Http404(_("Invoice not found..."))
+
+    print(finance_invoice)
+
+
+    template_path = 'system/invoices/export_invoice_pdf.html'
+    t = get_template(template_path)
+    print(t)
+    rendered_template = render_to_string(template_path)
+
+    return HttpResponse(rendered_template)
+
 
     # qs = OrganizationDocument.objects.filter(
     #     Q(document_type = "TERMS_AND_CONDITIONS") &
