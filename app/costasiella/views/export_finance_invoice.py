a2249b6c0d5fc061ca8b460aeb8b3b9ee7360454
Edwin van de Ven <edwin@simplycode.nl>
2019-12-28 18:08:28 +0100

    Return PDF

--- app/costasiella/views/export_finance_invoice.py
+++ app/costasiella/views/export_finance_invoice.py
@@ -1,7 +1,9 @@
+import io
+
 from django.utils.translation import gettext as _
 from django.utils import timezone
 from django.shortcuts import redirect, render
-from django.http import Http404, HttpResponse
+from django.http import Http404, HttpResponse, FileResponse
 from django.db.models import Q
 from django.template.loader import get_template, render_to_string
 
@@ -13,49 +15,16 @@ from ..models import AppSettings, FinanceInvoice, FinanceInvoiceGroup, FinanceIn
 from ..modules.gql_tools import require_login_and_permission, get_rid
 
 
-# Create your views here.
-def invoice_pdf(request, node_id):
+def invoice_html(node_id):
     """
-    Export invoice as PDF
-
-    :param: POST: node_id - FinanceInvoiceNode ID
+    Return rendered invoice html template
     """
-    print("InvoiceID:")
-    print(node_id)
-    # print(request.POST.node_id)
-
-    rid = get_rid(node_id)
-    print(rid)
-    finance_invoice = FinanceInvoice.objects.filter(id=rid.id).first()
-
-    print(finance_invoice)
-
-
-    template_path = 'system/invoices/export_invoice_pdf.html'
-    t = get_template(template_path)
-    print(t)
-    rendered_template = render_to_string(template_path)
-
-    return HttpResponse(rendered_template)
-
-
-def invoice_pdf_preview(request, node_id):
-    """
-    Preview for invoice_pdf
-
-    :param: POST: node_id - FinanceInvoiceNode ID
-    """
-    print("InvoiceID:")
-    print(node_id)
-    # print(request.POST.node_id)
-
     rid = get_rid(node_id)
     print(rid)
     finance_invoice = FinanceInvoice.objects.get(id=rid.id)
 
     if not finance_invoice:
         raise Http404(_("Invoice not found..."))
-
 
     app_settings = AppSettings.objects.get(id=1)
     organization = Organization.objects.get(id=100)
@@ -86,17 +55,64 @@ def invoice_pdf_preview(request, node_id):
         }
     )
 
-    return HttpResponse(rendered_template)
+    return rendered_template
 
 
-    # qs = OrganizationDocument.objects.filter(
-    #     Q(document_type = "TERMS_AND_CONDITIONS") &
-    #     Q(date_start__lte = today) &
-    #     (Q(date_end__gte = today) | Q(date_end__isnull = True))           
-    # )
+    # permission  = ((auth.has_membership(group_id='Admins') or
+    #                 auth.has_permission('read', 'invoices')) or
+    #                invoice.get_linked_customer_id() == auth.user.id)
 
-    # if qs.exists():
-    #     document = qs.first()
-    #     return redirect(document.document.url)
-    # else: 
-    #     raise Http404(_("File not found..."))
+    # if not permission:
+    #     return T("Not authorized")
+
+    # html = pdf_template(iID)
+
+    # fname = 'Invoice_' + invoice.invoice.InvoiceID + '.pdf'
+    # response.headers['Content-Type']='application/pdf'
+    # response.headers['Content-disposition']='attachment; filename=' + fname
+    # # return pyfpdf_from_html(html)
+
+    # stream = io.BytesIO()
+    # invoice = weasyprint.HTML(string=html).write_pdf(stream)
+
+    # return stream.getvalue()
+
+
+# Create your views here.
+def invoice_pdf(request, node_id):
+    """
+    Export invoice as PDF
+
+    :param: POST: node_id - FinanceInvoiceNode ID
+    """
+    import weasyprint
+    print("InvoiceID:")
+    print(node_id)
+    # print(request.POST.node_id)
+
+    html = invoice_html(node_id)
+
+    # Create a file-like buffer to receive PDF data.
+    buffer = io.BytesIO()
+    pdf = weasyprint.HTML(string=html).write_pdf(buffer)
+
+    # FileResponse sets the Content-Disposition header so that browsers
+    # present the option to save the file.
+    buffer.seek(0)
+
+    return FileResponse(buffer, as_attachment=True, filename='invoice.pdf')
+
+
+def invoice_pdf_preview(request, node_id):
+    """
+    Preview for invoice_pdf
+
+    :param: POST: node_id - FinanceInvoiceNode ID
+    """
+    print("InvoiceID:")
+    print(node_id)
+    # print(request.POST.node_id)
+
+    html = invoice_html(node_id)
+
+    return HttpResponse(html)
