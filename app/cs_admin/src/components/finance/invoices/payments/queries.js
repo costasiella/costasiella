6cdfe716e87c0ec788bb4a9e5b10f6392d32d0b9
Edwin van de Ven <edwin@simplycode.nl>
2019-12-31 14:25:23 +0100

    Get input values query

---
+++ app/cs_admin/src/components/finance/invoices/payments/queries.js
@@ -0,0 +1,59 @@
+import gql from "graphql-tag"
+
+
+export const GET_INPUT_VALUES_QUERY = gql`
+  query InputValues($before: String, $after: String) {
+    financePaymentMethods(first: 100, before: $before, after: $after, archived: false) {
+      pageInfo {
+        startCursor
+        endCursor
+        hasNextPage
+        hasPreviousPage
+      }
+      edges {
+        node {
+          id
+          name
+        }
+      }
+    }
+  }
+`
+
+
+export const CREATE_INVOICE_ITEM = gql`
+  mutation CreateFinanceInvoiceItem($input: CreateFinanceInvoiceItemInput!) {
+    createFinanceInvoiceItem(input: $input) {
+      financeInvoiceItem {
+        id
+        productName
+        description
+        quantity
+        price
+        financeTaxRate {
+          id
+          name
+        }
+      }
+    }
+  }
+`
+
+
+export const UPDATE_INVOICE_ITEM = gql`
+  mutation UpdateFinanceInvoiceItem($input: UpdateFinanceInvoiceItemInput!) {
+    updateFinanceInvoiceItem(input: $input) {
+      financeInvoiceItem {
+        id
+        productName
+        description
+        quantity
+        price
+        financeTaxRate {
+          id
+          name
+        }
+      }
+    }
+  }
+`
