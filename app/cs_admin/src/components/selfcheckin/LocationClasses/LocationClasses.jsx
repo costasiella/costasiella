c655d2daf8a028b2b291f2d6b83e9a802dffc9a0
Edwin van de Ven <edwin@simplycode.nl>
2020-01-23 19:55:13 +0100

    Add selfcheckin list of classes for a location

--- app/cs_admin/src/components/selfcheckin/LocationClasses/LocationClasses.jsx
+++ app/cs_admin/src/components/selfcheckin/LocationClasses/LocationClasses.jsx
@@ -1,55 +1,119 @@
 // @flow
 
-import React, {Component } from 'react'
+import React, { useContext } from 'react'
 import { withTranslation } from 'react-i18next'
 import { withRouter } from "react-router"
 import { useQuery } from '@apollo/react-hooks'
 import { Link } from 'react-router-dom'
 
+import moment from 'moment'
+
 import {
+  Card,
   Icon,
-  List
+  Table
 } from "tabler-react";
 import SelfCheckinBase from "../SelfCheckinBase"
 
+import AppSettingsContext from '../../context/AppSettingsContext'
+
 import HasPermissionWrapper from "../../HasPermissionWrapper"
-// import { GET_ORGANIZATION_LOCATIONS_QUERY } from "./queries"
+import { GET_LOCATION_CLASSES_QUERY } from "./queries"
 
 
 function LocationClasses({ t, match, history }) {
   const locationId = match.params.location_id
+  const appSettings = useContext(AppSettingsContext)
+  const dateFormat = appSettings.dateFormat
+  const timeFormat = appSettings.timeFormatMoment
+  const today = moment().format('YYYY-MM-DD')
 
-  // const { loading, error, data } = useQuery(GET_ORGANIZATION_LOCATIONS_QUERY);
+  const { loading, error, data } = useQuery(GET_LOCATION_CLASSES_QUERY, {
+    variables: {
+      dateFrom: today,
+      dateUntil: today,
+      organizationLocation: locationId,
+    }
+  })
 
-  // if (loading) return (
-  //   <SelfCheckinBase>
-  //     {t("general.loading_with_dots")}
-  //   </SelfCheckinBase>
-  // )
-  // if (error) return (
-  //   <SelfCheckinBase>
-  //     {t("selfcheckin.loadings.error_loading")}
-  //   </SelfCheckinBase>
-  // )
+  if (loading) return (
+    <SelfCheckinBase>
+      {t("general.loading_with_dots")}
+    </SelfCheckinBase>
+  )
+  if (error) return (
+    <SelfCheckinBase>
+      {t("selfcheckin.classes.error_loading")}
+    </SelfCheckinBase>
+  )
 
+  console.log(data)
 
   return (
-    <SelfCheckinBase title={t("selfcheckin.location_classes.title")}>
-      Hello world!
-      {/* <h4>{t("selfcheckin.locations.please_select")}</h4>
-        <List.Group>
+    <SelfCheckinBase title={t("selfcheckin.classes.title")}>
+      <Card>
+      <Table>
+        <Table.Header>
+          <Table.Row>
+            <Table.ColHeader>
+              { t("general.time") }
+            </Table.ColHeader>
+            <Table.ColHeader>
+              { t("general.room") }
+            </Table.ColHeader>
+            <Table.ColHeader>
+              { t("general.class") }
+            </Table.ColHeader>
+            <Table.ColHeader>
+              { t("general.teacher") }
+            </Table.ColHeader>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
           {
-            data.organizationLocations.edges.map(({node}) =>
-              <List.GroupItem action>
-                <Link to={"/selfcheckin/location/" + node.id}>
-                  <div>
-                    <span className="pull-right"><Icon name="chevron-right" /></span>
-                    {node.name}
-                  </div>
-                </Link>
-              </List.GroupItem>
-          )}
-        </List.Group>       */}
+            data.scheduleClasses.map(({ date, classes }) => 
+              classes.map((
+                { scheduleItemId, 
+                  frequencyType,
+                  date, 
+                  status,
+                  description,
+                  account, 
+                  role,
+                  account2,
+                  role2,
+                  organizationLocationRoom, 
+                  organizationClasstype, 
+                  organizationLevel,
+                  timeStart, 
+                  timeEnd,
+                  displayPublic }) => (
+                    // console.log(scheduleItemId)
+                    <Table.Row>
+                      <Table.Col>
+                        {moment(date + ' ' + timeStart).format(timeFormat)} {' - '}
+                        {moment(date + ' ' + timeEnd).format(timeFormat)}
+                      </Table.Col>
+                      <Table.Col>
+                        { organizationLocationRoom.name }
+                      </Table.Col>
+                      <Table.Col>
+                        { organizationClasstype.name }
+                      </Table.Col>
+                      <Table.Col>
+                        {/* Teacher(s) */}
+                        { (account) ? account.fullName : 
+                          <span className="text-red">{t("schedule.classes.no_teacher")}</span>
+                        }
+                      </Table.Col>
+                    </Table.Row>
+                  )
+                )
+            )
+          }
+        </Table.Body>
+      </Table>
+      </Card>
     </SelfCheckinBase>
   )
 }
---
+++ app/cs_admin/src/components/selfcheckin/LocationClasses/queries.js
@@ -0,0 +1,59 @@
+import gql from "graphql-tag"
+
+export const GET_LOCATION_CLASSES_QUERY = gql`
+  query ScheduleClasses(
+      $dateFrom: Date!, 
+      $dateUntil:Date!, 
+      $orderBy: String, 
+      $organizationClasstype: String,
+      $organizationLevel: String,
+      $organizationLocation: String
+    ){
+    scheduleClasses(
+        dateFrom:$dateFrom, 
+        dateUntil: $dateUntil, 
+        orderBy: $orderBy, 
+        organizationClasstype: $organizationClasstype,
+        organizationLevel: $organizationLevel,
+        organizationLocation: $organizationLocation
+    ){
+      date
+      classes {
+        scheduleItemId
+        frequencyType
+        date
+        status
+        description
+        account {
+          id
+          fullName
+        }
+        role
+        account2 {
+          id
+          fullName
+        }
+        role2
+        organizationLocationRoom {
+          id
+          name
+          organizationLocation {
+            id
+            name
+          }
+        }
+        organizationClasstype {
+          id
+          name
+        }
+        organizationLevel {
+          id
+          name
+        }
+        timeStart
+        timeEnd
+        displayPublic
+      }
+    }
+  }
+`
