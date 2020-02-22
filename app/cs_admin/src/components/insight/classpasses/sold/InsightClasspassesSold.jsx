b950f0c8cec654ac35f98758626993f1056ef0d5
Edwin van de Ven <edwin@simplycode.nl>
2020-02-22 12:59:19 +0100

    Add example graph

--- app/cs_admin/src/components/insight/classpasses/sold/InsightClasspassesSold.jsx
+++ app/cs_admin/src/components/insight/classpasses/sold/InsightClasspassesSold.jsx
@@ -5,12 +5,14 @@ import { v4 } from "uuid"
 import { withTranslation } from 'react-i18next'
 import { withRouter } from "react-router"
 import { Link } from 'react-router-dom'
+import C3Chart from "react-c3js"
 
 import AppSettingsContext from '../../../context/AppSettingsContext'
 
 import {
   Badge,
   Dropdown,
+  colors,
   Page,
   Grid,
   Icon,
@@ -42,7 +44,96 @@ function InsightClasspassesSold ({ t, history }) {
             <Grid.Col md={9}>
               <Card title={t('insight.classpasses.sold.title')}>
                 <Card.Body>
-                  content
+                  <C3Chart
+                    style={{ height: "14rem" }}
+                    data={{
+                      columns: [
+                        // each columns data
+                        [
+                          "data1",
+                          0,
+                          5,
+                          1,
+                          2,
+                          7,
+                          5,
+                          6,
+                          8,
+                          24,
+                          7,
+                          12,
+                          5,
+                          6,
+                          3,
+                          2,
+                          2,
+                          6,
+                          30,
+                          10,
+                          10,
+                          15,
+                          14,
+                          47,
+                          65,
+                          55,
+                        ],
+                      ],
+                      type: "area", // default type of chart
+                      groups: [["data1"]],
+                      colors: {
+                        data1: colors["blue"],
+                      },
+                      names: {
+                        // name of each serie
+                        data1: t("general.purchases"),
+                      },
+                      
+                    }}
+                    axis={{
+                      y: {
+                        padding: {
+                          bottom: 0,
+                        },
+                        show: true,
+                        // inner: true,
+                        // tick: {
+                        //   outer: true,
+                        // },
+                      },
+                      x: {
+                        // padding: {
+                        //   left: 0,
+                        //   right: 0,
+                        // },
+                        show: true,
+                      },
+                    }}
+                    // legend={{
+                    //   position: "inset",
+                    //   padding: 0,
+                    //   inset: {
+                    //     anchor: "top-left",
+                    //     x: 0,
+                    //     y: 8,
+                    //     step: 10,
+                    //   },
+                    // }}
+                    tooltip={{
+                      format: {
+                        title: function(x) {
+                          return "";
+                        },
+                      },
+                    }}
+                    padding={{
+                      bottom: 0,
+                      // left: -1,
+                      right: -1,
+                    }}
+                    point={{
+                      show: false,
+                    }}
+                  />
                 </Card.Body>
               </Card>
             </Grid.Col>
