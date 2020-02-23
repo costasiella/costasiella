1abd1426f7848667d926978cb7068b45c8657a31
Edwin van de Ven <edwin@simplycode.nl>
2020-02-23 12:44:45 +0100

    Add Current query

--- app/cs_admin/src/components/insight/classpasses/queries.js
+++ app/cs_admin/src/components/insight/classpasses/queries.js
@@ -1,8 +1,20 @@
 import gql from "graphql-tag"
 
+
 export const GET_CLASSPASSES_SOLD_QUERY = gql`
   query InsightAccountClasspassesSold($year: Int!) {
     insightAccountClasspassesSold(year: $year) {
+      description
+      data
+      year
+    }
+  }
+`
+
+
+export const GET_CLASSPASSES_CURRENT_QUERY = gql`
+  query InsightAccountClasspassesCurrent($year: Int!) {
+    insightAccountClasspassesCurrent(year: $year) {
       description
       data
       year
