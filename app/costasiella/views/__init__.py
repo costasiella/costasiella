42487ef0e5f18a6402d46b3c604aa9d899d8e1d2
Edwin van de Ven <edwin@simplycode.nl>
2020-02-23 13:02:37 +0100

    Backend export active passes

--- app/costasiella/views/__init__.py
+++ app/costasiella/views/__init__.py
@@ -1,4 +1,8 @@
+# Document exports
 from .organization_document import privacy_policy, terms_and_conditions
+# Invoice exports
 from .export_finance_invoice import invoice_pdf, invoice_pdf_preview
 
+# Insight exports
+from .insight.export_insight_classpasses_active import export_excel_insight_classpasses_active
 from .insight.export_insight_classpasses_sold import export_excel_insight_classpasses_sold
