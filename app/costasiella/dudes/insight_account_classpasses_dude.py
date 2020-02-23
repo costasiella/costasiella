9b68f92e31b39a2e4434e617dc148fe92d80aa6b
Edwin van de Ven <edwin@simplycode.nl>
2020-02-23 12:39:27 +0100

    Add current passes to cp insight dude

--- app/costasiella/dudes/insight_account_classpasses_dude.py
+++ app/costasiella/dudes/insight_account_classpasses_dude.py
@@ -75,3 +75,74 @@ class InsightAccountClasspassesDude:
             # print(data)
 
         return data
+
+
+    def get_classpasses_current_period_count(self, date_from, date_until):
+        """
+        Return count of current classpasses in a period
+        """
+        from ..models import AccountClasspass
+
+        count = AccountClasspass.objects.filter(
+            date_start__lte = date_until,
+            date_end__gte = date_from
+        ).count()
+
+        print(count)
+        return count
+
+
+    def get_classpasses_current_year_summary_count(self, year):
+        """
+        Return monthly counts of current passes
+        """
+        data = []
+
+        print(year)
+
+        for i in range(1, 13):
+            date_from = datetime.date(year, i, 1)
+            last_day_month = calendar.monthrange(year, i)[1]
+            date_until = datetime.date(year, i, last_day_month)
+
+            sold_in_month = self.get_classpasses_current_period_count(date_from, date_until)
+            data.append(sold_in_month)
+
+        return data
+
+
+    def get_classpasses_current_period_data(self, date_from, date_until):
+        """
+        Return data list of current classpasses in a period
+        """
+        from ..models import AccountClasspass
+
+        qs = AccountClasspass.objects.filter(
+            date_start__lte = date_until,
+            date_end__gte = date_from
+        ).order_by("date_start")
+
+        return qs
+
+
+    def get_classpasses_current_year_data(self, year):
+        """
+        Get classpasses current year data
+        """
+        from collections import OrderedDict
+
+        data = OrderedDict()
+
+        print(year)
+
+        for i in range(1, 13):
+            date_from = datetime.date(year, i, 1)
+            last_day_month = calendar.monthrange(year, i)[1]
+            date_until = datetime.date(year, i, last_day_month)
+
+            classpasses_current_in_month = self.get_classpasses_current_period_data(date_from, date_until)
+            data[i] = classpasses_current_in_month
+
+            # print(data)
+
+        return data
