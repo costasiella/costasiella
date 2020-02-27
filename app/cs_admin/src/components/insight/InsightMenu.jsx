1637b64e59e122f38f167ff9573763705cd4fd3a
Edwin van de Ven <edwin@simplycode.nl>
2020-02-27 20:31:58 +0100

    Add subscriptions to insight menu

--- app/cs_admin/src/components/insight/InsightMenu.jsx
+++ app/cs_admin/src/components/insight/InsightMenu.jsx
@@ -11,11 +11,13 @@ import HasPermissionWrapper from "../HasPermissionWrapper"
 
 
 let classpasses_active
+let subscriptions_active
 
 
 const InsightMenu = ({ t, active_link }) => (
     <List.Group transparent={true}>
         {(active_link === 'classpasses') ? classpasses_active = true: classpasses_active = false}
+        {(active_link === 'subscriptions') ? subscriptions_active = true: subscriptions_active = false}
         
         <HasPermissionWrapper 
             permission="view"
@@ -29,6 +31,20 @@ const InsightMenu = ({ t, active_link }) => (
               active={classpasses_active}
               >
               {t('insight.classpass.title')}
+          </List.GroupItem>
+        </HasPermissionWrapper>
+        <HasPermissionWrapper 
+            permission="view"
+            resource="insight"
+        >
+          <List.GroupItem
+              key={v4()}
+              className="d-flex align-items-center"
+              to="#/insight/subscriptions"
+              icon="edit"
+              active={subscriptions_active}
+              >
+              {t('insight.subscriptions.title')}
           </List.GroupItem>
         </HasPermissionWrapper>
         {/* <HasPermissionWrapper 
