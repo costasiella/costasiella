c4d129ce46814ac11dc716ccd54c200010f401c3
Edwin van de Ven <edwin@simplycode.nl>
2020-03-28 20:53:25 +0100

    Add back button on classpass details page

--- app/cs_admin/src/components/shop/ShopBase.jsx
+++ app/cs_admin/src/components/shop/ShopBase.jsx
@@ -1,22 +1,35 @@
 import React from 'react'
 import { withTranslation } from 'react-i18next'
 import { withRouter } from "react-router"
+import { Link } from 'react-router-dom'
 
 import {
   Page,
   Grid,
+  Icon,
   Container,
 } from "tabler-react";
 import SiteWrapperShop from "../SiteWrapperShop"
 
 
-function ShopBase({ t, match, history, children, title }) {
+function ShopBase({ t, match, history, children, title, return_url }) {
 
   return (
     <SiteWrapperShop>
       <div className="my-3 my-md-5">
         <Container>
-          <Page.Header title={title} />
+          <Page.Header title={title}>
+            <div className="page-options d-flex">
+              {/* Back */}
+              {(return_url) ?
+                <Link to={return_url} 
+                      className='btn btn-secondary mr-2'>
+                    <Icon prefix="fe" name="arrow-left" /> {t('general.back')} 
+                </Link>
+                : ""
+              }
+            </div>
+          </Page.Header>
             <Grid.Row>
               <Grid.Col md={12}>
                 { children }
--- app/cs_admin/src/components/shop/classpass/ShopClasspassBase.jsx
+++ app/cs_admin/src/components/shop/classpass/ShopClasspassBase.jsx
@@ -1,21 +1,18 @@
 // @flow
 
-import React, {Component } from 'react'
+import React from 'react'
 import { withTranslation } from 'react-i18next'
 import { withRouter } from "react-router"
-import { useQuery } from '@apollo/react-hooks'
-import { Link } from 'react-router-dom'
 
-import {
-  Icon,
-  List
-} from "tabler-react";
 import ShopBase from "../ShopBase"
 
 function ShopClasspassBase({ t, match, history, children }) {
   
   return (
-    <ShopBase title={t("shop.title")}>
+    <ShopBase 
+      title={t("shop.title")}
+      return_url="/shop/classpasses"
+    >
       <h4>{t("shop.classpass.selected")}</h4>
         {children}
     </ShopBase>
