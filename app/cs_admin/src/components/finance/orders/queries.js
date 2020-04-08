4b718ea49af01022e85a5900d8f1bb771ac1973d
Edwin van de Ven <edwin@simplycode.nl>
2020-04-08 18:16:33 +0200

    Add orders query

---
+++ app/cs_admin/src/components/finance/orders/queries.js
@@ -0,0 +1,179 @@
+import gql from "graphql-tag"
+
+export const GET_ORDERS_QUERY = gql`
+  query FinanceOrders($after: String, $before: String, $status: String) {
+    financeOrders(first: 15, before: $before, after: $after, status: $status) {
+      pageInfo {
+        hasNextPage
+        hasPreviousPage
+        startCursor
+        endCursor
+      }
+      edges {
+        node {
+          id
+          account {
+            id
+            fullName
+          }
+          note
+          status
+          total
+          totalDisplay
+          balance
+          balanceDisplay
+        }
+      }
+    }
+  }
+`
+
+export const GET_INVOICE_QUERY = gql`
+  query FinanceInvoice($id: ID!, $before: String, $after: String) {
+    financeInvoice(id:$id) {
+      id
+      account {
+        id
+        fullName
+      }
+      financePaymentMethod {
+        id
+        name
+      }
+      relationCompany
+      relationCompanyRegistration
+      relationCompanyTaxRegistration
+      relationContactName
+      relationAddress
+      relationPostcode
+      relationCity
+      relationCountry
+      status
+      summary
+      invoiceNumber
+      dateSent
+      dateDue
+      terms
+      footer
+      note
+      subtotalDisplay
+      taxDisplay
+      total
+      totalDisplay
+      paidDisplay
+      balance
+      balanceDisplay
+      updatedAt
+      items {
+        edges {
+          node {
+            id
+            lineNumber
+            productName
+            description
+            quantity
+            price
+            financeTaxRate {
+              id
+              name
+              percentage
+              rateType
+            }
+            subtotal
+            subtotalDisplay
+            tax
+            taxDisplay
+            total
+            totalDisplay
+            financeGlaccount {
+              id
+              name
+            }
+            financeCostcenter {
+              id
+              name
+            }
+          }
+        }
+      }
+      payments {
+        pageInfo {
+          hasNextPage
+          hasPreviousPage
+          startCursor
+          endCursor
+        }
+        edges {
+          node {
+            id
+            amount
+            amountDisplay
+            date
+            financePaymentMethod {
+              id
+              name
+            }
+            note
+          }
+        }
+      }
+    }
+    financePaymentMethods(first: 100, before: $before, after: $after, archived: false) {
+      pageInfo {
+        startCursor
+        endCursor
+        hasNextPage
+        hasPreviousPage
+      }
+      edges {
+        node {
+          id
+          name
+        }
+      }
+    }
+    financeTaxRates(first: 100, before: $before, after: $after, archived: false) {
+      pageInfo {
+        startCursor
+        endCursor
+        hasNextPage
+        hasPreviousPage
+      }
+      edges {
+        node {
+          id
+          name
+        }
+      }
+    }
+    organization(id:"T3JnYW5pemF0aW9uTm9kZToxMDA=") {
+      id
+      name
+      address
+      phone
+      email
+      registration
+      taxRegistration
+    }
+  }
+`
+
+
+export const UPDATE_INVOICE = gql`
+  mutation UpdateFinanceInvoice($input: UpdateFinanceInvoiceInput!) {
+    updateFinanceInvoice(input: $input) {
+      financeInvoice {
+        id
+        summary
+      }
+    }
+  }
+`
+
+export const DELETE_FINANCE_INVOICE = gql`
+  mutation DeleteFinanceInvoice($input: DeleteFinanceInvoiceInput!) {
+    deleteFinanceInvoice(input: $input) {
+      ok
+    }
+  }
+`
