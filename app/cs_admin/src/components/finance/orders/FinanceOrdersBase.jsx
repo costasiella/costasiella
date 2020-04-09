4bea08e698da6ff109eca8221b4d00ec9c00fade
Edwin van de Ven <edwin@simplycode.nl>
2020-04-09 17:14:57 +0200

    Complete orders base component

--- app/cs_admin/src/components/finance/orders/FinanceOrdersBase.jsx
+++ app/cs_admin/src/components/finance/orders/FinanceOrdersBase.jsx
@@ -14,17 +14,27 @@ import FinanceMenu from "../FinanceMenu"
 
 
 const FinanceOrdersBase = ({ t, history, children, refetch }) => (
-  <Grid.Row>
-    <Grid.Col md={9}>
-      {children}
-    </Grid.Col>
-    <Grid.Col md={3}>
-      <h5 className="mt-2 pt-1">{t("general.filter")}</h5>
-      <FinanceOrdersFilter refetch={refetch}/>
-      <h5>{t("general.menu")}</h5>
-      <FinanceMenu active_link='invoices'/>
-    </Grid.Col>
-  </Grid.Row>
+  <SiteWrapper>
+    <div className="my-3 my-md-5">
+      <Container>
+        <Page.Header title={t("finance.title")}>
+          <div className="page-options d-flex">
+          </div>
+        </Page.Header>
+        <Grid.Row>
+          <Grid.Col md={9}>
+            {children}
+          </Grid.Col>
+          <Grid.Col md={3}>
+            <h5 className="mt-2 pt-1">{t("general.filter")}</h5>
+            <FinanceOrdersFilter refetch={refetch}/>
+            <h5>{t("general.menu")}</h5>
+            <FinanceMenu active_link='invoices'/>
+          </Grid.Col>
+        </Grid.Row>
+      </Container>
+    </div>
+  </SiteWrapper>
 )
 
 export default withTranslation()(withRouter(FinanceOrdersBase))
