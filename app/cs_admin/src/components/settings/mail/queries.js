f77bc408bd70d4546df8e6d6b867d2320ccc04bf
Edwin van de Ven <edwin@simplycode.nl>
2020-04-21 17:42:05 +0200

    Add get templates query

---
+++ app/cs_admin/src/components/settings/mail/queries.js
@@ -0,0 +1,159 @@
+import gql from "graphql-tag"
+
+export const GET_SYSTEM_MAIL_TEMPLATES_QUERY = gql`
+  query SystemMailTemplates {
+    systemMailTemplates(first:100) {
+      pageInfo {
+        hasNextPage
+        hasPreviousPage
+        startCursor
+        endCursor
+      }
+      edges {
+        node {
+          id
+          subject
+          title
+          description
+          content
+          comments
+        }
+      }
+    }
+  }
+`
+
+export const GET_SYSTEM_MAIL_TEMPLATE_QUERY = gql`
+  query OrganizationSubscription($id: ID!, $after: String, $before: String, $archived: Boolean!) {
+    organizationSubscription(id:$id) {
+      id
+      archived
+      displayPublic
+      displayShop
+      name
+      description
+      sortOrder
+      minDuration
+      classes
+      subscriptionUnit
+      subscriptionUnitDisplay
+      reconciliationClasses
+      creditValidity
+      unlimited
+      termsAndConditions
+      registrationFee
+      organizationMembership {
+        id
+        name
+      }
+      quickStatsAmount
+      financeGlaccount {
+        id 
+        name
+      }
+      financeCostcenter {
+        id
+        name
+      }
+    }
+    organizationMemberships(first: 15, before: $before, after: $after, archived: $archived) {
+      pageInfo {
+        startCursor
+        endCursor
+        hasNextPage
+        hasPreviousPage
+      }
+      edges {
+        node {
+          id
+          archived
+          name
+        }
+      }
+    }
+    financeGlaccounts(first: 15, before: $before, after: $after, archived: $archived) {
+      pageInfo {
+        startCursor
+        endCursor
+        hasNextPage
+        hasPreviousPage
+      }
+      edges {
+        node {
+          id
+          archived
+          name
+          code
+        }
+      }
+    }
+    financeCostcenters(first: 15, before: $before, after: $after, archived: $archived) {
+      pageInfo {
+        startCursor
+        endCursor
+        hasNextPage
+        hasPreviousPage
+      }
+      edges {
+        node {
+          id
+          archived
+          name
+          code
+        }
+      }
+    }
+  }
+`
+
+export const GET_INPUT_VALUES_QUERY = gql`
+  query SubscriptionInputValues($after: String, $before: String, $archived: Boolean) {
+    organizationMemberships(first: 15, before: $before, after: $after, archived: $archived) {
+      pageInfo {
+        startCursor
+        endCursor
+        hasNextPage
+        hasPreviousPage
+      }
+      edges {
+        node {
+          id
+          archived
+          name
+        }
+      }
+    }
+    financeGlaccounts(first: 15, before: $before, after: $after, archived: $archived) {
+      pageInfo {
+        startCursor
+        endCursor
+        hasNextPage
+        hasPreviousPage
+      }
+      edges {
+        node {
+          id
+          archived
+          name
+          code
+        }
+      }
+    }
+    financeCostcenters(first: 15, before: $before, after: $after, archived: $archived) {
+      pageInfo {
+        startCursor
+        endCursor
+        hasNextPage
+        hasPreviousPage
+      }
+      edges {
+        node {
+          id
+          archived
+          name
+          code
+        }
+      }
+    }
+  }
+`
