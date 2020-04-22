378908c5eba3558611fc5f217bf947f27d2c9a0e
Edwin van de Ven <edwin@simplycode.nl>
2020-04-22 17:02:00 +0200

    Add get node query to queries

--- app/cs_admin/src/components/settings/mail/queries.js
+++ app/cs_admin/src/components/settings/mail/queries.js
@@ -25,136 +25,15 @@ export const GET_SYSTEM_MAIL_TEMPLATES_QUERY = gql`
 `
 
 export const GET_SYSTEM_MAIL_TEMPLATE_QUERY = gql`
-  query OrganizationSubscription($id: ID!, $after: String, $before: String, $archived: Boolean!) {
-    organizationSubscription(id:$id) {
+  query SystemMailTemplate($id: ID!) {
+    systemMailTemplate(id:$id) {
       id
-      archived
-      displayPublic
-      displayShop
       name
+      subject
+      title
       description
-      sortOrder
-      minDuration
-      classes
-      subscriptionUnit
-      subscriptionUnitDisplay
-      reconciliationClasses
-      creditValidity
-      unlimited
-      termsAndConditions
-      registrationFee
-      organizationMembership {
-        id
-        name
-      }
-      quickStatsAmount
-      financeGlaccount {
-        id 
-        name
-      }
-      financeCostcenter {
-        id
-        name
-      }
-    }
-    organizationMemberships(first: 15, before: $before, after: $after, archived: $archived) {
-      pageInfo {
-        startCursor
-        endCursor
-        hasNextPage
-        hasPreviousPage
-      }
-      edges {
-        node {
-          id
-          archived
-          name
-        }
-      }
-    }
-    financeGlaccounts(first: 15, before: $before, after: $after, archived: $archived) {
-      pageInfo {
-        startCursor
-        endCursor
-        hasNextPage
-        hasPreviousPage
-      }
-      edges {
-        node {
-          id
-          archived
-          name
-          code
-        }
-      }
-    }
-    financeCostcenters(first: 15, before: $before, after: $after, archived: $archived) {
-      pageInfo {
-        startCursor
-        endCursor
-        hasNextPage
-        hasPreviousPage
-      }
-      edges {
-        node {
-          id
-          archived
-          name
-          code
-        }
-      }
-    }
-  }
-`
-
-export const GET_INPUT_VALUES_QUERY = gql`
-  query SubscriptionInputValues($after: String, $before: String, $archived: Boolean) {
-    organizationMemberships(first: 15, before: $before, after: $after, archived: $archived) {
-      pageInfo {
-        startCursor
-        endCursor
-        hasNextPage
-        hasPreviousPage
-      }
-      edges {
-        node {
-          id
-          archived
-          name
-        }
-      }
-    }
-    financeGlaccounts(first: 15, before: $before, after: $after, archived: $archived) {
-      pageInfo {
-        startCursor
-        endCursor
-        hasNextPage
-        hasPreviousPage
-      }
-      edges {
-        node {
-          id
-          archived
-          name
-          code
-        }
-      }
-    }
-    financeCostcenters(first: 15, before: $before, after: $after, archived: $archived) {
-      pageInfo {
-        startCursor
-        endCursor
-        hasNextPage
-        hasPreviousPage
-      }
-      edges {
-        node {
-          id
-          archived
-          name
-          code
-        }
-      }
+      content
+      comments
     }
   }
 `
