5814e5bf47679400421724cb38fc28f5a7751c2b
Edwin van de Ven <edwin@simplycode.nl>
2020-04-24 14:58:14 +0200

    Use system setting for currency symbol in display function
    for all schema's

--- app/costasiella/modules/finance_tools.py
+++ app/costasiella/modules/finance_tools.py
@@ -1,6 +1,13 @@
 
 
 def display_float_as_amount(amount):
-    from constance import config
+    from ..models import SystemSetting
 
-    return ' '.join([config.CURRENCY_SYMBOL, format(amount, '.2f')])
+    setting = SystemSetting.objects.get(setting="finance_currency_symbol")
+    if setting:
+        currency_symbol = setting.value
+    else:
+        currency_symbol = "€"
+
+    return ' '.join([currency_symbol, format(amount, '.2f')])
+
