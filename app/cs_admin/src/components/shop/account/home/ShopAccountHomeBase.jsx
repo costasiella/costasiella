14c1843a28317d7558a262fe57c083b4c699967e
Edwin van de Ven <edwin@simplycode.nl>
2020-05-03 15:01:30 +0200

    Add subTitle to account home base

--- app/cs_admin/src/components/shop/account/home/ShopAccountHomeBase.jsx
+++ app/cs_admin/src/components/shop/account/home/ShopAccountHomeBase.jsx
@@ -13,12 +13,12 @@ import {
 import SiteWrapperShop from "../../../SiteWrapperShop"
 
 
-function ShopAccountHomeBase({ t, match, history, children }) {
+function ShopAccountHomeBase({ t, match, history, children, subTitle="" }) {
   return (
     <SiteWrapperShop>
       <div className="my-3 my-md-5">
         <Container>
-          <Page.Header title={t("shop.account.title")} />
+          <Page.Header title={t("shop.account.title")} subTitle={subTitle}/>
           { children }
         </Container>
       </div>
