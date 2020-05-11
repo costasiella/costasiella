27e0c1ff70bbd9783cc46b4ffee77a69a17bf04b
Edwin van de Ven <edwin@simplycode.nl>
2020-05-11 13:58:06 +0200

    Add FinanceOrderEdit component

---
+++ app/cs_admin/src/components/finance/orders/edit/FinanceOrderEdit.jsx
@@ -0,0 +1,163 @@
+// @flow
+
+import React, { useContext } from 'react'
+
+import { useQuery, useMutation } from "react-apollo";
+import { withTranslation } from 'react-i18next'
+import { withRouter } from "react-router"
+import { Link } from 'react-router-dom'
+import { Formik } from 'formik'
+import { toast } from 'react-toastify'
+import moment from 'moment'
+
+import { GET_ORDERS_QUERY } from '../queries'
+import { GET_FINANCE_ORDER_QUERY, UPDATE_ORDER } from './queries'
+import FinanceOrderEditForm from "./FinanceOrderEditForm"
+import FinanceOrderEditBase from './FinanceOrderEditBase'
+
+import {
+  Page,
+  Grid,
+  Icon,
+  Button,
+  Card,
+  Container,
+  Table,
+} from "tabler-react";
+import SiteWrapper from "../../../SiteWrapper"
+import { dateToLocalISO } from '../../../../tools/date_tools'
+import FormPrice from '../../invoices/edit/FormPrice';
+import AppSettingsContext from '../../../context/AppSettingsContext'
+
+
+function FinanceOrderEdit({t, match, history}) {
+  const appSettings = useContext(AppSettingsContext)
+  const dateFormat = appSettings.dateFormat
+  const timeFormat = appSettings.timeFormatMoment
+  const dateTimeFormat = dateFormat + ' ' + timeFormat
+
+  const id = match.params.id
+  const returnUrl = "/finance/orders"
+  
+  const { loading, error, data } = useQuery(GET_FINANCE_ORDER_QUERY, {
+    variables: { id: id },
+  })
+  const [updateStatus] = useMutation(UPDATE_ORDER)
+
+
+  // Loading
+  if (loading) return (
+    <FinanceOrderEditBase>
+      <p>{t('general.loading_with_dots')}</p>
+    </FinanceOrderEditBase>
+  )
+  // Error
+  if (error) {
+    console.log(error)
+    return (
+      <FinanceOrderEditBase>
+        <p>{t('general.error_sad_smiley')}</p>
+      </FinanceOrderEditBase>
+    )
+  }
+
+  console.log(data) 
+  const order = data.financeOrder
+  
+
+
+
+  return(
+    <FinanceOrderEditBase>
+      {/* Start info row */}
+      <Grid.Row>
+        <Grid.Col md={6}>
+          <Card title={t("finance.orders.edit_details")}>
+            <Table cards>
+              <Table.Row>
+                <Table.ColHeader>{t("general.account")}</Table.ColHeader>
+                <Table.Col>{order.account.fullName}</Table.Col>
+              </Table.Row>
+              <Table.Row>
+                <Table.ColHeader>{t("finance.orders.placed_at")}</Table.ColHeader>
+                <Table.Col>{moment(order.createdAt).format(dateTimeFormat)}</Table.Col>
+              </Table.Row>
+            </Table>
+          </Card>
+
+        </Grid.Col>
+        <Grid.Col md={6}>
+          <Formik
+            initialValues={{ 
+              status: order.status, 
+            }}
+            // validationSchema={GLACCOUNT_SCHEMA}
+            onSubmit={(values, { setSubmitting }) => {
+                console.log('submit values:')
+                console.log(values)
+
+                UPDATE_ORDER({ variables: {
+                  input: {
+                    id: match.params.id,
+                    status: values.status
+                  }
+                }, refetchQueries: [
+                    {query: GET_ORDERS_QUERY }
+                ]})
+                .then(({ data }) => {
+                    console.log('got data', data)
+                    toast.success((t('finance.orders.toast_edit_success')), {
+                        position: toast.POSITION.BOTTOM_RIGHT
+                      })
+                  }).catch((error) => {
+                    toast.error((t('general.toast_server_error')) + ': ' +  error, {
+                        position: toast.POSITION.BOTTOM_RIGHT
+                      })
+                    console.log('there was an error sending the query', error)
+                    setSubmitting(false)
+                  })
+            }}
+            >
+            {({ isSubmitting, errors, values }) => (
+              <FinanceOrderEditForm
+                isSubmitting={isSubmitting}
+                errors={errors}
+                values={values}
+                returnUrl={returnUrl}
+              />
+            )}
+          </Formik>
+        </Grid.Col>
+      </Grid.Row> 
+      {/* End information row */}
+      {/* Start items row */}
+      <Grid.Row>
+        <Grid.Col md={12}>
+          <Card title={t("general.items")}>
+            <Table cards>
+              <Table.Header>
+                <Table.Row>
+                  <Table.ColHeader>{t("general.product")}</Table.ColHeader>
+                  <Table.ColHeader>{t("general.description")}</Table.ColHeader>
+                  <Table.ColHeader>{t("general.quantity_short_and_price")}</Table.ColHeader>
+                  <Table.ColHeader>{t("general.tax")}</Table.ColHeader>
+                  <Table.ColHeader>{t("general.total")}</Table.ColHeader>
+                  <Table.ColHeader></Table.ColHeader>
+                </Table.Row>
+              </Table.Header>
+              <Table.Body>
+                
+              </Table.Body>                
+            </Table>
+          </Card>
+        </Grid.Col>
+      </Grid.Row>
+      {/* End items row */}
+      {/* Start note row */}
+      {/* End note row */}
+    </FinanceOrderEditBase>
+  )
+}
+
+
+export default withTranslation()(withRouter(FinanceOrderEdit))
