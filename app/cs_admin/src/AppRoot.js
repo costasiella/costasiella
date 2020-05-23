2a973cfe2aeadd5e3fd175951c67c207794373f3
Edwin van de Ven <edwin@simplycode.nl>
2020-05-23 12:09:51 +0200

    Add route for shop class book

--- app/cs_admin/src/AppRoot.js
+++ app/cs_admin/src/AppRoot.js
@@ -157,6 +157,7 @@ import ShopAccountHome from './components/shop/account/home/ShopAccountHome'
 import ShopAccountClasspasses from './components/shop/account/classpasses/ShopAccountClasspasses'
 import ShopAccountOrders from './components/shop/account/orders/ShopAccountOrders'
 import ShopAccountProfile from './components/shop/account/profile/ShopAccountProfile'
+import ShopClassBook from './components/shop/classes/book/ShopClassBook'
 import ShopClassesSchedule from './components/shop/classes/schedule/ShopClassesSchedule'
 import ShopHome from './components/shop/home/ShopHome'
 import ShopCheckoutPayment from './components/shop/checkout/payment/ShopCheckoutPayment'
@@ -366,6 +367,7 @@ function AppRoot({ t }) {
           <PrivateRoute exact path = "/shop/checkout/payment/:id" component={ShopCheckoutPayment} />
           <PrivateRoute exact path = "/shop/checkout/complete/:id" component={ShopCheckoutComplete} />
           <Route exact path = "/shop/classes" component={ShopClassesSchedule} />
+          <Route exact path = "/shop/class/book/:class_id/:date" component={ShopClassBook} />
           <Route exact path = "/shop/classpasses" component={ShopClasspasses} />
           <Route exact path = "/shop/classpass/:id" component={ShopClasspass} />
 
