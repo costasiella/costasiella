c19bf453232c9a9066e94f70d2566a9c9d991ce2
Edwin van de Ven <edwin@simplycode.nl>
2020-05-23 12:03:49 +0200

    Code clean up in ShopClassBookBack component

--- app/cs_admin/src/components/shop/classes/book/ShopClassBookBack.jsx
+++ app/cs_admin/src/components/shop/classes/book/ShopClassBookBack.jsx
@@ -12,7 +12,7 @@ import {
 } from "tabler-react"
 
 
-const ShopClassBookBack = ({ t, classId, date }) => (
+const ShopClassBookBack = ({ t }) => (
   <Link to={"/shop/classes"} >
     <Button color="primary btn-block mb-6">
       <Icon prefix="fe" name="chevrons-left" /> {t('general.back')}
