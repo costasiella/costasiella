c1afddb796e45f1e60154facc84e4f0cdcea9994
Edwin van de Ven <edwin@simplycode.nl>
2020-05-29 13:27:18 +0200

    Add ShopAccountClassesBase component

---
+++ app/cs_admin/src/components/shop/account/classes/ShopAccountClassesBase.jsx
@@ -0,0 +1,27 @@
+// @flow
+
+import React from 'react'
+import { withTranslation } from 'react-i18next'
+import { withRouter } from "react-router"
+
+import {
+  Page,
+  Container
+} from "tabler-react";
+import SiteWrapperShop from "../../../SiteWrapperShop"
+
+
+function ShopAccountClassesBase({ t, match, history, children, accountName="" }) {
+  return (
+      <SiteWrapperShop>
+        <div className="my-3 my-md-5">
+          <Container>
+            <Page.Header title={t("shop.account.title")} subTitle={ accountName }/>
+            { children }
+          </Container>
+        </div>
+      </SiteWrapperShop>
+  )
+}
+
+export default withTranslation()(withRouter(ShopAccountClassesBase))
