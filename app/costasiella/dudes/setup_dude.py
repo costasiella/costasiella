ff67540c53b06a1d2beb45ec23da1b2699e45fc8
Edwin van de Ven <edwin@simplycode.nl>
2020-06-03 17:58:37 +0200

    Add SetupDude

---
+++ app/costasiella/dudes/setup_dude.py
@@ -0,0 +1,37 @@
+from django.utils.translation import gettext as _
+
+from ..models import SystemSetting
+from .version_dude import VersionDude
+
+
+class SetupDude:
+    def __init__(self):
+        """
+        Fetch current version from SysSettings
+        """
+        self.setting_setup_complete = "system_setup_complete"
+
+        try:
+            self.setting_setup_complete_obj = SystemSetting.objects.get(setting=self.setting_setup_complete)
+        except SystemSetting.DoesNotExist:
+            self.setting_setup_complete_obj = SystemSetting.objects.create(
+                setting=self.setting_setup_complete,
+                value="F"
+            )
+
+        self.complete = self.setting_setup_complete_obj.value
+
+    def setup(self):
+        """
+        Set the current version
+        :return:
+        """
+        if self.complete == "T":
+            return _("Setup already executed... setup will not be executed again")
+        # Set current version
+        version_dude = VersionDude()
+
+        self.setting_setup_complete_obj.value = "T"
+        self.setting_setup_complete_obj.save()
+
+        return _("Setup complete!")
