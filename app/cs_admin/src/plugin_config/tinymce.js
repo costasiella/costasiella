918ff1bbf8a6f1b7830c9b6537f3999cc0f66891
Edwin van de Ven <edwin@simplycode.nl>
2020-06-16 16:33:36 +0200

    Add full screen button to tinymce config

--- app/cs_admin/src/plugin_config/tinymce.js
+++ app/cs_admin/src/plugin_config/tinymce.js
@@ -1,6 +1,7 @@
 export const tinymceBasicConf = {
+    resize: true,
     menubar: false,
-    plugins: 'link image code lists paste',
+    plugins: 'link image code lists paste fullscreen',
     statusbar: false,
-    toolbar: 'undo redo | formatselect | bold italic strikethrough | bullist numlist | alignleft aligncenter alignright | link image | pastetext code'    
+    toolbar: ' undo redo | formatselect | bold italic strikethrough | bullist numlist | alignleft aligncenter alignright | link image | pastetext code | fullscreen',    
 }
