1e1b060cf38ab84fa9a18ba0d4e7b47a25cc32f1
Edwin van de Ven <edwin@simplycode.nl>
2020-08-11 16:44:12 +0200

    Automation credits base component

---
+++ app/cs_admin/src/components/automation/account/subscription/credits/AutomationAccountSubscriptionCreditsBase.jsx
@@ -0,0 +1,36 @@
+// @flow
+
+import React from 'react'
+import { withTranslation } from 'react-i18next'
+import { withRouter } from "react-router"
+
+
+import {
+  Page,
+  Grid,
+  Container,
+} from "tabler-react";
+import SiteWrapper from "../../SiteWrapper"
+import HasPermissionWrapper from "../../HasPermissionWrapper"
+import AutomationBack from "../../../AutomationBack"
+
+function AutomationAccountSubscriptionCreditsBase({t, history, match}) {
+  return (
+    <SiteWrapper>
+      <div className="my-3 my-md-5">
+        <Container>
+          <Page.Header title={t('automation.title')} >
+            <AutomationBack />
+          </Page.Header>
+          <Grid.Row>
+            <Grid.Col md={12}>
+              {children}
+            </Grid.Col>
+          </Grid.Row>
+        </Container>
+      </div>
+    </SiteWrapper>
+  )
+}
+
+export default withTranslation()(withRouter(AutomationAccountSubscriptionCreditsBase))
