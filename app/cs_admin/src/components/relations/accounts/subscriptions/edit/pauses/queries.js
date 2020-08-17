2740eacb29445a79fe85d0e0b72503dab0f33bf8
Edwin van de Ven <edwin@simplycode.nl>
2020-08-17 17:26:04 +0200

    Add gql query for account subscription pauses

---
+++ app/cs_admin/src/components/relations/accounts/subscriptions/edit/pauses/queries.js
@@ -0,0 +1,26 @@
+import gql from "graphql-tag"
+
+export const GET_ACCOUNT_SUBSCRIPTION_PAUSES_QUERY = gql`
+query AccountSubscriptionPauses($before: String, $after: String, $accountSubscription: ID!) {
+  accountSubscriptionPauses(first: 20, before: $before, after: $after, accountSubscription: $accountSubscription) {
+    pageInfo {
+      hasNextPage
+      hasPreviousPage
+      startCursor
+      endCursor
+    }
+    edges {
+      node {
+        id
+        accountSubscription {
+          id
+        }
+        dateStart
+        dateEnd
+        description
+        createdAt
+      }
+    } 
+  }
+}
+`
