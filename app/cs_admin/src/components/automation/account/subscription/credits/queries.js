83a4fae83be15529d5d05f87de5b0ce4072a050a
Edwin van de Ven <edwin@simplycode.nl>
2020-08-17 16:54:25 +0200

    Placeholder components for automation credits

---
+++ app/cs_admin/src/components/automation/account/subscription/credits/queries.js
@@ -0,0 +1,15 @@
+import gql from "graphql-tag"
+
+export const GET_ACCOUNT_CREDITS_TASK_RESULT_QUERY = gql`
+
+`
+
+// export const GET_LEVEL_QUERY = gql`
+//   query SchoolLevel($id: ID!) {
+//     organizationLevel(id:$id) {
+//       id
+//       name
+//       archived
+//     }
+//   }
+// `
