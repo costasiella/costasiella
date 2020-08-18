293cf8baa782ed453054f41caba6c00eb8596741
Edwin van de Ven <edwin@simplycode.nl>
2020-08-18 17:01:14 +0200

    Hightlight correct tab when adding or editing a block

--- app/cs_admin/src/components/relations/accounts/subscriptions/edit/blocks/AccountSubscriptionEditBlockBase.jsx
+++ app/cs_admin/src/components/relations/accounts/subscriptions/edit/blocks/AccountSubscriptionEditBlockBase.jsx
@@ -8,7 +8,7 @@ import AccountSubscriptionEditBase from "../AccountSubscriptionEditBase"
 
 
 function AccountSubscriptionEditBlockBase({ t, history, match, children}) {
-  const activeTab = "pauses"
+  const activeTab = "blocks"
   const accountId = match.params.account_id
   const subscriptionId = match.params.subscription_id
   const returnUrl = `/relations/accounts/${accountId}/subscriptions/edit/${subscriptionId}/blocks/`
