9544c12aee515a2ed3d15b18309efaf3751a0c85
Edwin van de Ven <edwin@simplycode.nl>
2020-08-19 17:13:35 +0200

    Add subscription credit yup schema

---
+++ app/cs_admin/src/components/relations/accounts/subscriptions/edit/credits/yupSchema.js
@@ -0,0 +1,9 @@
+import { t } from 'i18next'
+import * as Yup from 'yup'
+
+export const ACCOUNT_SUBSCRIPTION_CREDIT_SCHEMA = Yup.object().shape({
+  mutationAmount: Yup.number()
+    .required(t('yup.field_required')),
+  mutationType: Yup.number()
+    .required(t('yup.field_required'))
+  })
