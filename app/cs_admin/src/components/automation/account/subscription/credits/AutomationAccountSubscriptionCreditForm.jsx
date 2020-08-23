9472704e4c2cde31508b6eb4d8c41031c5b23d15
Edwin van de Ven <edwin@simplycode.nl>
2020-08-23 17:36:57 +0200

    Add automation credits add form

---
+++ app/cs_admin/src/components/automation/account/subscription/credits/AutomationAccountSubscriptionCreditForm.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { withTranslation } from 'react-i18next'
+import { withRouter } from "react-router"
+
+import {
+    Button,
+    Card,
+    Form
+  } from "tabler-react"
+  import { Form as FoForm, Field, ErrorMessage } from 'formik'
+
+
+function AutomationAccountSubscriptionCreditForm({ t, history, isSubmitting, errors, returnUrl}) {
+  return (
+    <FoForm>
+      <Card.Body>
+        <Form.Group label={t('general.name')}>
+        <Field type="text" 
+                name="name" 
+                className={(errors.name) ? "form-control is-invalid" : "form-control"} 
+                autoComplete="off" />
+        <ErrorMessage name="name" component="span" className="invalid-feedback" />
+        </Form.Group>
+      </Card.Body>
+      <Card.Footer>
+        <Button 
+         color="primary"
+          className="pull-right" 
+          type="submit" 
+          disabled={isSubmitting}
+          >
+          {t('general.submit')}
+        </Button>
+        <Button color="link" onClick={() => history.push(return_url)}>
+            {t('general.cancel')}
+        </Button>
+      </Card.Footer>
+    </FoForm>
+  )
+
+}
+  
+  
+  export default withTranslation()(withRouter(AutomationAccountSubscriptionCreditForm))
