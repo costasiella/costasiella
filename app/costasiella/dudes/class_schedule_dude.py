7a87d3256bbe8a4da6453e4bd4b791cf0f4b6d80
Edwin van de Ven <edwin@simplycode.nl>
2020-08-29 12:20:14 +0200

    Add schedule_class_with_otc_data to class schedule dude

--- app/costasiella/dudes/class_schedule_dude.py
+++ app/costasiella/dudes/class_schedule_dude.py
@@ -1,7 +1,6 @@
 from django.utils.translation import gettext as _
 
-
-class ClassScheduleDude():
+class ClassScheduleDude:
     def schedule_item_takes_place_on_day(self, schedule_item, date):
         """
         :return: True if the schedule item takes place on given date
@@ -29,3 +28,41 @@ class ClassScheduleDude():
                 return False
 
         return True
+
+    def schedule_class_with_otc_data(self, schedule_item, date):
+        """
+        Fetch a schedule_item and check for otc data. If found, add it to the item.
+        :param schedule_item:
+        :param date:
+        :return:
+        """
+        from ..models import ScheduleItem, ScheduleItemWeeklyOTC
+
+        schedule_item_weekly_otc = None
+        qs = ScheduleItemWeeklyOTC.objects.filter(
+            schedule_item=schedule_item,
+            date=date
+        )
+        if qs.exists:
+            schedule_item_weekly_otc = qs.first()
+
+        if schedule_item_weekly_otc:
+            if schedule_item_weekly_otc.time_start:
+                schedule_item.time_start = schedule_item_weekly_otc.time_start
+
+            if schedule_item_weekly_otc.time_end:
+                schedule_item.time_end = schedule_item_weekly_otc.time_end
+
+            if schedule_item_weekly_otc.info_mail_content:
+                schedule_item.info_mail_content = schedule_item_weekly_otc.info_mail_content
+
+            if schedule_item_weekly_otc.organization_classtype:
+                schedule_item.organization_classtype = schedule_item_weekly_otc.organization_classtype
+
+            if schedule_item_weekly_otc.organization_location_room:
+                schedule_item.organization_location_room = schedule_item_weekly_otc.organization_location_room
+
+            if schedule_item_weekly_otc.organization_level:
+                schedule_item.organization_level = schedule_item_weekly_otc.organization_level
+
+        return schedule_item
