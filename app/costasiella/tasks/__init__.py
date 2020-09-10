d22d490adb69ea18534c3f3a07e535d8eaf9344c
Edwin van de Ven <edwin@simplycode.nl>
2020-09-11 00:57:35 +0200

    Add background task to add invoices

--- app/costasiella/tasks/__init__.py
+++ app/costasiella/tasks/__init__.py
@@ -1,2 +1,3 @@
 from .account.subscription.credits.tasks import account_subscription_credits_add_for_month
+from .account.subscription.invoices.tasks import account_subscription_invoices_add_for_month
 from .demo import add
