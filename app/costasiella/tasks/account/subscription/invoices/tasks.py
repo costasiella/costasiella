798254ec25c66aea96898fb7e1e018dfc49e61e2
Edwin van de Ven <edwin@simplycode.nl>
2020-09-11 20:41:57 +0200

    Start adding task to process recurring mollie payments for subscriptions

--- app/costasiella/tasks/account/subscription/invoices/tasks.py
+++ app/costasiella/tasks/account/subscription/invoices/tasks.py
@@ -79,3 +79,181 @@ def account_subscription_invoices_add_for_month(year, month, description='', inv
     db.commit()
 
     return T("Invoices in month") + ': ' + str(invoices_count)
+
+
+@shared_task
+def account_subscription_invoices_add_for_month_mollie_collection(year, month):
+    """
+
+    :param year:
+    :param month:
+    :return:
+    """
+    #TODO: Add function to send mail for failed payment collection
+    from mollie.api.error import Error as MollieError
+
+    from .....models import AccountSubscription
+    from .....dudes import MollieDude, date_tools_dude
+
+
+    date_tools_dude = DateToolsDude()
+    mollie_dude = MollieDude()
+
+    first_day_month = datetime.date(year, month, 1)
+    last_day_month = date_tools_dude.get_last_day_month(first_day_month)
+
+    # find active subscriptions with payment method mollie (100)
+    qs = AccountSubscription.objects.filter(
+        Q(finance_payment_method=100) &
+        Q(date_start__lte=last_day_month) &
+        (
+            Q(date_end__gte=first_day_month) |
+            Q(date_end__isnull=True)
+        )
+    )
+
+    success = 0
+    failed = 0
+
+    for i, account_subscription in qs:
+        finance_invoice_item = account_subscription.create_invoice_for_month(year, month)
+        # Check if an invoice (item) was created
+        if not finance_invoice_item:
+            continue
+
+        finance_invoice = finance_invoice_item.finance_invoice
+        # Only continue when the invoice status == SENT
+        if not finance_invoice.status == 'SENT':
+            continue
+
+        # Check if the amount of the invoice > 0:
+        if not finance_invoice.total:
+            continue
+
+        # Create mollie recurring payment
+        
+
+
+    ########## OpenStudio example code begin ###############
+    #
+    # def send_mail_failed(cuID):
+    #     """
+    #         When a recurring payment fails, mail customer with request to pay manually
+    #     """
+    #     os_mail = OsMail()
+    #     msgID = os_mail.render_email_template('payment_recurring_failed')
+    #     os_mail.send_and_archive(msgID, cuID)
+    #
+    # from openstudio.os_customer import Customer
+    #
+    # # hostname
+    # sys_hostname = get_sys_property('sys_hostname')
+    # # set up Mollie
+    # mollie = Client()
+    # mollie_api_key = get_sys_property('mollie_website_profile')
+    # mollie.set_api_key(mollie_api_key)
+    # # set dates
+    # today = datetime.date.today()
+    # firstdaythismonth = datetime.date(today.year, today.month, 1)
+    # lastdaythismonth = get_last_day_month(firstdaythismonth)
+    #
+    # # call some function to do stuff
+    #
+    # # find all active subscriptions with payment method 100 (Mollie)
+    # query = (db.customers_subscriptions.payment_methods_id == 100) & \
+    #         (db.customers_subscriptions.Startdate <= lastdaythismonth) & \
+    #         ((db.customers_subscriptions.Enddate >= firstdaythismonth) |
+    #          (db.customers_subscriptions.Enddate == None))
+    # rows = db(query).select(db.customers_subscriptions.ALL)
+    #
+    # success = 0
+    # failed = 0
+    #
+    # # create invoices
+    # for i, row in enumerate(rows):
+    #     cs = CustomerSubscription(row.id)
+    #     # This function returns the invoice id if it already exists
+    #     iID = cs.create_invoice_for_month(TODAY_LOCAL.year, TODAY_LOCAL.month)
+    #
+    #     #print 'invoice created'
+    #     #print iID
+    #
+    #     # Do we have an invoice?
+    #     if not iID:
+    #         continue
+    #
+    #     invoice = Invoice(iID)
+    #     # Only do something if the invoice status is sent
+    #     if not invoice.invoice.Status == 'sent':
+    #         continue
+    #
+    #     # We're good, continue processing
+    #     invoice_amounts = invoice.get_amounts()
+    #     #print invoice.invoice.InvoiceID
+    #     description = invoice.invoice.Description + ' - ' + invoice.invoice.InvoiceID
+    #     db.commit()
+    #
+    #     #create recurring payments using mandates
+    #     #subscription invoice
+    #     customer = Customer(row.auth_customer_id)
+    #     mollie_customer_id = customer.row.mollie_customer_id
+    #     mandates = customer.get_mollie_mandates()
+    #     valid_mandate = False
+    #     # set default recurring type, change to recurring if a valid mandate is found.
+    #     if mandates['count'] > 0:
+    #         # background payment
+    #         for mandate in mandates['_embedded']['mandates']:
+    #             if mandate['status'] == 'valid':
+    #                 valid_mandate = True
+    #                 break
+    #
+    #         if valid_mandate:
+    #             # Create recurring payment
+    #             try:
+    #                 webhook_url = URL('mollie', 'webhook', scheme='https', host=sys_hostname)
+    #                 payment = mollie.payments.create({
+    #                     'amount': {
+    #                         'currency': CURRENCY,
+    #                         'value': str(invoice_amounts.TotalPriceVAT)
+    #                     },
+    #                     'customerId': mollie_customer_id,
+    #                     'sequenceType': 'recurring',  # important
+    #                     'description': description,
+    #                     'webhookUrl': webhook_url,
+    #                     'metadata': {
+    #                         'invoice_id': invoice.invoice.id,
+    #                         'customers_orders_id': 'invoice' # This lets the webhook function know it's dealing with an invoice
+    #                     }
+    #                 })
+    #
+    #                 # link invoice to mollie_payment_id
+    #                 db.invoices_mollie_payment_ids.insert(
+    #                     invoices_id=invoice.invoice.id,
+    #                     mollie_payment_id=payment['id'],
+    #                     RecurringType='recurring',
+    #                     WebhookURL=webhook_url
+    #                 )
+    #
+    #                 success += 1
+    #
+    #             except MollieError as e:
+    #                 print(e)
+    #                 # send mail to ask customer to pay manually
+    #                 send_mail_failed(cs.auth_customer_id)
+    #
+    #                 failed += 1
+    #                 # return error
+    #                 # return 'API call failed: ' + e.message
+    #     else:
+    #         # send mail to ask customer to pay manually
+    #         send_mail_failed(cs.auth_customer_id)
+    #
+    #         failed +=1
+    #
+    # # For scheduled tasks, db has to be committed manually
+    # db.commit()
+    #
+    # return T("Payments collected") + ': ' + str(success) + '<br>' + \
+    #     T("Payments failed to collect") + ': ' + str(failed)
+
+    ############ OpenStudio example code end #######################
