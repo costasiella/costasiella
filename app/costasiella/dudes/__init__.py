d1155e23151854c2cc46e5d32622e9a8e3f7b6cb
Edwin van de Ven <edwin@simplycode.nl>
2020-10-14 13:32:34 +0200

    Import mail dude in dudes init

--- app/costasiella/dudes/__init__.py
+++ app/costasiella/dudes/__init__.py
@@ -8,6 +8,8 @@ from .date_tools_dude import DateToolsDude
 from .insight_account_classpasses_dude import InsightAccountClasspassesDude
 from .insight_account_subscriptions_dude import InsightAccountSubscriptionsDude
 
+from .mail_dude import MailDude
+
 from .mollie_dude import MollieDude
 
 from .sales_dude import SalesDude
