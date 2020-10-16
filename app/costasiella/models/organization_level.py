35d462331452088db05e4213b92923de678f0ef4
Edwin van de Ven <edwin@simplycode.nl>
2020-10-16 12:13:13 +0200

    Code cleanup

--- app/costasiella/models/organization_level.py
+++ app/costasiella/models/organization_level.py
@@ -2,12 +2,11 @@ import uuid
 
 from django.db import models
 
-# Create your models here.
 
+# Create your models here.
 class OrganizationLevel(models.Model):
     archived = models.BooleanField(default=False)
     name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
-    
