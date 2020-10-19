d9b69bc788b871a6a7d89852930d7057126fa389
Edwin van de Ven <edwin@simplycode.nl>
2020-10-19 17:43:01 +0200

    Add sidebar to ScheduleEventsBase component

--- app/cs_admin/src/components/schedule/events/ScheduleEventsBase.jsx
+++ app/cs_admin/src/components/schedule/events/ScheduleEventsBase.jsx
@@ -7,26 +7,61 @@ import {
   Page,
   Container
 } from "tabler-react";
+import HasPermissionWrapper from "../../../HasPermissionWrapper"
 import SiteWrapper from "../../../SiteWrapper"
 // import ShopAccountBack from "../ShopAccountBack"
+import ScheduleMenu from "../ScheduleMenu"
 
 
-function ScheduleEventsBase({ t, match, history, children, accountName="" }) {
+function ScheduleEventsBase({ t, match, history, children, sidebarContent="" }) {
   return (
-      <SiteWrapperShop>
+      <SiteWrapper>
         <div className="my-3 my-md-5">
           <Container>
-            <Page.Header title={t("schedule.events.title")} subTitle={ accountName }>
+            <Page.Header title={t("schedule.events.title")} >
               <div className="page-options d-flex">
                 {/* TODO: Add back button ? */}
                 Back here...
                 {/* <ShopAccountBack /> */}
               </div>
             </Page.Header>
-            { children }
+            <Grid.Row>
+            <Grid.Col md={9}>
+              { children }
+            </Grid.Col>
+            <Grid.Col md={3}>
+              { sidebarContent }
+              {/* <HasPermissionWrapper permission="add"
+                                    resource="scheduleevent">
+                <Button color="primary btn-block mb-1"
+                        onClick={() => history.push("/schedule/events/add")}>
+                  <Icon prefix="fe" name="plus-circle" /> {t('schedule.events.add')}
+                </Button>
+              </HasPermissionWrapper> */}
+                  {/* <div>
+                    <Button
+                      className="pull-right"
+                      color="link"
+                      size="sm"
+                      onClick={() => {
+                        localStorage.setItem(CSLS.SCHEDULE_CLASSES_FILTER_CLASSTYPE, "")
+                        localStorage.setItem(CSLS.SCHEDULE_CLASSES_FILTER_LEVEL, "")
+                        localStorage.setItem(CSLS.SCHEDULE_CLASSES_FILTER_LOCATION, "")
+                        refetch(get_list_query_variables())
+                      }}
+                    >
+                      {t("general.clear")}
+                    </Button>
+                  </div> */}
+                  {/* <h5 className="mt-2 pt-1">{t("general.filter")}</h5>
+                  <ScheduleClassesFilter data={data} refetch={refetch} /> */}
+              <h5>{t("general.menu")}</h5>
+              <ScheduleMenu active_link='events'/>
+            </Grid.Col>
+            </Grid.Row>
           </Container>
         </div>
-      </SiteWrapperShop>
+      </SiteWrapper>
   )
 }
 
