f1b56fa9ac3811243ca5d2bd864e4f65fe8ad061
Edwin van de Ven <edwin@simplycode.nl>
2020-10-22 22:30:58 +0200

    Add schedule event query

---
+++ app/cs_admin/src/components/schedule/events/queries.js
@@ -0,0 +1,67 @@
+import gql from "graphql-tag"
+
+export const GET_EVENTS_QUERY = gql`
+  query ScheduleEvents($before:String, $after:String) {
+    scheduleEvents(first: 100, before: $before, after:$after) {
+      pageInfo {
+        hasNextPage
+        hasPreviousPage
+        startCursor
+        endCursor
+      }
+      edges {
+        node {
+          id
+          archived
+          displayPublic
+          displayShop
+          autoSendInfoMail
+          organizationLocation {
+            id
+            name
+          }
+          name
+          tagline
+          preview
+          description
+          organizationLevel {
+            id
+            name
+          }
+          teacher {
+            id 
+            fullName
+          }
+          teacher2 {
+            id
+            fullName
+          }
+          dateStart
+          dateEnd
+          timeStart
+          timeEnd
+          infoMailContent
+          scheduleItems {
+            edges {
+              node {
+                id
+              }
+            }
+          }
+          createdAt
+          updatedAt
+        }
+      }
+    }
+  }
+`
+
+export const GET_EVENT_QUERY = gql`
+  query SchoolLevel($id: ID!) {
+    organizationLevel(id:$id) {
+      id
+      name
+      archived
+    }
+  }
+`
