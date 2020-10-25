2554a68ca3a06fd7c6968c00f8000acd45f703d9
Edwin van de Ven <edwin@simplycode.nl>
2020-10-25 18:58:07 +0100

    Query input values in add form & handle loading + error states

--- app/cs_admin/src/components/schedule/events/ScheduleEventAdd.jsx
+++ app/cs_admin/src/components/schedule/events/ScheduleEventAdd.jsx
@@ -1,7 +1,7 @@
 // @flow
 
 import React from 'react'
-import { Mutation } from "react-apollo";
+import { useQuery, useMutation } from "react-apollo";
 import gql from "graphql-tag"
 import { withTranslation } from 'react-i18next'
 import { withRouter } from "react-router"
@@ -12,7 +12,7 @@ import { Link } from 'react-router-dom'
 
 import {
   Page,
-  Grid,
+  Dimmer,
   Icon,
   Button,
   Card,
@@ -22,14 +22,17 @@ import {
 import SiteWrapper from "../../SiteWrapper"
 import HasPermissionWrapper from "../../HasPermissionWrapper"
 
+import { get_list_query_variables } from "./tools"
 
-import { GET_INPUT_VALUES_QUERY } from './queries'
+
+import { GET_SCHEDULE_EVENTS_QUERY, GET_INPUT_VALUES_QUERY } from './queries'
 // import { LEVEL_SCHEMA } from './yupSchema'
 import ScheduleEventForm from './ScheduleEventForm'
 import ScheduleEventsBase from './ScheduleEventsBase'
+import formatISODateStr from '../../ui/ISODateString';
 
 
-const ADD_EVENT = gql`
+const CREATE_SCHEDULE_EVENT = gql`
   mutation CreateScheduleEvent($input:CreateScheduleEventInput!) {
     createScheduleEvent(input: $input) {
       scheduleEvent{
@@ -42,16 +45,105 @@ const ADD_EVENT = gql`
 const return_url = "/organization/events"
 
 function ScheduleEventAdd({ t, history }) {
-  const sidebarContent = <Link to="/schedule/events">
-      <Button color="primary btn-block mb-6"
-              onClick={() => history.push(return_url)}>
+  const returnUrl = "/schedule/events"
+  const sidebarContent = <Link to={returnUrl}>
+      <Button color="primary btn-block mb-6">
         <Icon prefix="fe" name="chevrons-left" /> {t('general.back')}
       </Button>
     </Link>
 
+  const { loading, error, data } = useQuery(GET_INPUT_VALUES_QUERY)
+  const [ createScheduleEvent ] = useMutation(CREATE_SCHEDULE_EVENT)
+
+  if (loading) {
+    return (
+      <ScheduleEventsBase sidebarContent={sidebarContent}>
+        <Card title={t("schedule.events.add")}>
+          <Card.Body>
+            <Dimmer loading={true} active={true} />
+          </Card.Body>
+        </Card>
+      </ScheduleEventsBase>
+    )
+  }
+
+  if (error) {
+    return (
+      <ScheduleEventsBase sidebarContent={sidebarContent}>
+        <Card title={t("schedule.events.add")}>
+          <Card.Body>
+            {t("schedule.events.error_loading")}
+          </Card.Body>
+        </Card>
+      </ScheduleEventsBase>
+    )
+  }
+
+  console.log("CREATE SCHEDULE EVENT INPUT DATA")
+  console.log(data)
+  const inputData = data
+
   return (
     <ScheduleEventsBase sidebarContent={sidebarContent}>
-      hello world
+      <Card title={t("schedule.events.add")}>
+        <Formik
+          initialValues={{ 
+            displayPublic: true,
+            displayShop: true,
+            name: "",
+            description: "",
+          }}
+          // validationSchema={SUBSCRIPTION_SCHEMA}
+          onSubmit={(values, { setSubmitting }) => {
+              console.log('submit values:')
+              console.log(values)
+
+              createScheduleEvent({ variables: {
+                input: {
+                  displayPublic: values.displayPublic,
+                  displayShop: values.displayShop,
+                  autoSentInfoMail: values.autoSentInfoMail,
+                  organizationLocation: values.organizationLocation,
+                  organizationLevel: values.organizationLevel,
+                  name: values.name,
+                  tagline: values.tagline,
+                  preview: values.preview,
+                  description: values.description,
+                  teacher: values.teacher,
+                  teacher2: values.teacher2,
+                  infoMailContent: values.infoMailContent,
+                }
+              }, refetchQueries: [
+                  { query: GET_SCHEDULE_EVENTS_QUERY, variables: get_list_query_variables() }
+              ]})
+              .then(({ data }) => {
+                  console.log('got data', data)
+                  toast.success((t('schedule.events.toast_add_success')), {
+                      position: toast.POSITION.BOTTOM_RIGHT
+                    })
+                }).catch((error) => {
+                  toast.error((t('general.toast_server_error')) + ': ' +  error, {
+                      position: toast.POSITION.BOTTOM_RIGHT
+                    })
+                  console.log('there was an error sending the query', error)
+                  setSubmitting(false)
+                })
+          }}
+          >
+          {({ isSubmitting, setFieldValue, setFieldTouched, errors, values }) => (
+            <ScheduleEventForm
+              inputData={inputData}
+              isSubmitting={isSubmitting}
+              setFieldValue={setFieldValue}
+              setFieldTouched={setFieldTouched}
+              errors={errors}
+              values={values}
+              returnUrl={returnUrl}
+            />
+          )}
+      </Formik>
+      </Card>
+
     </ScheduleEventsBase>
     // <SiteWrapper>
     //   <div className="my-3 my-md-5">
