87ebe3c3cce1afd53cc2616b4b6c0ca0b00f84a8
Edwin van de Ven <edwin@simplycode.nl>
2020-10-27 19:54:44 +0100

    Update ScheduleEventArchive component

--- app/cs_admin/src/components/schedule/events/ScheduleEventArchive.jsx
+++ app/cs_admin/src/components/schedule/events/ScheduleEventArchive.jsx
@@ -5,6 +5,7 @@ import { withRouter } from "react-router"
 
 import { ARCHIVE_SCHEDULE_EVENT, GET_SCHEDULE_EVENTS_QUERY } from "./queries"
 import confirm_archive from "../../../tools/confirm_archive"
+import confirm_unarchive from "../../../tools/confirm_unarchive"
 
 import {
   Icon
@@ -12,37 +13,64 @@ import {
 import { get_list_query_variables } from './tools'
 
 
-function ScheduleEventArchive({t, match, history, id}) {
-  const classId = match.params.class_id
+function ScheduleEventArchive({t, match, history, node}) {
   const [archiveScheduleEvent] = useMutation(ARCHIVE_SCHEDULE_EVENT)
+  const refetchQueries = [
+    { query: GET_SCHEDULE_EVENTS_QUERY, variables: get_list_query_variables() },
+  ]
 
-  return (
-    <button className="icon btn btn-warning btn-sm mb-3 pull-right" 
-      title={t('general.archive')} 
-      onClick={() => {
-        confirm_archive({
-          t: t,
-          msgConfirm: t("schedule.events.archive_confirm_msg"),
-          msgDescription: <p></p>,
-          msgSuccess: t('general.archived'),
-          archiveFunction: archiveScheduleEvent,
-          functionVariables: { 
-            variables: {
-              input: {
-                id: id,
-                archived: true
-              },
-            }, 
-            refetchQueries: [
-              { query: GET_SCHEDULE_EVENTS_QUERY, variables: get_list_query_variables() },
-            ]
-          }
-        })
-    }}>
-      <span className="text-white"><Icon prefix="fe" name="inbox" /> {" "} {t("")}</span>
-    </button>
-  )
+  if (!node.archived) {
+    return (
+      <button className="icon btn btn-warning btn-sm mb-3 pull-right" 
+          title={t('general.unarchive')} 
+          onClick={() => {
+            confirm_archive({
+              t: t,
+              msgConfirm: t("schedule.events.unarchive_confirm_msg"),
+              msgDescription: <p></p>,
+              msgSuccess: t('general.unarchived'),
+              archiveFunction: archiveScheduleEvent,
+              functionVariables: { 
+                variables: {
+                  input: {
+                    id: node.id,
+                    archived: false
+                  },
+                }, 
+                refetchQueries: refetchQueries,
+              }
+            })
+        }}>
+        <span className="text-white"><Icon prefix="fe" name="inbox" /> {" "} {t("")}</span>
+      </button>
+    )
+
+  } else {
+    return (
+      <button className="icon btn btn-warning btn-sm mb-3 pull-right" 
+        title={t('general.archive')} 
+        onClick={() => {
+          confirm_unarchive({
+            t: t,
+            msgConfirm: t("schedule.events.archive_confirm_msg"),
+            msgDescription: <p></p>,
+            msgSuccess: t('general.archived'),
+            archiveFunction: archiveScheduleEvent,
+            functionVariables: { 
+              variables: {
+                input: {
+                  id: node.id,
+                  archived: true
+                },
+              }, 
+              refetchQueries: refetchQueries,
+            }
+          })
+      }}>
+        <span className="text-white"><Icon prefix="fe" name="inbox" /> {" "} {t("")}</span>
+      </button>
+    )
+  }
 }
-
 
 export default withTranslation()(withRouter(ScheduleEventArchive))
