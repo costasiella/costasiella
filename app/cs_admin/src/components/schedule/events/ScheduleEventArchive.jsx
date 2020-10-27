2157ca93a4da1285333587bf069066d94a4aeff3
Edwin van de Ven <edwin@simplycode.nl>
2020-10-27 19:41:11 +0100

    ScheduleEventArchive component

---
+++ app/cs_admin/src/components/schedule/events/ScheduleEventArchive.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { useMutation } from '@apollo/react-hooks';
+import { withTranslation } from 'react-i18next'
+import { withRouter } from "react-router"
+
+import { ARCHIVE_SCHEDULE_EVENT, GET_SCHEDULE_EVENTS_QUERY } from "./queries"
+import confirm_archive from "../../../tools/confirm_archive"
+
+import {
+  Icon
+} from "tabler-react"
+import { get_list_query_variables } from './tools'
+
+
+function ScheduleEventArchive({t, match, history, id}) {
+  const classId = match.params.class_id
+  const [archiveScheduleEvent] = useMutation(ARCHIVE_SCHEDULE_EVENT)
+
+  return (
+    <button className="icon btn btn-warning btn-sm mb-3 pull-right" 
+      title={t('general.archive')} 
+      onClick={() => {
+        confirm_archive({
+          t: t,
+          msgConfirm: t("schedule.events.archive_confirm_msg"),
+          msgDescription: <p></p>,
+          msgSuccess: t('general.archived'),
+          archiveFunction: archiveScheduleEvent,
+          functionVariables: { 
+            variables: {
+              input: {
+                id: id,
+                archived: true
+              },
+            }, 
+            refetchQueries: [
+              { query: GET_SCHEDULE_EVENTS_QUERY, variables: get_list_query_variables() },
+            ]
+          }
+        })
+    }}>
+      <span className="text-white"><Icon prefix="fe" name="inbox" /> {" "} {t("")}</span>
+    </button>
+  )
+}
+
+
+export default withTranslation()(withRouter(ScheduleEventArchive))
