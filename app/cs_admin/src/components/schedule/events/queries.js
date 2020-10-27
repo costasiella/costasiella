f23e29eb66878615c682397e4bbbba7a5daeabc6
Edwin van de Ven <edwin@simplycode.nl>
2020-10-27 21:13:11 +0100

    Add single Schedule Event Query

--- app/cs_admin/src/components/schedule/events/queries.js
+++ app/cs_admin/src/components/schedule/events/queries.js
@@ -56,12 +56,92 @@ export const GET_SCHEDULE_EVENTS_QUERY = gql`
   }
 `
 
-export const GET_EVENT_QUERY = gql`
-  query SchoolLevel($id: ID!) {
-    organizationLevel(id:$id) {
+export const GET_SCHEDULE_EVENT_QUERY = gql`
+  query ScheduleEvent($id: ID!, before: String, after: String) {
+    scheduleEvent(id: $id) {
       id
-      name
       archived
+      displayPublic
+      displayShop
+      autoSendInfoMail
+      organizationLocation {
+        id
+        name
+      }
+      name
+      tagline
+      preview
+      description
+      organizationLevel {
+        id
+        name
+      }
+      teacher {
+        id 
+        fullName
+      }
+      teacher2 {
+        id
+        fullName
+      }
+      dateStart
+      dateEnd
+      timeStart
+      timeEnd
+      infoMailContent
+      scheduleItems {
+        edges {
+          node {
+            id
+          }
+        }
+      }
+      createdAt
+      updatedAt
+    }
+    organizationLocations(first: 100, before: $before, after: $after, archived: false) {
+      pageInfo {
+        startCursor
+        endCursor
+        hasNextPage
+        hasPreviousPage
+      }
+      edges {
+        node {
+          id
+          archived
+          name
+        }
+      }
+    }
+    organizationLevels(first: 100, before: $before, after: $after, archived: false) {
+      pageInfo {
+        startCursor
+        endCursor
+        hasNextPage
+        hasPreviousPage
+      }
+      edges {
+        node {
+          id
+          archived
+          name
+        }
+      }
+    }
+    accounts(first: 100, before: $before, after: $after, isActive: true, teacher: true) {
+      pageInfo {
+        startCursor
+        endCursor
+        hasNextPage
+        hasPreviousPage
+      }
+      edges {
+        node {
+          id
+          fullName
+        }
+      }
     }
   }
 `
