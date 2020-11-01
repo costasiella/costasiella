a8fca420292a264c77f31c3a43e50fdffbab89a6
Edwin van de Ven <edwin@simplycode.nl>
2020-11-01 20:48:50 +0100

    Add ScheduleEventEditTabs component

---
+++ app/cs_admin/src/components/schedule/events/edit/ScheduleEventEditTabs.jsx
@@ -0,0 +1,34 @@
+// @flow
+
+import React from 'react'
+import { withTranslation } from 'react-i18next'
+import { Link } from 'react-router-dom'
+
+import CardTabs from "../../../ui/CardTabs"
+
+function ScheduleEventEditTabs({ t, active, event_id}) {
+
+  return (
+    <CardTabs 
+      position="top"
+      tabs={[
+          {
+            name: "general", 
+            title: t("schedule.events.edit_menu.general"), 
+            link: `/events/edit/${event_id}/`
+          },
+          {
+            name: "tickets", 
+            title: t("schedule.events.edit_menu.tickets"), 
+            link: `/events/edit/${event_id}/tickets`
+          },
+      ]}
+      active={active}
+    />
+  )
+}
+
+export default withTranslation()(ScheduleEventEditTabs)
+
+
+
