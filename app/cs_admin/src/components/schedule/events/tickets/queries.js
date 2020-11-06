56aca2e620f39c498ffaecc8268af3dbb3ab44fc
Edwin van de Ven <edwin@simplycode.nl>
2020-11-06 21:13:28 +0100

    Add tickets list query

---
+++ app/cs_admin/src/components/schedule/events/tickets/queries.js
@@ -0,0 +1,64 @@
+import gql from "graphql-tag"
+
+export const GET_SCHEDULE_EVENT_TICKETS_QUERY = gql`
+query ScheduleEventTickets($before:String, $after:String, $schedule_event:ID!) {
+  scheduleEventTickets(first: 100, before:$before, after:$after, scheduleEvent:$schedule_event) {
+    pageInfo {
+      hasNextPage
+      hasPreviousPage
+      startCursor
+      endCursor
+    }
+    edges {
+      node {
+        id
+        scheduleEvent {
+          id
+        }
+        fullEvent
+        deletable
+        displayPublic
+        name
+        description
+        price
+        priceDisplay
+        financeTaxRate {
+          id
+          name
+        }
+        financeGlaccount {
+          id
+          name
+        }
+        financeCostcenter {
+          id
+          name
+        }
+      }
+    }
+  }
+}
+`
+
+// export const GET_ACCOUNT_SUBSCRIPTION_BLOCK_QUERY = gql`
+// query AccountSubscriptionBlock($id: ID!) {
+//   accountSubscriptionBlock(id:$id) {
+//     id
+//     accountSubscription {
+//       id
+//     }
+//     dateStart
+//     dateEnd
+//     description
+//   }
+// }
+// `
+
+
+// export const DELETE_ACCOUNT_SUBSCRIPTION_BLOCK = gql`
+//   mutation DeleteAccountSubscriptionBlock($input: DeleteAccountSubscriptionBlockInput!) {
+//     deleteAccountSubscriptionBlock(input: $input) {
+//       ok
+//     }
+//   }
+// `
