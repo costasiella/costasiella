8a8c56034de1fa6e1f7987127d5e4b37d0b54aa0
Edwin van de Ven <edwin@simplycode.nl>
2020-11-09 13:07:44 +0100

    ScheduleEventTicketAdd component

--- app/cs_admin/src/components/schedule/events/tickets/ScheduleEventTicketAdd.jsx
+++ app/cs_admin/src/components/schedule/events/tickets/ScheduleEventTicketAdd.jsx
@@ -8,11 +8,11 @@ import { withRouter } from "react-router"
 import { Formik } from 'formik'
 import { toast } from 'react-toastify'
 
-import { GET_ACCOUNT_SUBSCRIPTION_BLOCKS_QUERY } from "./queries"
-import { ACCOUNT_SUBSCRIPTION_BLOCK_SCHEMA } from './yupSchema'
-import { dateToLocalISO } from '../../../../../../tools/date_tools'
+import { GET_SCHEDULE_EVENT_TICKETS_QUERY, GET_INPUT_VALUES_QUERY } from "./queries"
+// import { ACCOUNT_SUBSCRIPTION_BLOCK_SCHEMA } from './yupSchema'
+// import { dateToLocalISO } from '../../../../../../tools/date_tools'
 
-import AccountSubscriptionEditBlockBase from "./AccountSubscriptionEditBlockBase"
+import ScheduleEventEditBase from "../ScheduleEventEditBase"
 import ScheduleEventTicketForm from "./ScheduleEventTicketForm"
 
 
@@ -29,15 +29,29 @@ const ADD_SCHEDULE_EVENT_TICKET = gql`
 
 function ScheduleEventTicketAdd({ t, history, match }) {
   const eventId = match.params.event_id
-  const returnUrl = `/schedule/events/${eventId}/tickets/`
-
-
+  const returnUrl = `/schedule/events/edit/${eventId}/tickets/`
+  const activeTab = 'tickets'
   const [addScheduleEventTicket] = useMutation(ADD_SCHEDULE_EVENT_TICKET, {
     onCompleted: () => history.push(returnUrl),
   })
+  const { loading, error, data, fetchMore } = useQuery(GET_INPUT_VALUES_QUERY)
+  
+  if (loading) return (
+    <ScheduleEventEditBase activeTab={activeTab}>
+      {t("general.loading_with_dots")}
+    </ScheduleEventEditBase>
+  )
+  if (error) return (
+    <ScheduleEventEditBase activeTab={activeTab}>
+      <p>{t('general.error_sad_smiley')}</p>
+      <p>{error.message}</p>
+    </ScheduleEventEditBase>
+  )
+
+  const inputData = data
 
   return (
-    <AccountSubscriptionEditBlockBase>
+    <ScheduleEventEditBase activeTab={activeTab}>
       <Formik
         initialValues={{ 
           displayPublic: true,
@@ -51,19 +65,23 @@ function ScheduleEventTicketAdd({ t, history, match }) {
 
           addScheduleEventTicket({ variables: {
             input: {
-              accountSubscription: subscriptionId,
-              dateStart: dateToLocalISO(values.dateStart),
-              dateEnd: dateToLocalISO(values.dateEnd),
-              description: values.description
+              scheduleEvent: eventId,
+              displayPublic: values.displayPublic,
+              name: values.name,
+              description: values.description,
+              price: values.price,
+              financeTaxRate: values.financeTaxRate,
+              financeGlaccount: values.financeGlaccount,
+              financeCostcenter: values.financeCostcenter
             }
           }, refetchQueries: [
-              {query: GET_ACCOUNT_SUBSCRIPTION_BLOCKS_QUERY, variables: {
-                accountSubscription: subscriptionId
+              {query: GET_SCHEDULE_EVENT_TICKETS_QUERY, variables: {
+                schedule_event: eventId
               }},
           ]})
           .then(({ data }) => {
               console.log('got data', data);
-              toast.success((t('relations.account.subscriptions.blocks.toast_add_success')), {
+              toast.success((t('schedule.events.tickets.toast_add_success')), {
                   position: toast.POSITION.BOTTOM_RIGHT
                 })
             }).catch((error) => {
@@ -82,12 +100,13 @@ function ScheduleEventTicketAdd({ t, history, match }) {
             setFieldValue={setFieldValue}
             errors={errors}
             values={values}
+            inputData={inputData}
             returnUrl={returnUrl}
             formTitle="create"
           />
         )}
       </Formik>
-    </AccountSubscriptionEditBlockBase>
+    </ScheduleEventEditBase>
   )
 }
 
