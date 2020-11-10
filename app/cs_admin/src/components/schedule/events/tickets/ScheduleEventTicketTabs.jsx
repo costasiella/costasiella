31fdca04e0ba65158c94137bd0d26db348087231
Edwin van de Ven <edwin@simplycode.nl>
2020-11-10 19:18:40 +0100

    ScheduleEventTicketTabs component

---
+++ app/cs_admin/src/components/schedule/events/tickets/ScheduleEventTicketTabs.jsx
@@ -0,0 +1,30 @@
+// @flow
+
+import React from 'react'
+import { withTranslation } from 'react-i18next'
+import { Link } from 'react-router-dom'
+
+import CardTabs from "../../ui/CardTabs"
+
+function ScheduleEventTicketTabs({ t, active, eventId, ticketId}) {
+  return (
+    <CardTabs 
+      position="top"
+      tabs={[
+          {
+            name: "general", 
+            title: t("schedule.events.tickets.edit_menu.general"), 
+            link: `/schedule/events/edit/${eventId}/tickets/edit/${ticketId}`
+          },
+          {
+            name: "activities", 
+            title: t("schedule.events.tickets.edit_menu.activities"), 
+            link: `/schedule/events/edit/${eventId}/tickets/edit/${ticketId}/activities`
+          },
+      ]}
+      active={active}
+    />
+  )
+}
+
+export default withTranslation()(ScheduleEventTicketTabs)
