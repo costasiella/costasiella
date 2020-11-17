0840c1a014db593a8c3432ec353c13341484157d
Edwin van de Ven <edwin@simplycode.nl>
2020-11-17 13:49:26 +0100

    Add event activities query

---
+++ app/cs_admin/src/components/schedule/events/activities/queries.js
@@ -0,0 +1,160 @@
+import gql from "graphql-tag"
+
+export const GET_SCHEDULE_EVENT_ACTIVITIES_QUERY = gql`
+query ScheduleItem($before:String, $after:String, $schedule_event:ID!) {
+  scheduleItems(first:100, before:$before, after:$after, scheduleEvent:$schedule_event) {
+    pageInfo {
+      hasNextPage
+      hasPreviousPage
+      startCursor
+      endCursor
+    }
+    edges {
+      node {
+        id
+        displayPublic
+        scheduleEvent {
+          id
+          name
+        }
+      	scheduleItemType
+        frequencyType
+        scheduleItemType
+        organizationLocationRoom {
+          id
+          name
+        }
+        name
+        spaces
+        walkInSpaces
+        dateStart
+        timeStart
+        timeEnd
+      	description
+      }
+    }
+  }
+}
+`
+
+// export const GET_SCHEDULE_EVENT_TICKET_QUERY = gql`
+// query ScheduleEventTicket($before:String, $after:String, $id:ID!) {
+//   scheduleEventTicket(id: $id) {
+//     id
+//     displayPublic
+//     name
+//     description
+//     price
+//     financeTaxRate {
+//       id
+//       name
+//     }
+//     financeGlaccount {
+//       id
+//       name
+//     }
+//     financeCostcenter {
+//       id
+//       name
+//     }
+//   }
+//   financeTaxRates(first: 100, before: $before, after: $after, archived: false) {
+//     pageInfo {
+//       startCursor
+//       endCursor
+//       hasNextPage
+//       hasPreviousPage
+//     }
+//     edges {
+//       node {
+//         id
+//         name
+//       }
+//     }
+//   }
+//   financeGlaccounts(first: 100, before: $before, after: $after, archived: false) {
+//     pageInfo {
+//       startCursor
+//       endCursor
+//       hasNextPage
+//       hasPreviousPage
+//     }
+//     edges {
+//       node {
+//         id
+//         name
+//       }
+//     }
+//   }
+//   financeCostcenters(first: 100, before: $before, after: $after, archived: false) {
+//     pageInfo {
+//       startCursor
+//       endCursor
+//       hasNextPage
+//       hasPreviousPage
+//     }
+//     edges {
+//       node {
+//         id
+//         name
+//       }
+//     }
+//   }
+// }
+// `
+
+// export const DELETE_SCHEDULE_EVENT_TICKET = gql`
+//   mutation DeleteScheduleEventTicket($input: DeleteScheduleEventTicketInput!) {
+//     deleteScheduleEventTicket(input: $input) {
+//       ok
+//     }
+//   }
+// `
+
+
+// export const GET_INPUT_VALUES_QUERY = gql`
+//   query ScheduleEventTicketInputValues($after: String, $before: String) {
+//     financeTaxRates(first: 100, before: $before, after: $after, archived: false) {
+//       pageInfo {
+//         startCursor
+//         endCursor
+//         hasNextPage
+//         hasPreviousPage
+//       }
+//       edges {
+//         node {
+//           id
+//           name
+//         }
+//       }
+//     }
+//     financeGlaccounts(first: 100, before: $before, after: $after, archived: false) {
+//       pageInfo {
+//         startCursor
+//         endCursor
+//         hasNextPage
+//         hasPreviousPage
+//       }
+//       edges {
+//         node {
+//           id
+//           name
+//         }
+//       }
+//     }
+//     financeCostcenters(first: 100, before: $before, after: $after, archived: false) {
+//       pageInfo {
+//         startCursor
+//         endCursor
+//         hasNextPage
+//         hasPreviousPage
+//       }
+//       edges {
+//         node {
+//           id
+//           name
+//         }
+//       }
+//     }
+//   }
+// `
