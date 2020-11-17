49f5ce4ec144f09c6e594042fce7fd395bfac5d3
Edwin van de Ven <edwin@simplycode.nl>
2020-11-17 14:00:46 +0100

    Add activities list componen

---
+++ app/cs_admin/src/components/schedule/events/activities/ScheduleEventActivities.jsx
@@ -0,0 +1,160 @@
+// @flow
+
+import React, { useContext } from 'react'
+import { useQuery } from "react-apollo";
+import { withTranslation } from 'react-i18next'
+import { withRouter } from "react-router"
+import { Link } from 'react-router-dom'
+import { v4 } from 'uuid'
+
+import AppSettingsContext from '../../../context/AppSettingsContext'
+import ButtonAddSecondaryMenu from '../../../ui/ButtonAddSecondaryMenu'
+import BadgeBoolean from "../../../ui/BadgeBoolean"
+
+import { GET_SCHEDULE_EVENT_ACTIVITIES_QUERY } from './queries'
+
+import {
+  Page,
+  Grid,
+  Icon,
+  Button,
+  Card,
+  Container,
+  Table,
+} from "tabler-react";
+// import HasPermissionWrapper from "../../../../HasPermissionWrapper"
+import ScheduleEventEditListBase from "../edit/ScheduleEventEditListBase"
+import ScheduleEventTicketListBase from "./ScheduleEventTicketListBase"
+import ScheduleEventTicketDelete from "./ScheduleEventActivityDelete"
+import moment from 'moment';
+
+
+function ScheduleEventActivities({t, match, history}) {
+  const appSettings = useContext(AppSettingsContext)
+  const dateFormat = appSettings.dateFormat
+  console.log(appSettings)
+  
+  const eventId = match.params.event_id
+  const activeLink = "activities"
+
+  const sidebarContent = <Link to={`/schedule/events/edit/${eventId}/activities/add`}>
+    <Button color="primary btn-block mb-6">
+      <Icon prefix="fe" name="plus-circle" /> {t('schedule.events.activitiess.add')}
+    </Button>
+  </Link>
+
+  const { loading, error, data, fetchMore } = useQuery(GET_SCHEDULE_EVENT_ACTIVITIES_QUERY, {
+    variables: {
+      schedule_event: eventId
+    }
+  })
+  
+  if (loading) return (
+    <ScheduleEventEditListBase activeLink={activeLink} sidebarContent={sidebarContent}>
+      {t("general.loading_with_dots")}
+    </ScheduleEventEditListBase>
+  )
+  if (error) return (
+    <ScheduleEventEditListBase activeLink={activeLink} sidebarContent={sidebarContent}>
+      <p>{t('general.error_sad_smiley')}</p>
+      <p>{error.message}</p>
+    </ScheduleEventEditListBase>
+  )
+
+  console.log('query data')
+  console.log(data)
+
+  const scheduleEventActivities = data.scheduleEventActivities
+  const pageInfo = data.scheduleEventActivities.pageInfo
+
+  // Empty list
+  if (!scheduleEventActivities.edges.length) { return (
+    <ScheduleEventEditListBase activeLink={activeLink} sidebarContent={sidebarContent}>
+      <p>{t('schedule.events.tickets.empty_list')}</p>
+    </ScheduleEventEditListBase>
+  )}
+
+  const onLoadMore = () => {
+    fetchMore({
+      variables: {
+        after: scheduleEventActivities.pageInfo.endCursor
+      },
+      updateQuery: (previousResult, { fetchMoreResult }) => {
+        const newEdges = fetchMoreResult.scheduleEventActivities.edges
+        const pageInfo = fetchMoreResult.scheduleEventActivities.pageInfo
+
+        return newEdges.length
+          ? {
+              // Put the new invoices at the end of the list and update `pageInfo`
+              // so we have the new `endCursor` and `hasNextPage` values
+              scheduleEventActivities: {
+                __typename: previousResult.scheduleEventActivities.__typename,
+                edges: [ ...previousResult.scheduleEventActivities.edges, ...newEdges ],
+                pageInfo
+              }
+            }
+          : previousResult
+      }
+    })
+  }
+
+  return (
+    <ScheduleEventEditListBase activeLink={activeLink} pageInfo={pageInfo} onLoadMore={onLoadMore} sidebarContent={sidebarContent}>
+      <Table>
+        <Table.Header>
+          <Table.Row key={v4()}>
+            <Table.ColHeader>{t('general.name')}</Table.ColHeader>
+            {/* <Table.ColHeader>{t('general.price')}</Table.ColHeader> */}
+            <Table.ColHeader>{t('general.public')}</Table.ColHeader>
+            {/* <Table.ColHeader>{t('general.glaccount')}</Table.ColHeader> */}
+            {/* <Table.ColHeader>{t('general.costcenter')}</Table.ColHeader> */}
+            <Table.ColHeader></Table.ColHeader>
+            <Table.ColHeader></Table.ColHeader>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+            {scheduleEventActivities.edges.map(({ node }) => (
+              <Table.Row key={v4()}>
+                {/* <Table.Col>
+                  {moment(node.dateStart).format(dateFormat)}
+                </Table.Col>
+                <Table.Col>
+                  {moment(node.dateEnd).format(dateFormat)}
+                </Table.Col> */}
+                <Table.Col>
+                  {node.name} <br />
+                  <div dangerouslySetInnerHTML={{__html: node.description}} className="text-muted"/>
+                </Table.Col>
+                {/* <Table.Col>
+                  {node.priceDisplay}
+                </Table.Col> */}
+                <Table.Col>
+                  <BadgeBoolean value={node.displayPublic} />
+                </Table.Col>
+                {/* <Table.Col>
+                  {(node.financeGlaccount) ? node.financeGlaccount.name : ""}
+                </Table.Col>
+                <Table.Col>
+                  {(node.financeCostcenter) ? node.financeCostcenter.name : ""}
+                </Table.Col> */}
+                <Table.Col className="text-right">
+                  <Link to={`/schedule/events/edit/${eventId}/activities/edit/${node.id}`}>
+                    <Button className='btn-sm' 
+                            color="secondary">
+                      {t('general.edit')}
+                    </Button>
+                  </Link>
+                </Table.Col>
+                <Table.Col className="text-right">
+                  {/* Check for fullEvent / deletable before showing delete button */}
+                  {(node.deletable) ? <ScheduleEventActivityDelete id={node.id} /> : ""}
+                </Table.Col>
+              </Table.Row>
+            ))}
+        </Table.Body>
+      </Table>
+    </ScheduleEventEditListBase>
+  )
+}
+
+export default withTranslation()(withRouter(ScheduleEventActivities))
