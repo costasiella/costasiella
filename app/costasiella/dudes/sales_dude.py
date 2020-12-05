73b2f0ead7eec7632c3896279915d50b039f2e44
Edwin van de Ven <edwin@simplycode.nl>
2020-12-05 12:29:22 +0100

    Add sell ticket fn to sales dude

--- app/costasiella/dudes/sales_dude.py
+++ app/costasiella/dudes/sales_dude.py
@@ -132,3 +132,66 @@ class SalesDude:
         finance_invoice_item = finance_invoice.item_add_subscription(account_subscription, year, month)
 
         return finance_invoice_item
+
+    def sell_schedule_event_ticket(self,
+                                   account,
+                                   schedule_event_ticket,
+                                   create_invoice=True):
+        """
+        Sell subscription to account
+        """
+        from ..models.account_schedule_event_ticket import AccountScheduleEventTicket
+
+        account_schedule_event_ticket = AccountScheduleEventTicket(
+            account=account,
+            schedule_event_ticket=schedule_event_ticket
+        )
+
+        # set date end & save
+        account_schedule_event_ticket.save()
+
+        print('creating invoice...')
+
+        finance_invoice_item = None
+        if create_invoice:
+            print('still alive')
+            finance_invoice_item = self._sell_schedule_event_ticket_create_invoice(account_schedule_event_ticket)
+
+        return {
+            "account_schedule_event_ticket": account_schedule_event_ticket,
+            "finance_invoice_item": finance_invoice_item
+        }
+
+    @staticmethod
+    def _sell_schedule_event_ticket_create_invoice(account_schedule_event_ticket):
+        """
+        Create an invoice for sold subscription.
+        This function should only be used for the 1st invoice for a subscription
+        """
+        from ..models.finance_invoice_group_default import FinanceInvoiceGroupDefault
+        from ..models.finance_invoice import FinanceInvoice
+
+        finance_invoice_group_default = FinanceInvoiceGroupDefault.objects.filter(item_type="EVENT_TICKETS").first()
+        finance_invoice_group = finance_invoice_group_default.finance_invoice_group
+        print("invoice group")
+        print(finance_invoice_group)
+
+        finance_invoice = FinanceInvoice(
+            account=account_schedule_event_ticket.account,
+            finance_invoice_group=finance_invoice_group,
+            summary=_("Event ticket: %s - %s" % (
+                account_schedule_event_ticket.schedule_event_ticket.schedule_event.name,
+                account_schedule_event_ticket.schedule_event_ticket.name
+            )),
+            status="SENT",
+            terms=finance_invoice_group.terms,
+            footer=finance_invoice_group.footer
+        )
+
+        # Save invoice
+        finance_invoice.save()
+
+        # Add invoice item
+        finance_invoice_item = finance_invoice.item_add_schedule_event_ticket(account_schedule_event_ticket)
+
+        return finance_invoice_item
