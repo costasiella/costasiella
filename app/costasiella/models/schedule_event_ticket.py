8c052becfa122c389d7721b6ffc5d791f820b060
Edwin van de Ven <edwin@simplycode.nl>
2020-12-14 14:30:55 +0100

    Implement is_sold_out fn for schedule event ticket

--- app/costasiella/models/schedule_event_ticket.py
+++ app/costasiella/models/schedule_event_ticket.py
@@ -1,6 +1,7 @@
 from django.utils.translation import gettext as _
 
 from django.db import models
+from django.db.models import Q
 
 from .schedule_event import ScheduleEvent
 from .schedule_event_ticket_schedule_item import ScheduleEventTicketScheduleItem
@@ -30,3 +31,30 @@ class ScheduleEventTicket(models.Model):
 
     def __str__(self):
         return self.name + ' [' + str(self.price) + ']'
+
+    def is_sold_out(self):
+        """
+        Check if this ticket is sold out.
+        Sold out when at least 1 activity doesn't have sufficient spaces remaining.
+        :return:
+        """
+        from .schedule_event_ticket_schedule_item import ScheduleEventTicketScheduleItem
+        from .schedule_item_attendance import ScheduleItemAttendance
+
+        sold_out = False
+
+        ticket_schedule_items = ScheduleEventTicketScheduleItem.objects.filter(
+            schedule_event_ticket=self
+        )
+        for ticket_schedule_item in ticket_schedule_items:
+            schedule_item = ticket_schedule_item.schedule_item
+            count_attendance = ScheduleItemAttendance.objects.filter(
+                Q(schedule_item=schedule_item) &
+                ~Q(booking_status="cancelled")
+            ).count()
+
+            if count_attendance >= schedule_item.spaces:
+                sold_out = True
+                break
+
+        return sold_out
