0c049dc06048ffbb08ae9d547600a36849b923f9
Edwin van de Ven <edwin@simplycode.nl>
2020-12-27 16:27:38 +0100

    Set related name for schedule event media model

--- app/costasiella/models/schedule_event_media.py
+++ app/costasiella/models/schedule_event_media.py
@@ -6,7 +6,7 @@ from .schedule_event import ScheduleEvent
 
 # Create your models here.
 class ScheduleEventMedia(models.Model):
-    schedule_event = models.ForeignKey(ScheduleEvent, on_delete=models.CASCADE)
+    schedule_event = models.ForeignKey(ScheduleEvent, on_delete=models.CASCADE, related_name="media")
     sort_order = models.PositiveIntegerField(default=0)
     description = models.CharField(max_length=255)
     image = ImageField(upload_to='schedule_event_media', default=None)
