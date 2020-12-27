44fb3ef59e42aad389fae2ce4f69c0b21ac76e33
Edwin van de Ven <edwin@simplycode.nl>
2020-12-27 17:14:13 +0100

    ShopEvents working

--- app/cs_admin/src/components/shop/events/ShopEvents.jsx
+++ app/cs_admin/src/components/shop/events/ShopEvents.jsx
@@ -46,7 +46,7 @@ function ShopEvents({ t, match, history }) {
     <ShopEventsBase title={title}>
         <Grid.Row>
           {scheduleEvents.edges.map(({ node }) => (
-            <Grid.Col md={4} key={v4()}>
+            <Grid.Col sm={12} md={4} key={v4()}>
               <GalleryCard>
                 <GalleryCard.Image 
                   src={(node.media.edges.length) ? node.media.edges[0].node.urlImage: ""} 
@@ -81,33 +81,6 @@ function ShopEvents({ t, match, history }) {
                   </GalleryCard.Footer>
                 </Link>
               </GalleryCard>
-              {/* <ShopClasspassPricingCard
-                classpass={node}
-                btnLink={"/shop/classpass/" + node.id}
-              /> */}
-              {/* <PricingCard>
-                <PricingCard.Category>
-                  {node.name}
-                </PricingCard.Category>
-                <PricingCard.Price>
-                  {node.priceDisplay}
-                </PricingCard.Price>
-                <PricingCard.AttributeList>
-                  <PricingCard.AttributeItem>
-                    <b>{(node.unlimited) ? t('general.unlimited') : node.classes }</b> { " " }
-                    {((node.classes != 1) || (node.unlimited))? t('general.classes'): t('general.class')}
-                  </PricingCard.AttributeItem>
-                  <PricingCard.AttributeItem>
-                    {t('general.valid_for')} { " " }
-                    <b>{node.validity}</b> {' '} {node.validityUnitDisplay}
-                  </PricingCard.AttributeItem>
-                </PricingCard.AttributeList>
-                <Link to={"/shop/classpass/" + node.id}>
-                  <PricingCard.Button >
-                    {t("shop.classpasses.choose")} <Icon name="chevron-right" />
-                  </PricingCard.Button>
-                </Link>
-              </PricingCard> */}
             </Grid.Col>
           ))}
         </Grid.Row>
@@ -119,30 +92,3 @@ function ShopEvents({ t, match, history }) {
 
 
 export default withTranslation()(withRouter(ShopEvents))
-
-
-{/* <Grid.Col sm={6} lg={3}>
-<PricingCard active>
-  <PricingCard.Category>{"Premium"}</PricingCard.Category>
-  <PricingCard.Price>{"$49"} </PricingCard.Price>
-  <PricingCard.AttributeList>
-    <PricingCard.AttributeItem>
-      <strong>10 </strong>
-      {"Users"}
-    </PricingCard.AttributeItem>
-    <PricingCard.AttributeItem hasIcon available>
-      {"Sharing Tools"}
-    </PricingCard.AttributeItem>
-    <PricingCard.AttributeItem hasIcon available>
-      {"Design Tools"}
-    </PricingCard.AttributeItem>
-    <PricingCard.AttributeItem hasIcon available={false}>
-      {"Private Messages"}
-    </PricingCard.AttributeItem>
-    <PricingCard.AttributeItem hasIcon available={false}>
-      {"Twitter API"}
-    </PricingCard.AttributeItem>
-  </PricingCard.AttributeList>
-  <PricingCard.Button active>{"Choose plan"} </PricingCard.Button>
-</PricingCard>
-</Grid.Col> */}
