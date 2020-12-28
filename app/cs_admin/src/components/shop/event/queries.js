00e237ce992420712a2d81a5064d59dfd2f5ddda
Edwin van de Ven <edwin@simplycode.nl>
2020-12-28 14:14:26 +0100

    Add shop single event query

---
+++ app/cs_admin/src/components/shop/event/queries.js
@@ -0,0 +1,93 @@
+import gql from "graphql-tag"
+
+
+export const GET_SCHEDULE_EVENT_QUERY = gql`
+query ScheduleEvent($id: ID!) {
+  scheduleEvent(id: $id) {
+    id
+    archived
+    displayPublic
+    displayShop
+    autoSendInfoMail
+    organizationLocation {
+      id
+      name
+    }
+    name
+    tagline
+    preview
+    description
+    organizationLevel {
+      id
+      name
+    }
+    teacher {
+      id 
+      fullName
+    }
+    teacher2 {
+      id
+      fullName
+    }
+    dateStart
+    dateEnd
+    timeStart
+    timeEnd
+    infoMailContent
+    media(first: 1) {
+      pageInfo {
+        hasNextPage
+        hasPreviousPage
+        startCursor
+        endCursor
+      }
+      edges {
+        node {
+          urlImageThumbnailSmall
+          urlImageThumbnailLarge
+        }
+      }
+    }
+    tickets {
+      pageInfo {
+        hasNextPage
+        hasPreviousPage
+        startCursor
+        endCursor
+      }
+      edges {
+        node {
+          name
+          price
+          description
+          scheduleItems {
+            pageInfo {
+              hasNextPage
+              hasPreviousPage
+              startCursor
+              endCursor
+            }
+            edges {
+              node {
+                name
+                description
+                organizationLocationRoom {
+                  name
+                  organizationLocation {
+                    name
+                  }
+                }
+                dateStart
+                timeStart
+                timeEnd
+              }
+            }
+          }
+        }
+      }
+    }
+    createdAt
+    updatedAt
+  }
+}
+`
