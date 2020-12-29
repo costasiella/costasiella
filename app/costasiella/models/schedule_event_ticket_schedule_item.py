0da9f990eb0b1f6969b10c55a7588dbcc2e4be4c
Edwin van de Ven <edwin@simplycode.nl>
2020-12-29 12:44:43 +0100

    Add related_name for ScheduleEventTicketScheduleItem model

--- app/costasiella/models/schedule_event_ticket_schedule_item.py
+++ app/costasiella/models/schedule_event_ticket_schedule_item.py
@@ -4,7 +4,9 @@ from django.db import models
 
 
 class ScheduleEventTicketScheduleItem(models.Model):
-    schedule_event_ticket = models.ForeignKey('ScheduleEventTicket', on_delete=models.CASCADE)
+    schedule_event_ticket = models.ForeignKey('ScheduleEventTicket',
+                                              on_delete=models.CASCADE,
+                                              related_name="ticket_schedule_items")
     schedule_item = models.ForeignKey('ScheduleItem', on_delete=models.CASCADE)
     included = models.BooleanField(default=False)
 
