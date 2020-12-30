755a910766ec1effa7455be304c82adebe86cf06
Edwin van de Ven <edwin@simplycode.nl>
2020-12-30 12:27:11 +0100

    Fetch eventId from url in EventTicketBase component

--- app/cs_admin/src/components/shop/event_ticket/ShopEventTicketBase.jsx
+++ app/cs_admin/src/components/shop/event_ticket/ShopEventTicketBase.jsx
@@ -6,7 +6,8 @@ import { withRouter } from "react-router"
 
 import ShopBase from "../ShopBase"
 
-function ShopEventTicketBase({ t, match, history, eventId, children }) {
+function ShopEventTicketBase({ t, match, history, children }) {
+  const eventId = match.params.event_id
   
   return (
     <ShopBase 
