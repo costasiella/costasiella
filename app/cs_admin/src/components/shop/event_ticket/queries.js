16b5f34d90e7aa1e65c003d43b2f4913a4d37f7b
Edwin van de Ven <edwin@simplycode.nl>
2020-12-30 12:27:24 +0100

    Add event_ticket query

---
+++ app/cs_admin/src/components/shop/event_ticket/queries.js
@@ -0,0 +1,43 @@
+import gql from "graphql-tag"
+
+
+export const GET_SCHEDULE_EVENT_TICKET_QUERY = gql`
+  query ScheduleEventTicket($id: ID!) {
+    scheduleEventTicket(id:$id) {
+      id
+      name
+      price
+      priceDisplay
+      description
+      ticketScheduleItems(included: true) {
+        pageInfo{
+        hasNextPage
+        hasPreviousPage
+        startCursor
+        endCursor
+        }
+        edges {
+          node {
+            id
+            included
+            scheduleItem {
+              name
+              dateStart
+              timeStart
+              timeEnd
+              organizationLocationRoom {
+                organizationLocation {
+                  name
+                }
+              }
+            }
+          }
+        }
+      }
+      scheduleEvent {
+        id
+        name
+      }
+    }
+  }
+`
