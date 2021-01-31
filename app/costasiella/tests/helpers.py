47ac91b07a87186fa650c073bab103b3c61cde33
Edwin van de Ven <edwin@simplycode.nl>
2021-01-31 14:27:22 +0100

    Add clean media function to helpers

--- app/costasiella/tests/helpers.py
+++ app/costasiella/tests/helpers.py
@@ -1,8 +1,13 @@
+import os
+import shutil
+
 import graphene
 from django.test import RequestFactory
 from graphene.test import Client
 
 from .. import schema as cs_schema
+
+from app.settings.development import MEDIA_ROOT
 
 
 # Create schema object
@@ -22,3 +27,16 @@ def execute_test_client_api_query(query, user=None, variables=None, **kwargs):
     client = Client(schema)
     executed = client.execute(query, context=context, variables=variables, **kwargs)
     return executed
+
+
+def clean_media():
+    """ Clean up media files """
+    if 'TRAVIS' not in os.environ:
+        # Clean test media root after each test to prevent stray files after testing
+        # But don't run on Travis-CI, as we don't seem to be allowed to remove stuff from dirs
+        # in the tests
+        for root, dirs, files in os.walk(MEDIA_ROOT):
+            for f in files:
+                os.unlink(os.path.join(root, f))
+            for d in dirs:
+                shutil.rmtree(os.path.join(root, d))
