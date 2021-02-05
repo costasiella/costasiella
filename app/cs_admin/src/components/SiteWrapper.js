367d01c8dbe3afa0a247709d2702a10cebc803e1
Edwin van de Ven <edwin@simplycode.nl>
2021-02-05 18:43:01 +0100

    Migrate site wrapper to fn comp

--- app/cs_admin/src/components/SiteWrapper.js
+++ app/cs_admin/src/components/SiteWrapper.js
@@ -3,7 +3,7 @@
 import * as React from "react"
 import { withTranslation } from 'react-i18next'
 import { NavLink, withRouter } from "react-router-dom"
-import { Query } from "react-apollo"
+import { useQuery, Query } from "react-apollo"
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import 'react-confirm-alert/src/react-confirm-alert.css'
@@ -169,148 +169,139 @@ const getNavBarItems = (t, user) => {
 
 const now = new Date()
 
-class SiteWrapper extends React.Component<Props, State> {
-  state = {}  
+function SiteWrapper({t, match, history, children}) {
+  const { error, loading, data, fetchMore } = useQuery(GET_USER)
 
-  render(): React.Node {
-    return (
-      <Query query={GET_USER} >
-        {({ loading, error, data }) => {
-          if (loading) return <p>{this.props.t('general.loading_with_dots')}</p>;
-          if (error) return <p>{this.props.t('system.user.error_loading')}</p>; 
-          
-          console.log('user data in site wrapper')
-          console.log(data)
-      
-          return <Site.Wrapper
-            headerProps={{
-                href: "/",
-                alt: "Costasiella",
-                imageURL: "/d/static/logos/stock/logo_stock_backend.svg", // Set logo url here
-                navItems: (
-                  <Nav.Item type="div" className="d-none d-md-flex">
-                    <Link to="/settings">
-                      <Button
-                        className="mr-2"
-                        icon="settings"
-                        outline
-                        size="sm"
-                        color="primary"
-                      >
-                        {this.props.t('general.settings')}
-                      </Button>
-                    </Link>
-                    <Link to="/user/welcome">
-                      <Button
-                        className="mr-2"
-                        icon="link"
-                        outline
-                        size="sm"
-                        color="primary"
-                      >
-                        {this.props.t('goto.title')}
-                      </Button>
-                    </Link>
-                  </Nav.Item>
-                ),
-                // notificationsTray: {
-                //   notificationsObjects,
-                //   markAllAsRead: () =>
-                //     this.setState(
-                //       () => ({
-                //         notificationsObjects: this.state.notificationsObjects.map(
-                //           v => ({ ...v, unread: false })
-                //         ),
-                //       }),
-                //       () =>
-                //         setTimeout(
-                //           () =>
-                //             this.setState({
-                //               notificationsObjects: this.state.notificationsObjects.map(
-                //                 v => ({ ...v, unread: true })
-                //               ),
-                //             }),
-                //           5000
-                //         )
-                //     ),
-                //   unread: unreadCount,
-                // },
-                accountDropdown: {
-                avatarURL: "#",
-                name: data.user.firstName + ' ' + data.user.lastName,
-                description: "",
-                options: [
-                  // { icon: "user", value: "Profile" },
-                  { icon: "lock", value: "Change password", to: "/#/user/password/change/" },
-                  { isDivider: true },
-                  { icon: "log-out", value: "Sign out", to: "/#/user/logout/" },
-                ],
-              },
-              }}
-              // navProps={{ itemsObjects: navBarItems }}
-              navProps={{ itemsObjects: getNavBarItems(this.props.t, data.user) }}
-              routerContextComponentType={withRouter(RouterContextProvider)}
-              footerProps={{
-                // links: [
-                //   <a href="#">First Link</a>,
-                //   <a href="#">Second Link</a>,
-                //   <a href="#">Third Link</a>,
-                //   <a href="#">Fourth Link</a>,
-                //   <a href="#">Five Link</a>,
-                //   <a href="#">Sixth Link</a>,
-                //   <a href="#">Seventh Link</a>,
-                //   <a href="#">Eigth Link</a>,
-                // ],
-                // note:
-                //   "Premium and Open Source dashboard template with responsive and high quality UI. For Free!",
-                copyright: (
-                  <React.Fragment>
-                    Copyleft © {now.getFullYear()}.
-                    <a
-                      href="https://www.costasiella.com"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      {" "}
-                      Edwin van de Ven
-                    </a>{". "}
-                    All rights reserved.
-                  </React.Fragment>
-                ),
-                nav: (
-                  <React.Fragment>
-                    <Grid.Col auto={true}>
-                      {/* <List className="list-inline list-inline-dots mb-0">
-                        <List.Item className="list-inline-item">
-                          <a href="./docs/index.html">Documentation</a>
-                        </List.Item>
-                        <List.Item className="list-inline-item">
-                          <a href="./faq.html">FAQ</a>
-                        </List.Item>
-                      </List> */}
-                    </Grid.Col>
-                    <Grid.Col auto={true}>
-                      {/* <Button
-                        href="https://github.com/tabler/tabler-react"
-                        size="sm"
-                        outline
-                        color="primary"
-                        RootComponent="a"
-                      >
-                        Source code
-                      </Button> */}
-                    </Grid.Col>
-                  </React.Fragment>
-                ),
-              }}
-            >
-              {this.props.children}
-              <ToastContainer autoClose={5000}/>
-            </Site.Wrapper>
-          }}
-        </Query>
-    );
-  }
+  if (loading) return <p>{t('general.loading_with_dots')}</p>;
+  if (error) return <p>{t('system.user.error_loading')}</p>; 
+
+  return (
+    <Site.Wrapper
+      headerProps={{
+          href: "/",
+          alt: "Costasiella",
+          imageURL: "/d/static/logos/stock/logo_stock_backend.svg", // Set logo url here
+          navItems: (
+            <Nav.Item type="div" className="d-none d-md-flex">
+              <Link to="/settings">
+                <Button
+                  className="mr-2"
+                  icon="settings"
+                  outline
+                  size="sm"
+                  color="primary"
+                >
+                  {t('general.settings')}
+                </Button>
+              </Link>
+              <Link to="/user/welcome">
+                <Button
+                  className="mr-2"
+                  icon="link"
+                  outline
+                  size="sm"
+                  color="primary"
+                >
+                  {t('goto.title')}
+                </Button>
+              </Link>
+            </Nav.Item>
+          ),
+          // notificationsTray: {
+          //   notificationsObjects,
+          //   markAllAsRead: () =>
+          //     this.setState(
+          //       () => ({
+          //         notificationsObjects: this.state.notificationsObjects.map(
+          //           v => ({ ...v, unread: false })
+          //         ),
+          //       }),
+          //       () =>
+          //         setTimeout(
+          //           () =>
+          //             this.setState({
+          //               notificationsObjects: this.state.notificationsObjects.map(
+          //                 v => ({ ...v, unread: true })
+          //               ),
+          //             }),
+          //           5000
+          //         )
+          //     ),
+          //   unread: unreadCount,
+          // },
+          accountDropdown: {
+          avatarURL: "#",
+          name: data.user.firstName + ' ' + data.user.lastName,
+          description: "",
+          options: [
+            // { icon: "user", value: "Profile" },
+            { icon: "lock", value: "Change password", to: "/#/user/password/change/" },
+            { isDivider: true },
+            { icon: "log-out", value: "Sign out", to: "/#/user/logout/" },
+          ],
+        },
+        }}
+        // navProps={{ itemsObjects: navBarItems }}
+        navProps={{ itemsObjects: getNavBarItems(t, data.user) }}
+        routerContextComponentType={withRouter(RouterContextProvider)}
+        footerProps={{
+          // links: [
+          //   <a href="#">First Link</a>,
+          //   <a href="#">Second Link</a>,
+          //   <a href="#">Third Link</a>,
+          //   <a href="#">Fourth Link</a>,
+          //   <a href="#">Five Link</a>,
+          //   <a href="#">Sixth Link</a>,
+          //   <a href="#">Seventh Link</a>,
+          //   <a href="#">Eigth Link</a>,
+          // ],
+          // note:
+          //   "Premium and Open Source dashboard template with responsive and high quality UI. For Free!",
+          copyright: (
+            <React.Fragment>
+              Copyleft © {now.getFullYear()}.
+              <a
+                href="https://www.costasiella.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {" "}
+                Edwin van de Ven
+              </a>{". "}
+              All rights reserved.
+            </React.Fragment>
+          ),
+          nav: (
+            <React.Fragment>
+              <Grid.Col auto={true}>
+                {/* <List className="list-inline list-inline-dots mb-0">
+                  <List.Item className="list-inline-item">
+                    <a href="./docs/index.html">Documentation</a>
+                  </List.Item>
+                  <List.Item className="list-inline-item">
+                    <a href="./faq.html">FAQ</a>
+                  </List.Item>
+                </List> */}
+              </Grid.Col>
+              <Grid.Col auto={true}>
+                {/* <Button
+                  href="https://github.com/tabler/tabler-react"
+                  size="sm"
+                  outline
+                  color="primary"
+                  RootComponent="a"
+                >
+                  Source code
+                </Button> */}
+              </Grid.Col>
+            </React.Fragment>
+          ),
+        }}
+      >
+        {children}
+        <ToastContainer autoClose={5000}/>
+      </Site.Wrapper>
+  )
 }
 
 export default withTranslation()(SiteWrapper)
