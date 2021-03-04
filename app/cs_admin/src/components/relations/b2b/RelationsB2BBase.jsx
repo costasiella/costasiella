4ffba89c2214a85b361f3fdbde46ba1ebda2ac24
Edwin van de Ven <edwin@simplycode.nl>
2021-03-04 19:16:39 +0100

    Add RelationsB2BBase component

---
+++ app/cs_admin/src/components/relations/b2b/RelationsB2BBase.jsx
@@ -0,0 +1,55 @@
+
+import React from 'react'
+import { withTranslation } from 'react-i18next'
+import { withRouter } from "react-router"
+
+import {
+    Page,
+    Grid,
+    Icon,
+    Button,
+    Container,
+  } from "tabler-react";
+
+import CSLS from "../../../tools/cs_local_storage"
+import InputSearch from "../../general/InputSearch"
+import RelationsMenu from "../RelationsMenu"
+import { get_list_query_variables } from "./tools"
+import HasPermissionWrapper from "../../HasPermissionWrapper"
+
+
+const RelationsB2BBase = ({t, history, refetch, children }) => (
+    <Container>
+      <Page.Header title={t("relations.title")}>
+        <div className="page-options d-flex">
+          <InputSearch 
+            initialValueKey={CSLS.RELATIONS_BUSINESSES_SEARCH}
+            placeholder="Search..."
+            onChange={(value) => {
+              console.log(value)
+              localStorage.setItem(CSLS.RELATIONS_BUSINESSES_SEARCH, value)
+              refetch(get_list_query_variables())
+            }}
+          />
+        </div>
+      </Page.Header>
+      <Grid.Row>
+        <Grid.Col md={9}>
+          {children}
+        </Grid.Col>
+        <Grid.Col md={3}>
+          <HasPermissionWrapper permission="add"
+                                resource="business">
+            <Button color="primary btn-block mb-6"
+                    onClick={() => history.push("/relations/business/add")}>
+              <Icon prefix="fe" name="plus-circle" /> {t('relations.business.add')}
+            </Button>
+          </HasPermissionWrapper>
+          <RelationsMenu active_link='b2b'/>
+        </Grid.Col>
+      </Grid.Row>
+    </Container>
+  )
+
+
+  export default withTranslation()(withRouter(RelationsB2BBase))
