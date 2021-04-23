5adae1a69810a1af0d8cb1dee5e8692f17200534
Edwin van de Ven <edwin@simplycode.nl>
2021-04-23 16:04:39 +0200

    Add AutomationAccountSubscriptionInvoices component

---
+++ app/cs_admin/src/components/automation/account/subscription/invoices/AutomationAccountSubscriptionInvoices.jsx
@@ -0,0 +1,164 @@
+// @flow
+
+import React, { useContext } from 'react'
+import { Query, Mutation, useQuery } from "react-apollo"
+import gql from "graphql-tag"
+import { v4 } from "uuid"
+import { withTranslation } from 'react-i18next'
+import { withRouter } from "react-router"
+import { Link } from "react-router-dom"
+import moment from 'moment'
+import AppSettingsContext from '../../../../context/AppSettingsContext'
+
+
+import {
+  Page,
+  Grid,
+  Icon,
+  Dimmer,
+  Badge,
+  Button,
+  Card,
+  Container,
+  Table
+} from "tabler-react";
+import SiteWrapper from "../../../../SiteWrapper"
+import HasPermissionWrapper from "../../../../HasPermissionWrapper"
+// import { confirmAlert } from 'react-confirm-alert'; // Import
+import { toast } from 'react-toastify'
+
+import ContentCard from "../../../../general/ContentCard"
+
+import { GET_TASK_RESULT_QUERY } from "../../../queries"
+import AutomationAccountSubscriptionInvoicesBase from './AutomationAccountSubscriptionInvoicesBase'
+import AutomationTaskResultStatus from "../../../AutomationTaskResultStatus"
+
+// const ARCHIVE_LEVEL = gql`h
+//   mutation ArchiveOrganizationLevel($input: ArchiveOrganizationLevelInput!) {
+//     archiveOrganizationLevel(input: $input) {
+//       organizationLevel {
+//         id
+//         archived
+//       }
+//     }
+//   }
+// `
+
+function AutomationAccountSubscriptionInvoices({t, history, match}) {
+  const appSettings = useContext(AppSettingsContext)
+  const dateTimeFormatMoment = appSettings.dateTimeFormatMoment
+
+  const { error, loading, data, fetchMore } = useQuery(GET_TASK_RESULT_QUERY, {
+    variables: {
+      taskName: "costasiella.tasks.account.subscription.invoices.tasks.account_subscription_invoices_add_for_month"
+    }
+  })
+
+  const headerOptions = <Card.Options>
+    <Link to={"/automation/account/subscriptions/invoices/add"}>
+      <Button color="primary" 
+              size="sm"
+      >
+      {t('general.new_task')}
+      </Button>
+    </Link>
+  </Card.Options>
+
+
+  // Loading
+  if (loading) return (
+    <AutomationAccountSubscriptionInvoicesBase>
+      <p>{t('general.loading_with_dots')}</p>
+    </AutomationAccountSubscriptionInvoicesBase>
+  )
+  // Error
+  if (error) {
+    console.log(error)
+    return (
+      <AutomationAccountSubscriptionInvoicesBase>
+        <p>{t('general.error_sad_smiley')}</p>
+      </AutomationAccountSubscriptionInvoicesBase>
+    )
+  }
+
+  console.log("Automation invoices data:")
+  console.log(data)
+  const taskResults = data.djangoCeleryResultTaskResults
+  // const account = data.account
+  // const scheduleItemAttendances = data.scheduleItemAttendances
+  
+
+  return (
+    <AutomationAccountSubscriptionInvoicesBase>
+      <ContentCard 
+        cardTitle={t('automation.account.subscriptions.invoices.title_card')}
+        pageInfo={taskResults.pageInfo}
+        headerContent={headerOptions}
+        onLoadMore={() => {
+          fetchMore({
+            variables: {
+              after: taskResults.pageInfo.endCursor
+            },
+            updateQuery: (previousResult, { fetchMoreResult }) => {
+              const newEdges = fetchMoreResult.djangoCeleryResultTaskResults.edges
+              const pageInfo = fetchMoreResult.djangoCeleryResultTaskResults.pageInfo
+
+              return newEdges.length
+                ? {
+                    // Put the new invoices at the end of the list and update `pageInfo`
+                    // so we have the new `endCursor` and `hasNextPage` values
+                    djangoCeleryResultTaskResults: {
+                      __typename: previousResult.djangoCeleryResultTaskResults.__typename,
+                      edges: [ ...previousResult.djangoCeleryResultTaskResults.edges, ...newEdges ],
+                      pageInfo
+                    }
+                  }
+                : previousResult
+              }
+            })
+          }} 
+        >
+          { (!taskResults.edges.length) ? 
+            // Empty list
+            <p>{t('automation.account.subscriptions.invoices.empty_list')}</p>
+            :
+            // Content
+            <Table>
+              <Table.Header>
+                <Table.Row key={v4()}>
+                  <Table.ColHeader>{t('automation.general.status.title')}</Table.ColHeader>
+                  <Table.ColHeader>{t('automation.general.time_completed')}</Table.ColHeader>
+                  <Table.ColHeader>{t('automation.general.task_kwargs')}</Table.ColHeader>
+                  <Table.ColHeader>{t('automation.general.task_result')}</Table.ColHeader>
+                  <Table.ColHeader></Table.ColHeader>
+                </Table.Row>
+              </Table.Header>
+              <Table.Body>
+                {taskResults.edges.map(({ node }) => (
+                  <Table.Row key={v4()}>
+                    <Table.Col>
+                      <AutomationTaskResultStatus status={node.status} />
+                    </Table.Col>
+                    <Table.Col>
+                      {moment(node.dateDone).format(dateTimeFormatMoment)}
+                    </Table.Col>
+                    <Table.Col>
+                      {node.taskKwargs}
+                    </Table.Col>
+                    <Table.Col>
+                      {node.result}
+                    </Table.Col>
+                    <Table.Col>
+
+                    </Table.Col>
+                  </Table.Row>
+                ))}
+              </Table.Body>
+            </Table>
+          }
+        </ContentCard>
+    </AutomationAccountSubscriptionInvoicesBase>
+  )
+}
+
+export default withTranslation()(withRouter(AutomationAccountSubscriptionInvoices))
