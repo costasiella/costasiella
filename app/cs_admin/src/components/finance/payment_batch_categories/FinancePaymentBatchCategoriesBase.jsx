b78150f1d628167da7a4f5ccedd502c96f271e02
Edwin van de Ven <edwin@simplycode.nl>
2021-05-09 13:10:18 +0200

    Add add & back button switch to base component

--- app/cs_admin/src/components/finance/payment_batch_categories/FinancePaymentBatchCategoriesBase.jsx
+++ app/cs_admin/src/components/finance/payment_batch_categories/FinancePaymentBatchCategoriesBase.jsx
@@ -3,7 +3,7 @@
 import React from 'react'
 import { withTranslation } from 'react-i18next'
 import { withRouter } from "react-router"
-
+import { Link } from "react-router-dom"
 
 import {
   Page,
@@ -18,7 +18,7 @@ import HasPermissionWrapper from "../../HasPermissionWrapper"
 import FinanceMenu from "../FinanceMenu"
 
 
-function FinancePaymentBatchCategoriesBase({t, history, children}) {
+function FinancePaymentBatchCategoriesBase({t, history, children, showAdd=false, showBack=false}) {
   return (
     <SiteWrapper>
       <div className="my-3 my-md-5">
@@ -29,13 +29,28 @@ function FinancePaymentBatchCategoriesBase({t, history, children}) {
               {children}
             </Grid.Col>
             <Grid.Col md={3}>
-              <HasPermissionWrapper permission="add"
-                                    resource="financepaymentbatchcategory">
-                <Button color="primary btn-block mb-6"
-                        onClick={() => history.push("/finance/paymentbatchcategories/add")}>
-                  <Icon prefix="fe" name="plus-circle" /> {t('finance.payment_batch_categories.add')}
-                </Button>
-              </HasPermissionWrapper>
+              {(showAdd) ?
+                <HasPermissionWrapper permission="add"
+                                      resource="financepaymentbatchcategory">
+                  <Link to={"/finance/paymentbatchcategories/add"}>
+                    <Button color="primary btn-block mb-6">
+                      <Icon prefix="fe" name="plus-circle" /> {t('finance.payment_batch_categories.add')}
+                    </Button>
+                  </Link>
+                </HasPermissionWrapper>
+                : "" 
+              }
+              {(showBack) ?
+                <HasPermissionWrapper permission="view"
+                                      resource="financepaymentbatchcategory">
+                  <Link to="/finance/paymentbatchcategories">
+                    <Button color="primary btn-block mb-6">
+                      <Icon prefix="fe" name="chevrons-left" /> {t('general.back')}
+                    </Button>
+                  </Link>
+                </HasPermissionWrapper>
+                : "" 
+              }
               <FinanceMenu active_link='payment_batch_categories'/>
             </Grid.Col>
           </Grid.Row>
