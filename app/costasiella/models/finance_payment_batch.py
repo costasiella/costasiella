357511fe6d0f63755ebdfe0632ddc8de050ed69d
Edwin van de Ven <edwin@simplycode.nl>
2021-05-13 18:08:54 +0200

    Add finance_payment_batch model

---
+++ app/costasiella/models/finance_payment_batch.py
@@ -0,0 +1,41 @@
+from django.utils.translation import gettext as _
+from django.utils import timezone
+from django.db import models
+
+from ..modules.encrypted_fields import EncryptedTextField
+from .organization_location import OrganizationLocation
+from .finance_payment_batch_category import FinancePaymentBatchCategory
+
+from .helpers import model_string
+
+
+class FinancePaymentBatch(models.Model):
+    BATCH_TYPES = (
+        ('DRAFT', _("Draft")),
+        ('SENT', _("Sent")),
+        ('PAID', _("Paid")),
+        ('CANCELLED', _("Cancelled"))
+    )
+
+    STATUSES = (
+        ('SENT_TO_BANK', _("Sent to Bank")),
+        ('APPROVED', _("Approved")),
+        ('AWAITING_APPROVAL', _("Awaiting approval")),
+        ('REJECTED', _("Rejected")),
+    )
+
+    batch_type = models.CharField(max_length=255, choices=BATCH_TYPES)
+    finance_payment_batch_category = models.ForeignKey(FinancePaymentBatchCategory, null=True, on_delete=models.CASCADE)
+    status = models.CharField(max_length=255, choices=STATUSES, default="AWAITING_APPROVAL")
+    description = models.CharField(max_length=255, null=False, default="")  # default bankstatement description
+    year = models.IntegerField()
+    month = models.IntegerField()
+    execution_date = models.DateField()
+    include_zero_amounts = models.BooleanField(default=False)
+    organization_location = models.ForeignKey(OrganizationLocation, on_delete=models.CASCADE)
+    note = models.TextField(default="")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return model_string(self)
