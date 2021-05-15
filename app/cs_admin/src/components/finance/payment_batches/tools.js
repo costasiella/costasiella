4ebabeeca20b89b831d3de3f18aa7edc885f1ede
Edwin van de Ven <edwin@simplycode.nl>
2021-05-15 14:29:35 +0200

    Add category batch option

--- app/cs_admin/src/components/finance/payment_batches/tools.js
+++ app/cs_admin/src/components/finance/payment_batches/tools.js
@@ -22,10 +22,16 @@ export function get_add_options_collection(t, batchType) {
   return (
     <List unstyled>
       <List.Item>
-        <Link to={`/finance/paymentbatches/${batchType}/add`}>
+        <Link to={`/finance/paymentbatches/${batchType}/add/invoices`}>
           <b>{t("finance.payment_batch_collections.invoices_batch")} <Icon name="chevron-right" /></b>
         </Link><br />
         <p>{t("finance.payment_batch_collections.invoices_batch_description")}</p>
+      </List.Item>
+      <List.Item>
+        <Link to={`/finance/paymentbatches/${batchType}/add/category`}>
+          <b>{t("finance.payment_batch_collections.category_batch")} <Icon name="chevron-right" /></b>
+        </Link><br />
+        <p>{t("finance.payment_batch_collections.category_batch_description")}</p>
       </List.Item>
     </List>
   )
