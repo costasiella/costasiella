9a34b3e34518cba15c70ca65b1e95003ae2b336a
Edwin van de Ven <edwin@simplycode.nl>
2021-05-15 13:47:46 +0200

    Add FinancePaymentBatchAddWhat component

---
+++ app/cs_admin/src/components/finance/payment_batches/FinancePaymentBatchAddWhat.jsx
@@ -0,0 +1,55 @@
+// @flow
+
+import React from 'react'
+import { useQuery, useMutation } from "react-apollo"
+import { v4 } from "uuid"
+import { withTranslation } from 'react-i18next'
+import { withRouter } from "react-router"
+import { Link } from 'react-router-dom'
+
+
+import {
+  Page,
+  Grid,
+  Icon,
+  Dimmer,
+  Badge,
+  Button,
+  Card,
+  Container,
+  Table
+} from "tabler-react";
+import SiteWrapper from "../../SiteWrapper"
+import HasPermissionWrapper from "../../HasPermissionWrapper"
+// import { confirmAlert } from 'react-confirm-alert'; // Import
+import { toast } from 'react-toastify'
+
+import FinancePaymentBatchCategory from "../../ui/FinancePaymentBatchCategory"
+import ContentCard from "../../general/ContentCard"
+import FinancePaymentBatchesBase from "./FinancePaymentBatchesBase"
+
+
+function FinancePaymentBatchAddWhat({t, history, match }) {
+  const batchType = match.params.batch_type
+  const returnUrl = `/finance/paymentbatches/${batchType}`
+
+  let cardTitle
+  let options
+  if (batchType == "collection") {
+    cardTitle = t('finance.payment_batch_collections.title_add_what')
+  } else {
+    cardTitle = t('finance.payment_batch_payments.title_add_what')
+  }
+
+  return (
+    <FinancePaymentBatchesBase showBack={true} returnUrl={returnUrl}>
+      <Card cardTitle={cardTitle}>
+        <Card.Body>
+          Hello World
+        </Card.Body>
+      </Card>
+    </FinancePaymentBatchesBase>
+  )
+}
+
+export default withTranslation()(withRouter(FinancePaymentBatchAddWhat))
