a0f40af44a51075fd21bfc7f00f61995b8225700
Edwin van de Ven <edwin@simplycode.nl>
2021-05-15 14:24:42 +0200

    Load options in AddWhat component

--- app/cs_admin/src/components/finance/payment_batches/FinancePaymentBatchAddWhat.jsx
+++ app/cs_admin/src/components/finance/payment_batches/FinancePaymentBatchAddWhat.jsx
@@ -28,14 +28,18 @@ import FinancePaymentBatchCategory from "../../ui/FinancePaymentBatchCategory"
 import ContentCard from "../../general/ContentCard"
 import FinancePaymentBatchesBase from "./FinancePaymentBatchesBase"
 
+import { get_add_options_collection } from './tools'
+
 
 function FinancePaymentBatchAddWhat({t, history, match }) {
   const batchType = match.params.batch_type
   const returnUrl = `/finance/paymentbatches/${batchType}`
 
   let cardTitle
+  let options
   if (batchType == "collection") {
     cardTitle = t('finance.payment_batch_collections.title_add_what')
+    options = get_add_options_collection(t, batchType)
   } else {
     cardTitle = t('finance.payment_batch_payments.title_add_what')
   }
@@ -47,7 +51,7 @@ function FinancePaymentBatchAddWhat({t, history, match }) {
           <Card.Title>{cardTitle}</Card.Title>
         </Card.Header>
         <Card.Body>
-          Hello World
+          {options}
         </Card.Body>
       </Card>
     </FinancePaymentBatchesBase>
