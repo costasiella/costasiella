f1f7fe28ebe5cb86176aafc4e84f8cf1f6d0dc71
Edwin van de Ven <edwin@simplycode.nl>
2021-05-20 13:24:03 +0200

    Add account bank account queries

---
+++ app/cs_admin/src/components/relations/accounts/bank_accounts/queries.js
@@ -0,0 +1,65 @@
+import gql from "graphql-tag"
+
+
+export const GET_ACCOUNT_BANK_ACCOUNTS_QUERY = gql`
+  query AccountBankAccounts($after: String, $before: String, $account: ID!) {
+    accountBankAccounts(
+      first: 1, 
+      before: $before, 
+      after: $after, 
+      account: $account
+    ) {
+      pageInfo {
+        hasNextPage
+        hasPreviousPage
+        startCursor
+        endCursor
+      }
+      edges {
+        node {
+          id
+          number
+          holder
+          bic
+        }
+      }
+    }
+  }
+`
+
+// export const GET_ACCOUNT_QUERY = gql`
+//   query Account($id: ID!) {
+//     account(id:$id) {
+//       id
+//       customer
+//       teacher
+//       employee
+//       firstName
+//       lastName
+//       email
+//       dateOfBirth
+//       gender
+//       address
+//       postcode
+//       city
+//       country
+//       phone
+//       mobile
+//       emergency
+//       isActive
+//     }
+//   }
+// `
+
+export const UPDATE_ACCOUNT_BANK_ACCOUNT = gql`
+  mutation UpdateAccountBanKAccount($input:UpdateAccountBankAccountInput!) {
+    updateAccountBankAccount(input: $input) {
+      accountBankAccount {
+        id
+        number
+        holder
+        bic
+      }
+    }
+  }
+`
