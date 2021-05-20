f89c55d0ea244a4f553495c31d47734a55eb2ff8
Edwin van de Ven <edwin@simplycode.nl>
2021-05-20 14:09:37 +0200

    Use encrypted fields for bank account model

--- app/costasiella/models/account_bank_account.py
+++ app/costasiella/models/account_bank_account.py
@@ -3,13 +3,14 @@ from django.db import models
 from .account import Account
 
 from .helpers import model_string
+from ..modules.encrypted_fields import EncryptedTextField
 
 
 class AccountBankAccount(models.Model):
     account = models.ForeignKey(Account, on_delete=models.CASCADE, related_name="bank_accounts")
-    number = models.CharField(max_length=255, default="")
-    holder = models.CharField(max_length=255, default="")
-    bic = models.CharField(max_length=255, default="")
+    number = EncryptedTextField(default="")
+    holder = EncryptedTextField(default="")
+    bic = EncryptedTextField(default="")
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     updated_at = models.DateTimeField(auto_now=True, editable=False)
 
