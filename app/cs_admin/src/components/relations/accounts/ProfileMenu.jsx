2f89a4f04b3f02276d113556f48bdd710faf621e
Edwin van de Ven <edwin@simplycode.nl>
2021-05-20 11:50:04 +0200

    Add bank accounts to profile menu

--- app/cs_admin/src/components/relations/accounts/ProfileMenu.jsx
+++ app/cs_admin/src/components/relations/accounts/ProfileMenu.jsx
@@ -22,6 +22,7 @@ let tickets_active
 let teacher_profile_active
 let orders_active
 let invoices_active
+let bank_account_active
 let accepted_documents_active
 
 const ProfileMenu = ({ t, account_id, active_link }) => (
@@ -50,6 +51,7 @@ const ProfileMenu = ({ t, account_id, active_link }) => (
             {(active_link === 'teacher_profile') ? teacher_profile_active = true: teacher_profile_active = false}
             {(active_link === 'orderss') ? orders_active = true: orders_active = false}
             {(active_link === 'invoices') ? invoices_active = true: invoices_active = false}
+            {(active_link === 'bank_account') ? bank_account_active = true: bank_account_active = false}
             {(active_link === 'accepted_documents') ? accepted_documents_active = true: accepted_documents_active = false}
             
 
@@ -151,6 +153,19 @@ const ProfileMenu = ({ t, account_id, active_link }) => (
                     active={invoices_active}
                     >
                 {t('relations.account.invoices.title')}
+                </List.GroupItem>
+            </HasPermissionWrapper>
+            <HasPermissionWrapper 
+                permission="view"
+                resource="accountbankaccount">
+                <List.GroupItem
+                    key={v4()}
+                    className="d-flex align-items-center"
+                    to={"#/relations/accounts/" + account_id + "/bank_accounts"}
+                    icon="briefcase"
+                    active={bank_account_active}
+                    >
+                {t('relations.account.bank_accounts.title')}
                 </List.GroupItem>
             </HasPermissionWrapper>
             { (account.teacher) ?
