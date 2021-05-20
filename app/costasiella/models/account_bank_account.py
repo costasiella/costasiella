eb7e1a9ef7e543e2a0ff93bbbc8d08a7df6fd5f7
Edwin van de Ven <edwin@simplycode.nl>
2021-05-20 11:44:20 +0200

    Add default value for account_bank_account.bic field

--- app/costasiella/models/account_bank_account.py
+++ app/costasiella/models/account_bank_account.py
@@ -9,7 +9,7 @@ class AccountBankAccount(models.Model):
     account = models.ForeignKey(Account, on_delete=models.CASCADE, related_name="bank_accounts")
     number = models.CharField(max_length=255)
     holder = models.CharField(max_length=255)
-    bic = models.CharField(max_length=255)
+    bic = models.CharField(max_length=255, default="")
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     updated_at = models.DateTimeField(auto_now=True, editable=False)
 
