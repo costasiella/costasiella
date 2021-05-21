d47f37b83984633cff654c9df579b2a20c6ee633
Edwin van de Ven <edwin@simplycode.nl>
2021-05-21 20:23:36 +0200

    Add mandate query

--- app/cs_admin/src/components/relations/accounts/bank_accounts/mandates/queries.js
+++ app/cs_admin/src/components/relations/accounts/bank_accounts/mandates/queries.js
@@ -1,6 +1,18 @@
 import gql from "graphql-tag"
 
 
+export const GET_ACCOUNT_BANK_ACCOUNT_MANDATE_QUERY = gql`
+  query AccountBankAccountMandate($id: ID!) {
+    accountBankAccountMandate(id:$id) {
+      id
+      reference
+      content
+      signatureDate
+    }
+  }
+`
+
+
 export const CREATE_ACCOUNT_BANK_ACCOUNT_MANDATE = gql`
   mutation CreateAccountBankAccountMandate($input:CreateAccountBankAccountMandateInput!) {
     createAccountBankAccountMandate(input: $input) {
