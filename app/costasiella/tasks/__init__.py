74be34157a0410ae71b69918dd35c32f6955d04c
Edwin van de Ven <edwin@simplycode.nl>
2021-05-22 12:38:51 +0200

    Allow finance payment batch task imports

--- app/costasiella/tasks/__init__.py
+++ app/costasiella/tasks/__init__.py
@@ -1,4 +1,7 @@
 from .account.subscription.credits.tasks import account_subscription_credits_add_for_month
 from .account.subscription.invoices.tasks import account_subscription_invoices_add_for_month
 from .account.subscription.invoices.tasks import account_subscription_invoices_add_for_month_mollie_collection
+
+from .finance.payment_batch.tasks import finance_payment_batch_generate_items_invoices
+
 from .demo import add
