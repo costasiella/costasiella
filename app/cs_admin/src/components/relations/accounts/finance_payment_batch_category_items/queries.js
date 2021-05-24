d066d46108bb12bb5cfed52bd1388e7313dd52d0
Edwin van de Ven <edwin@simplycode.nl>
2021-05-24 14:08:05 +0200

    Add payment batch category items list ui queries

---
+++ app/cs_admin/src/components/relations/accounts/finance_payment_batch_category_items/queries.js
@@ -0,0 +1,105 @@
+import gql from "graphql-tag"
+
+export const GET_ACCOUNT_FINANCE_PAYMENT_BATCH_CATEGORY_ITEMS_QUERY = gql`
+  query AccountFinancePaymentBatchCategoryItems($after: String, $before: String, $accountId: ID!) {
+    accountFinancePaymentBatchCategoryItems(first: 15, before: $before, after: $after, account: $accountId) {
+      pageInfo {
+        startCursor
+        endCursor
+        hasNextPage
+        hasPreviousPage
+      }
+      edges {
+        node {
+          id
+          financePaymentBatchCategory {
+            id
+            name
+          }
+          year
+          month
+          amountDisplay
+          description
+        }
+      }
+    }
+    account(id:$accountId) {
+      id
+      firstName
+      lastName
+      email
+      phone
+      mobile
+      isActive
+    }
+  }
+`
+
+export const GET_ACCOUNT_CLASSPASS_QUERY = gql`
+  query AccountClasspass($id: ID!, $accountId: ID!, $after: String, $before: String, $archived: Boolean!) {
+    accountClasspass(id:$id) {
+      id
+      organizationClasspass {
+        id
+        name
+      }
+      dateStart
+      dateEnd
+      note
+      createdAt
+    }
+    organizationClasspasses(first: 100, before: $before, after: $after, archived: $archived) {
+      pageInfo {
+        startCursor
+        endCursor
+        hasNextPage
+        hasPreviousPage
+      }
+      edges {
+        node {
+          id
+          archived
+          name
+        }
+      }
+    }
+    account(id:$accountId) {
+      id
+      firstName
+      lastName
+      email
+      phone
+      mobile
+      isActive
+    }
+  }
+`
+
+export const GET_INPUT_VALUES_QUERY = gql`
+  query ClasspassInputValues($after: String, $before: String, $archived: Boolean!, $accountId: ID!) {
+    organizationClasspasses(first: 100, before: $before, after: $after, archived: $archived) {
+      pageInfo {
+        startCursor
+        endCursor
+        hasNextPage
+        hasPreviousPage
+      }
+      edges {
+        node {
+          id
+          archived
+          name
+        }
+      }
+    }
+    account(id:$accountId) {
+      id
+      firstName
+      lastName
+      email
+      phone
+      mobile
+      isActive
+    }
+  }
+`
