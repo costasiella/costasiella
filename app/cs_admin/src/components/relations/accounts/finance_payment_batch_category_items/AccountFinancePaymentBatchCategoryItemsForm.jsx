19c80eb7bef8575f2569ac7128a2b15b70798e0f
Edwin van de Ven <edwin@simplycode.nl>
2021-05-27 18:40:37 +0200

    Add account batch category item form component

---
+++ app/cs_admin/src/components/relations/accounts/finance_payment_batch_category_items/AccountFinancePaymentBatchCategoryItemsForm.jsx
@@ -0,0 +1,103 @@
+// @flow
+
+import React from 'react'
+import { withTranslation } from 'react-i18next'
+import { withRouter } from "react-router"
+import { Form as FoForm, Field, ErrorMessage } from 'formik'
+import { v4 } from "uuid"
+
+import { Editor } from '@tinymce/tinymce-react'
+import { tinymceBasicConf } from "../../../../plugin_config/tinymce"
+import CSDatePicker from "../../../ui/CSDatePicker"
+
+
+import {
+  Button,
+  Card,
+  Form,
+  Grid,
+} from "tabler-react";
+
+
+const AccountFinancePaymentBatchCategoryItemsForm = ({ t, history, inputData, isSubmitting, setFieldValue, setFieldTouched, errors, values, return_url }) => (
+  <FoForm>
+    <Card.Body> 
+      <Grid.Row>
+        <Grid.Col>
+          <Form.Group label={t('general.year')} >
+            <Field type="text" 
+                  name="year" 
+                  className={(errors.year) ? "form-control is-invalid" : "form-control"} 
+                  autoComplete="off" />
+            <ErrorMessage name="year" component="span" className="invalid-feedback" />
+          </Form.Group>
+        </Grid.Col>
+        <Grid.Col>
+          <Form.Group label={t('general.month')} >
+            <Field type="text" 
+                  name="month" 
+                  className={(errors.month) ? "form-control is-invalid" : "form-control"} 
+                  autoComplete="off" />
+            <ErrorMessage name="month" component="span" className="invalid-feedback" />
+          </Form.Group>
+        </Grid.Col>
+      </Grid.Row>
+      <Grid.Row>
+        <Grid.Col>
+          <Form.Group label={t('general.amount')} >
+            <Field type="text" 
+                  name="amount" 
+                  className={(errors.amount) ? "form-control is-invalid" : "form-control"} 
+                  autoComplete="off" />
+            <ErrorMessage name="amount" component="span" className="invalid-feedback" />
+          </Form.Group>
+        </Grid.Col>
+        <Grid.Col>
+          <Form.Group label={t('general.category')}>
+            <Field component="select" 
+                  name="financePaymentBatchCategory" 
+                  className={(errors.financePaymentBatchCategory) ? "form-control is-invalid" : "form-control"} 
+                  autoComplete="off">
+              <option value="" key={v4()}>{t('general.please_select')}</option>
+              {inputData.financePaymentBatchCategory.edges.map(({ node }) =>
+                <option value={node.id} key={v4()}>{node.name}</option>
+              )}
+            </Field>
+            <ErrorMessage name="financePaymentBatchCategory" component="span" className="invalid-feedback" />
+          </Form.Group>
+        </Grid.Col>
+      </Grid.Row> 
+      <Grid.Row>
+        <Grid.Col>
+          <Form.Group label={t('general.description')} >
+            <Field type="text" 
+                  name="description" 
+                  className={(errors.description) ? "form-control is-invalid" : "form-control"} 
+                  autoComplete="off" />
+            <ErrorMessage name="description" component="span" className="invalid-feedback" />
+          </Form.Group>
+        </Grid.Col>
+      </Grid.Row>
+    </Card.Body>
+    <Card.Footer>
+        <Button 
+          className="pull-right"
+          color="primary"
+          disabled={isSubmitting}
+          type="submit"
+        >
+          {t('general.submit')}
+        </Button>
+        <Button
+          type="button" 
+          color="link" 
+          onClick={() => history.push(return_url)}
+        >
+          {t('general.cancel')}
+        </Button>
+    </Card.Footer>
+  </FoForm>
+)
+
+
+export default withTranslation()(withRouter(AccountFinancePaymentBatchCategoryItemsForm))
