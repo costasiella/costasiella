02a03696f477989b7f7782e36568df46ac86448a
Edwin van de Ven <edwin@simplycode.nl>
2021-06-01 14:34:41 +0200

    Fix capitalization in a comment

--- app/app/celery.py
+++ app/app/celery.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 
-# set the default Django settings module for the 'celery' program.
+# Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings.development')
 
 celery_app = Celery('app')
