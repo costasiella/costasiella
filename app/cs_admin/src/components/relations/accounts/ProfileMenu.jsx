8a7263f29372b60a7ccd07bec16b5f7ec8c96835
Edwin van de Ven <edwin@simplycode.nl>
2021-06-01 16:49:28 +0200

    Add notes to customer menu

--- app/cs_admin/src/components/relations/accounts/ProfileMenu.jsx
+++ app/cs_admin/src/components/relations/accounts/ProfileMenu.jsx
@@ -23,6 +23,7 @@ let teacher_profile_active
 let orders_active
 let invoices_active
 let bank_account_active
+let notes_active
 let finance_payment_batch_category_item_active
 let accepted_documents_active
 
@@ -53,6 +54,7 @@ const ProfileMenu = ({ t, account_id, active_link }) => (
             {(active_link === 'orderss') ? orders_active = true: orders_active = false}
             {(active_link === 'invoices') ? invoices_active = true: invoices_active = false}
             {(active_link === 'bank_account') ? bank_account_active = true: bank_account_active = false}
+            {(active_link === 'notes') ? notes_active = true: notes_active = false}
             {(active_link === 'finance_payment_batch_category_item') ? 
                 finance_payment_batch_category_item_active = true : 
                 finance_payment_batch_category_item_active = false }
@@ -170,6 +172,19 @@ const ProfileMenu = ({ t, account_id, active_link }) => (
                     active={bank_account_active}
                     >
                 {t('relations.account.bank_accounts.title')}
+                </List.GroupItem>
+            </HasPermissionWrapper>
+            <HasPermissionWrapper 
+                permission="view"
+                resource="accountnote">
+                <List.GroupItem
+                    key={v4()}
+                    className="d-flex align-items-center"
+                    to={"#/relations/accounts/" + account_id + "/notes"}
+                    icon="message-square"
+                    active={notes_active}
+                    >
+                {t('relations.account.notes.title')}
                 </List.GroupItem>
             </HasPermissionWrapper>
             <HasPermissionWrapper 
