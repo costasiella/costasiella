97fbf570823e102ca703f93130073fe97759fb20
Edwin van de Ven <edwin@simplycode.nl>
2021-06-01 22:30:54 +0200

    Add earlybird discounts to event edit menu

--- app/cs_admin/src/components/schedule/events/edit/ScheduleEventEditMenu.jsx
+++ app/cs_admin/src/components/schedule/events/edit/ScheduleEventEditMenu.jsx
@@ -12,6 +12,7 @@ import HasPermissionWrapper from "../../../HasPermissionWrapper"
 
 let general_active
 let tickets_active
+let earlybirds_active
 let activities_active
 let media_active
 
@@ -19,6 +20,7 @@ const ScheduleEventMenu = ({ t, eventId, active_link }) => (
   <List.Group transparent={true}>
     {(active_link === 'general') ? general_active = true: general_active = false}
     {(active_link === 'tickets') ? tickets_active = true: tickets_active = false}
+    {(active_link === 'earlybirds') ? earlybirds_active = true: earlybirds_active = false}
     {(active_link === 'activities') ? activities_active = true: activities_active = false}
     {(active_link === 'media') ? media_active = true: media_active = false}
     
@@ -49,6 +51,20 @@ const ScheduleEventMenu = ({ t, eventId, active_link }) => (
           active={tickets_active}
           >
           {t('schedule.events.tickets.title')}
+      </List.GroupItem>
+    </HasPermissionWrapper>
+    <HasPermissionWrapper 
+        resource="scheduleeventearlybird"
+        permission="view" 
+    >
+      <List.GroupItem
+          key={v4()}
+          className="d-flex align-items-center"
+          to={`#/schedule/events/edit/${eventId}/earlybirds`}
+          icon="clock"
+          active={earlybirds_active}
+          >
+          {t('schedule.events.earlybirds.title')}
       </List.GroupItem>
     </HasPermissionWrapper>
     <HasPermissionWrapper 
