19967c02ea31960e423827f4cca7b76ea6a15683
Edwin van de Ven <edwin@simplycode.nl>
2021-06-01 22:36:15 +0200

    Export add & update earlybird queries

--- app/cs_admin/src/components/schedule/events/earlybirds/queries.js
+++ app/cs_admin/src/components/schedule/events/earlybirds/queries.js
@@ -38,7 +38,7 @@ export const GET_SCHEDULE_EVENT_EARLYBIRD_QUERY = gql`
 `
 
 
-const ADD_SCHEDULE_EVENT_EARLYBIRD = gql`
+export const ADD_SCHEDULE_EVENT_EARLYBIRD = gql`
   mutation CreateScheduleEventEarlybird($input:CreateScheduleEventEarlybirdInput!) {
     createScheduleEventEarlybird(input: $input) {
       scheduleEventEarlybird {
@@ -49,7 +49,7 @@ const ADD_SCHEDULE_EVENT_EARLYBIRD = gql`
 `
 
 
-const UPDATE_SCHEDULE_EVENT_EARLYBIRD = gql`
+export const UPDATE_SCHEDULE_EVENT_EARLYBIRD = gql`
   mutation UpdateScheduleEventEarlybird($input:UpdateScheduleEventEarlybirdInput!) {
     updateScheduleEventEarlybird(input: $input) {
       scheduleEventEarlybird {
