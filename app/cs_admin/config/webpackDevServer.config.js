5bf497e429cd89c27277d81c3a725b377d125c92
Edwin van de Ven <edwin@simplycode.nl>
2021-06-05 01:36:32 +0200

    Updates react-dev-utils & react-scripts
    Modified webbackDevServer.config.js and... it seems to be running again

--- app/cs_admin/config/webpackDevServer.config.js
+++ app/cs_admin/config/webpackDevServer.config.js
@@ -102,7 +102,7 @@ module.exports = function(proxy, allowedHost) {
       // We do this in development to avoid hitting the production cache if
       // it used the same host and port.
       // https://github.com/facebook/create-react-app/issues/2272#issuecomment-302832432
-      app.use(noopServiceWorkerMiddleware());
+      app.use(noopServiceWorkerMiddleware(''));
     },
     // Allow CORS requests coming from port 8000 (Django web dev server)
     headers: {
