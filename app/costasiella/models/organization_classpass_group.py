294990a79f49645bc1d7045c55257cdf7eb3bffe
Edwin van de Ven <edwin@simplycode.nl>
2021-06-14 10:58:18 +0200

    Remove archived field from organization classpass group model

--- app/costasiella/models/organization_classpass_group.py
+++ app/costasiella/models/organization_classpass_group.py
@@ -2,11 +2,13 @@ from django.db import models
 
 from .organization_classpass import OrganizationClasspass
 
+
 class OrganizationClasspassGroup(models.Model):
-    archived = models.BooleanField(default=False)
     name = models.CharField(max_length=255)
-    organization_classpasses = models.ManyToManyField(OrganizationClasspass, through='OrganizationClasspassGroupClasspass', related_name='classpasses')
+    description = models.TextField(default="")
+    organization_classpasses = models.ManyToManyField(OrganizationClasspass,
+                                                      through='OrganizationClasspassGroupClasspass',
+                                                      related_name='classpasses')
 
     def __str__(self):
         return self.name
-    
--- app/costasiella/models/organization_classpass_group_classpass.py
+++ app/costasiella/models/organization_classpass_group_classpass.py
@@ -3,10 +3,11 @@ from django.db import models
 from .organization_classpass import OrganizationClasspass
 from .organization_classpass_group import OrganizationClasspassGroup
 
+
 class OrganizationClasspassGroupClasspass(models.Model):
     organization_classpass_group = models.ForeignKey(OrganizationClasspassGroup, on_delete=models.CASCADE)
     organization_classpass = models.ForeignKey(OrganizationClasspass, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.organization_classpass_group.name + ' ' + self.organization_classpass.name
-    
+
