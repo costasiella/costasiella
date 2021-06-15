f0e3bd212e55b81126db85c2604a5308f3c3c35c
Edwin van de Ven <edwin@simplycode.nl>
2021-06-15 13:56:33 +0200

    Add OrganizationAnnouncementsBase component

---
+++ app/cs_admin/src/components/organization/announcements/OrganizationAnnouncementsBase.jsx
@@ -0,0 +1,50 @@
+// @flow
+
+import React from 'react'
+import { withTranslation } from 'react-i18next'
+import { withRouter } from "react-router"
+import { Link } from "react-router-dom"
+
+
+import {
+  Page,
+  Grid,
+  Icon,
+  Button,
+  Container,
+} from "tabler-react";
+import SiteWrapper from "../../SiteWrapper"
+import HasPermissionWrapper from "../../HasPermissionWrapper"
+import OrganizationMenu from "../OrganizationMenu"
+
+
+function OrganizationAnnouncementsBase({t, history}) {
+  return (
+    <SiteWrapper>
+    <div className="my-3 my-md-5">
+      <Container>
+        <Page.Header title={t("organization.title")} />
+        <Grid.Row>
+          <Grid.Col md={9}>
+            {chilren}
+          </Grid.Col>
+          <Grid.Col md={3}>
+            <HasPermissionWrapper permission="add"
+                                  resource="organizationlevel">
+              <Link to="/organization/announcements/add">
+                <Button color="primary btn-block mb-6">
+                  <Icon prefix="fe" name="plus-circle" /> {t('organization.announcements.add')}
+                </Button>
+              </Link>
+            </HasPermissionWrapper>
+            <OrganizationMenu active_link='announcements'/>
+          </Grid.Col>
+        </Grid.Row>
+      </Container>
+    </div>
+  </SiteWrapper>
+  )
+}
+
+
+export default withTranslation()(withRouter(OrganizationAnnouncementsBase))
