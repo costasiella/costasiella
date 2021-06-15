18e7af124c53ca56593c60e754e636b7e9b93cab
Edwin van de Ven <edwin@simplycode.nl>
2021-06-15 13:28:35 +0200

    Add announcement queries

---
+++ app/cs_admin/src/components/organization/announcements/queries.js
@@ -0,0 +1,43 @@
+import gql from "graphql-tag"
+
+export const GET_ANNOUNCEMENTS_QUERY = gql`
+  query OrganizationAnnouncements($after: String, $before: String) {
+    organizationLevels(first: 15, before: $before, after: $after) {
+      pageInfo {
+        startCursor
+        endCursor
+        hasNextPage
+        hasPreviousPage
+      }
+      edges {
+        node {
+          id
+          displayPublic
+          displayShop
+          displayBackend
+          name
+          content
+          dateStart
+          dateEnd
+          priority
+        }
+      }
+    }
+  }
+`
+
+export const GET_ANNOUNCEMENT_QUERY = gql`
+  query OrganizationAnnouncement($id: ID!) {
+    organizationAnnouncement(id:$id) {
+      id
+      displayPublic
+      displayShop
+      displayBackend
+      name
+      content
+      dateStart
+      dateEnd
+      priority
+    }
+  }
+`
