42ca90f2a7be55631afe092168d584c6066c73ac
Edwin van de Ven <edwin@simplycode.nl>
2021-06-17 11:40:36 +0200

    Set port for gh build

--- app/app/settings/common.py
+++ app/app/settings/common.py
@@ -122,7 +122,7 @@ if 'GITHUB_WORKFLOW' in os.environ:
             'USER': 'root',
             'PASSWORD': '',
             'HOST': '127.0.0.1',
-            # 'PORT': 3306
+            'PORT': 3306
         }
     }
 else:
