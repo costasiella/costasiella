f428cea4c989c7a04bb0f55207bed3e257a3e6e8
Edwin van de Ven <edwin@simplycode.nl>
2021-08-30 15:50:59 +0200

    Test organization discovery and language fields in account model

--- app/costasiella/tests/factories.py
+++ app/costasiella/tests/factories.py
@@ -409,7 +409,8 @@ class RegularUserFactory(factory.DjangoModelFactory):
     first_name = 'user'
     last_name = 'regular user'
     password = factory.PostGenerationMethodCall('set_password', 'CSUser1#')
-
+    organization_discovery = factory.SubFactory(OrganizationDiscoveryFactory)
+    organization_language = factory.SubFactory(OrganizationLanguageFactory)
     is_active = True
 
 
