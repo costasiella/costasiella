06a53ddbb69d53f996c6af056a625e80714249cc
Edwin van de Ven <edwin@simplycode.nl>
2021-09-01 16:44:34 +0200

    Set static root for production config

--- app/app/settings/production.py
+++ app/app/settings/production.py
@@ -1,9 +1,11 @@
 """
-Django settings for development
+Django settings for production deployment
 """
-
 from app.settings.common import *
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+
+# Static files config
+STATIC_ROOT = "/opt/static"
