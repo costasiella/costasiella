b9831db575ad04092de51706d7614f829b99dfd1
Edwin van de Ven <edwin@simplycode.nl>
2021-10-14 17:21:43 +0200

    Fix mail dude imports

--- app/costasiella/dudes/mail_dude.py
+++ app/costasiella/dudes/mail_dude.py
@@ -1,7 +1,7 @@
 import html2text
 
 from django.utils.translation import gettext as _
-from django.core.mail import EmailMessage
+from django.core.mail import send_mail
 
 # https://docs.djangoproject.com/en/2.2/topics/email/
 
