5da8ffab198401a02775b51b058a589a0487f990
Edwin van de Ven <edwin@simplycode.nl>
2021-11-07 13:17:28 +0100

    Update development & production settings for orphaned file cleanup

--- app/app/settings/production.py
+++ app/app/settings/production.py
@@ -1,6 +1,7 @@
 """
 Django settings for production deployment
 """
+from os import path
 from app.settings.common import *
 
 
@@ -16,6 +17,17 @@ DEFAULT_FROM_EMAIL = 'My Name <my_from_email@domain.com>'
 # Static files config
 STATIC_ROOT = "/opt/static"
 MEDIA_ROOT = "/opt/media"
+
+# Orphaned files cleanup
+ORPHANED_APPS_MEDIABASE_DIRS = {
+    'costasiella': {
+        'root': MEDIA_ROOT,  # MEDIA_ROOT => default location(s) of your uploaded items e.g. /var/www/mediabase
+        'skip': (               # optional iterable of subfolders to preserve, e.g. sorl.thumbnail cache
+            path.join(MEDIA_ROOT, 'cache'),
+        ),
+        'exclude': ('.gitignore',) # optional iterable of files to preserve
+    }
+}
 
 # Point to redis container instead of localhost for Django-defender & celery
 DEFENDER_REDIS_URL = 'redis://redis:6379/0'
