beb3dcb8e1de9876386d7f5cabc1414db1e879be
Edwin van de Ven <edwin@simplycode.nl>
2022-01-11 16:01:29 +0100

    Remove account for schedule item enrollment model

--- app/costasiella/models/schedule_item_enrollment.py
+++ app/costasiella/models/schedule_item_enrollment.py
@@ -16,7 +16,6 @@ from .helpers import model_string
 
 # Create your models here
 class ScheduleItemEnrollment(models.Model):
-    account = models.ForeignKey(Account, on_delete=models.CASCADE)
     schedule_item = models.ForeignKey(ScheduleItem, on_delete=models.CASCADE)
     account_subscription = models.ForeignKey(AccountSubscription, on_delete=models.CASCADE)
     date_start = models.DateField()
