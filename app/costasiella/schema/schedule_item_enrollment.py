93f91f638bcd06057b9bc12a6f73477b438bec47
Edwin van de Ven <edwin@simplycode.nl>
2022-01-11 12:41:06 +0100

    schedule_item_enrollment schema mutations

--- app/costasiella/schema/schedule_item_enrollment.py
+++ app/costasiella/schema/schedule_item_enrollment.py
@@ -109,14 +109,9 @@ class CreateScheduleItemEnrollment(graphene.relay.ClientIDMutation):
     class Input:
         account = graphene.ID(required=False)
         schedule_item = graphene.ID(required=True)
-        account_classpass = graphene.ID(required=False)
-        account_subscription = graphene.ID(required=False)
-        organization_classpass = graphene.ID(required=False)
-        finance_invoice_item = graphene.ID(required=False)
-        enrollment_type = graphene.String(required=True)
-        date = graphene.types.datetime.Date(required=True)
-        online_booking = graphene.Boolean(required=False, default_value=False)
-        booking_status = graphene.String(required=False, default_value="BOOKED")
+        account_subscription = graphene.ID(required=True)
+        date_start = graphene.types.datetime.Date(required=True)
+        date_end = graphene.types.datetime.Date(required=False)
 
     schedule_item_enrollment = graphene.Field(ScheduleItemEnrollmentNode)
 
@@ -125,78 +120,23 @@ class CreateScheduleItemEnrollment(graphene.relay.ClientIDMutation):
         user = info.context.user
         require_login(user)
 
-        permission = user.has_perm('costasiella.add_scheduleitemenrollment') or \
-            user.has_perm('costasiella.view_selfcheckin')
-
+        permission = user.has_perm('costasiella.add_scheduleitemenrollment')
         validation_result = validate_schedule_item_enrollment_create_update_input(input)
         if not permission or 'account' not in input:
             # When the user doesn't have permissions; always use their own account
             validation_result['account'] = user
 
-        class_checkin_dude = ClassCheckinDude()
-        class_schedule_dude = ClassScheduleDude()
-
-        class_takes_place = class_schedule_dude.schedule_item_takes_place_on_day(
+        schedule_item_enrollment = ScheduleItemEnrollment(
+            account=validation_result['account'],
             schedule_item=validation_result['schedule_item'],
-            date=input['date']
+            account_subscription=validation_result['account_subscription'],
+            date_start=input['date_start']
         )
-        
-        if not class_takes_place:
-            raise Exception(
-                _("This class doesn't take place on this date, please check for the correct date or any holidays.")
-            )
 
-        enrollment_type = input['enrollment_type']
-        if enrollment_type == "CLASSPASS":
-            if not validation_result['account_classpass']:
-                raise Exception(_('accountClasspass field is mandatory when doing a class pass check-in'))
+        if 'date_end' in input:
+            schedule_item_enrollment.date_end = input['date_end']
 
-            account_classpass = validation_result['account_classpass']
-            schedule_item_enrollment = class_checkin_dude.class_checkin_classpass(
-                account=validation_result['account'],
-                account_classpass=account_classpass,
-                schedule_item=validation_result['schedule_item'],
-                date=input['date'],
-                booking_status=input['booking_status'],
-                online_booking=input['online_booking'],
-            )
-
-            account_classpass.update_classes_remaining()
-
-        elif enrollment_type == "SUBSCRIPTION":
-            if not validation_result['account_subscription']:
-                raise Exception(_('accountSubscription field is mandatory when doing a subscription check-in'))
-
-            print("SUBSCRIPTION checkin")
-
-            account_subscription = validation_result['account_subscription']
-            schedule_item_enrollment = class_checkin_dude.class_checkin_subscription(
-                account=validation_result['account'],
-                account_subscription=account_subscription,
-                schedule_item=validation_result['schedule_item'],
-                date=input['date'],
-                booking_status=input['booking_status'],
-                online_booking=input['online_booking'],
-            )
-
-        elif enrollment_type == "CLASSPASS_BUY_AND_BOOK":
-            if not validation_result['organization_classpass']:
-                raise Exception(_('organizationClasspass field is mandatory when doing a classpass buy and check-in'))
-
-            organization_classpass = validation_result['organization_classpass']
-            result = class_checkin_dude.sell_classpass_and_class_checkin(
-                account = validation_result['account'],
-                organization_classpass = organization_classpass,
-                schedule_item = validation_result['schedule_item'],
-                date = input['date'],
-                booking_status = input['booking_status'],
-                online_booking = input['online_booking'],                    
-            )
-
-            schedule_item_enrollment = result['schedule_item_enrollment']
-            account_classpass = result['account_classpass']
-
-            account_classpass.update_classes_remaining()
+        schedule_item_enrollment.save()
 
         return CreateScheduleItemEnrollment(schedule_item_enrollment=schedule_item_enrollment)
 
@@ -204,48 +144,28 @@ class CreateScheduleItemEnrollment(graphene.relay.ClientIDMutation):
 class UpdateScheduleItemEnrollment(graphene.relay.ClientIDMutation):
     class Input:
         id = graphene.ID(required=True)
-        booking_status = graphene.String(required=False, default_value="BOOKED")
+        date_start = graphene.types.datetime.Date(required=False)
+        date_end = graphene.types.datetime.Date(required=False)
         
     schedule_item_enrollment = graphene.Field(ScheduleItemEnrollmentNode)
 
     @classmethod
     def mutate_and_get_payload(self, root, info, **input):
         user = info.context.user
-        require_login_and_one_of_permissions(user, [
-            'costasiella.change_scheduleitemenrollment',
-            'costasiella.view_selfcheckin'
-        ])
+        require_login_and_permissions(user, 'costasiella.change_scheduleitemenrollment')
 
         rid = get_rid(input['id'])
         schedule_item_enrollment = ScheduleItemEnrollment.objects.filter(id=rid.id).first()
         if not schedule_item_enrollment:
             raise Exception('Invalid Schedule Item Enrollment ID!')
 
-        validation_result = validate_schedule_item_enrollment_create_update_input(input)
-        
-        if 'booking_status' in input:
-            schedule_item_enrollment.booking_status = input['booking_status']
+        if 'date_start' in input:
+            schedule_item_enrollment.date_start = input['date_start']
+
+        if 'date_end' in input:
+            schedule_item_enrollment.date_end = input['date_end']
 
         schedule_item_enrollment.save()
-
-        # Update classpass classes remaining
-        if schedule_item_enrollment.account_classpass:
-             schedule_item_enrollment.account_classpass.update_classes_remaining()
-
-        # Refund subscription credit (remove it)
-        if schedule_item_enrollment.account_subscription:
-            if input['booking_status'] == 'CANCELLED':
-                AccountSubscriptionCredit.objects.filter(
-                    schedule_item_enrollment=schedule_item_enrollment,
-                ).delete()
-            if input['booking_status'] == 'BOOKED' or input['booking_status'] == 'ATTENDING':
-                qs = AccountSubscriptionCredit.objects.filter(
-                    schedule_item_enrollment=schedule_item_enrollment
-                )
-                if not qs.exists():
-                    from ..dudes import ClassCheckinDude
-                    class_checkin_dude = ClassCheckinDude()
-                    class_checkin_dude.class_checkin_subscription_subtract_credit(schedule_item_enrollment)
 
         return UpdateScheduleItemEnrollment(schedule_item_enrollment=schedule_item_enrollment)
 
@@ -259,26 +179,15 @@ class DeleteScheduleItemEnrollment(graphene.relay.ClientIDMutation):
     @classmethod
     def mutate_and_get_payload(self, root, info, **input):
         user = info.context.user
-        require_login_and_one_of_permissions(user, [
-            'costasiella.delete_scheduleitemenrollment',
-            'costasiella.view_selfcheckin'
-        ])
+        require_login_and_permissions(user, 'costasiella.change_scheduleitemenrollment')
 
         rid = get_rid(input['id'])
         schedule_item_enrollment = ScheduleItemEnrollment.objects.filter(id=rid.id).first()
         if not schedule_item_enrollment:
             raise Exception('Invalid Schedule Item Enrollment ID!')
 
-        # Get linked class pass if any
-        account_classpass = None
-        if schedule_item_enrollment.account_classpass:
-             account_classpass = schedule_item_enrollment.account_classpass
-
         # Actually remove
         ok = schedule_item_enrollment.delete()
-
-        if account_classpass:
-            account_classpass.update_classes_remaining()
 
         return DeleteScheduleItemEnrollment(ok=ok)
 
