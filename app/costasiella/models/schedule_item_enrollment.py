2ea411d360b7b8e1de151b8529f83723467e7cd9
Edwin van de Ven <edwin@simplycode.nl>
2022-01-11 12:23:25 +0100

    Add ScheduleItemEnrollment model

---
+++ app/costasiella/models/schedule_item_enrollment.py
@@ -0,0 +1,28 @@
+from django.utils.translation import gettext as _
+
+import datetime
+import pytz
+
+from django.db import models
+
+from .schedule_item import ScheduleItem
+from .account import Account
+from .account_classpass import AccountClasspass
+from .account_subscription import AccountSubscription
+from .account_schedule_event_ticket import AccountScheduleEventTicket
+from .finance_invoice_item import FinanceInvoiceItem
+
+from .helpers import model_string
+
+# Create your models here
+class ScheduleItemEnrollment(models.Model):
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    schedule_item = models.ForeignKey(ScheduleItem, on_delete=models.CASCADE)
+    account_subscription = models.ForeignKey(AccountSubscription, on_delete=models.CASCADE)
+    date_start = models.DateField()
+    date_end = models.DateField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return model_string(self)
