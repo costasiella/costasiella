f6c3abeee3707b6412330b458953e6ce756d3bf5
Edwin van de Ven <edwin@simplycode.nl>
2022-03-26 21:37:08 +0100

    Add insight revenue subscriptions dude & schema

--- app/costasiella/dudes/insight_revenue_dude.py
+++ app/costasiella/dudes/insight_revenue_dude.py
@@ -7,9 +7,10 @@ from django.db.models import Q, Sum
 
 
 class InsightRevenueDude:
-    def get_revenue_total_period(self, date_from, date_until):
+    @staticmethod
+    def get_revenue_total_period(date_from, date_until):
         """
-        Return count of sold (new) subscriptions in a period
+        Return sums of revenue in the given period
         """
         from ..models import FinanceInvoice
 
@@ -17,7 +18,6 @@ class InsightRevenueDude:
             ~(Q(status='CANCELLED') | Q(status='DRAFT')),
             date_sent__gte=date_from,
             date_sent__lte=date_until,
-
         ).aggregate(Sum('total'), Sum('subtotal'), Sum('tax'))
 
         return sums
@@ -60,6 +60,84 @@ class InsightRevenueDude:
             date_until = datetime.date(year, i, last_day_month)
 
             tax_in_month = self.get_revenue_total_period(date_from, date_until).get('tax__sum')
+            data[i] = tax_in_month or Decimal(0)
+
+        return data
+
+    @staticmethod
+    def get_revenue_in_category_period(date_from, date_until, category):
+        """
+        Return sums of revenue in the given period
+        """
+        from ..models import FinanceInvoiceItem
+
+        categories = [
+            'SUBSCRIPTIONS',
+            'CLASSPASSES',
+            'EVENTTICKETS',
+            'OTHER'
+        ]
+        if category not in categories:
+            raise Exception(_("Category not one of SUBSCRIPTIONS, CLASSPASSES, EVENTTICKETS or OTHER"))
+
+        sums = FinanceInvoiceItem.objects.filter(
+            ~(Q(finance_invoice__status='CANCELLED') | Q(finance_invoice__status='DRAFT')),
+            finance_invoice__date_sent__gte=date_from,
+            finance_invoice__date_sent__lte=date_until,
+        )
+
+        if category == 'SUBSCRIPTIONS':
+            sums.filter(account_subscription__isnull=False)
+        #TODO: Add the other categories here
+
+        sums.aggregate(Sum('total'), Sum('subtotal'), Sum('tax'))
+
+        return sums
+
+    def get_revenue_total_in_category_for_year(self, year, category):
+        from collections import OrderedDict
+
+        data = OrderedDict()
+        for i in range(1, 13):
+            date_from = datetime.date(year, i, 1)
+            last_day_month = calendar.monthrange(year, i)[1]
+            date_until = datetime.date(year, i, last_day_month)
+
+            revenue_in_month = self.get_revenue_in_category_period(
+                date_from, date_until, category
+            ).get('total__sum')
+            data[i] = revenue_in_month or Decimal(0)
+
+        return data
+
+    def get_revenue_subtotal_in_category_for_year(self, year, category):
+        from collections import OrderedDict
+
+        data = OrderedDict()
+        for i in range(1, 13):
+            date_from = datetime.date(year, i, 1)
+            last_day_month = calendar.monthrange(year, i)[1]
+            date_until = datetime.date(year, i, last_day_month)
+
+            revenue_in_month = self.get_revenue_in_category_period(
+                date_from, date_until, category
+            ).get('subtotal__sum')
+            data[i] = revenue_in_month or Decimal(0)
+
+        return data
+
+    def get_revenue_tax_in_category_for_year(self, year, category):
+        from collections import OrderedDict
+
+        data = OrderedDict()
+        for i in range(1, 13):
+            date_from = datetime.date(year, i, 1)
+            last_day_month = calendar.monthrange(year, i)[1]
+            date_until = datetime.date(year, i, last_day_month)
+
+            tax_in_month = self.get_revenue_in_category_period(
+                date_from, date_until, category
+            ).get('tax__sum')
             data[i] = tax_in_month or Decimal(0)
 
         return data
---
+++ app/costasiella/schema/insight_revenue_subscriptions.py
@@ -0,0 +1,115 @@
+from django.utils.translation import gettext as _
+from django.utils import timezone
+import graphene
+
+from ..dudes import InsightRevenueDude
+from ..modules.gql_tools import require_login_and_permission
+
+
+class RevenueTotalSubscriptionsType(graphene.ObjectType):
+    description = graphene.String()
+    data = graphene.List(graphene.Decimal)
+    year = graphene.Int()
+
+    @staticmethod
+    def resolve_description(info):
+        return _("revenue_total_subscriptions")
+
+    def resolve_data(self, info):       
+        insight_revenue_dude = InsightRevenueDude()
+        year = self.year
+        if not year:
+            year = timezone.now().year
+
+        data = insight_revenue_dude.get_revenue_total_in_category_for_year(year, 'SUBSCRIPTIONS')
+        amounts = []
+        for month in data:
+            amounts.append(data[month])
+
+        return amounts
+
+
+class RevenueSubTotalSubscriptionsType(graphene.ObjectType):
+    description = graphene.String()
+    data = graphene.List(graphene.Decimal)
+    year = graphene.Int()
+
+    def resolve_description(self, info):
+        return _("revenue_subtotal")
+
+    def resolve_data(self, info):
+        insight_revenue_dude = InsightRevenueDude()
+        year = self.year
+        if not year:
+            year = timezone.now().year
+
+        data = insight_revenue_dude.get_revenue_total_in_category_for_year(self.year, 'SUBSCRIPTIONS')
+        amounts = []
+        for month in data:
+            amounts.append(data[month])
+
+        return amounts
+
+
+class RevenueTaxSubscriptionsType(graphene.ObjectType):
+    description = graphene.String()
+    data = graphene.List(graphene.Decimal)
+    year = graphene.Int()
+
+    def resolve_description(self, info):
+        return _("revenue_tax")
+
+    def resolve_data(self, info):
+        insight_revenue_dude = InsightRevenueDude()
+        year = self.year
+        if not year:
+            year = timezone.now().year
+
+        data = insight_revenue_dude.get_revenue_tax_in_category_for_year(self.year, 'SUBSCRIPTIONS')
+        amounts = []
+        for month in data:
+            amounts.append(data[month])
+
+        return amounts
+
+
+class InsightRevenueSubscriptionsQuery(graphene.ObjectType):
+    insight_revenue_total_subscriptions = graphene.Field(RevenueTotalSubscriptionsType,
+                                                         year=graphene.Int())
+    insight_revenue_subtotal_subscriptions = graphene.Field(RevenueSubTotalSubscriptionsType,
+                                                            year=graphene.Int())
+    insight_revenue_tax_subscriptions = graphene.Field(RevenueTaxSubscriptionsType,
+                                                       year=graphene.Int())
+
+    @staticmethod
+    def resolve_insight_revenue_total(info,
+                                      year=graphene.Int(required=True, default_value=timezone.now().year)):
+        user = info.context.user
+        require_login_and_permission(user, 'costasiella.view_insightrevenue')
+
+        revenue_total_subscriptions = RevenueTotalSubscriptionsType()
+        revenue_total_subscriptions.year = year
+
+        return revenue_total_subscriptions
+
+    @staticmethod
+    def resolve_insight_revenue_subtotal(info,
+                                         year=graphene.Int(required=True, default_value=timezone.now().year)):
+        user = info.context.user
+        require_login_and_permission(user, 'costasiella.view_insightrevenue')
+
+        revenue_subtotal_subscriptions = RevenueSubTotalSubscriptionsType()
+        revenue_subtotal_subscriptions.year = year
+
+        return revenue_subtotal_subscriptions
+
+    @staticmethod
+    def resolve_insight_revenue_tax(info,
+                                    year=graphene.Int(required=True, default_value=timezone.now().year)):
+        user = info.context.user
+        require_login_and_permission(user, 'costasiella.view_insightrevenue')
+
+        revenue_tax_subscriptions = RevenueTaxSubscriptionsType()
+        revenue_tax_subscriptions.year = year
+
+        return revenue_tax_subscriptions
