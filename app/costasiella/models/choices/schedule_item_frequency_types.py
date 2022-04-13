c479ef5dcb3ddaed28058180ba4d0c92ab7d64a4
Edwin van de Ven <edwin@simplycode.nl>
2022-04-13 22:33:02 +0200

    Add LAST_WEEKDAY_OF_MONTH choice

--- app/costasiella/models/choices/schedule_item_frequency_types.py
+++ app/costasiella/models/choices/schedule_item_frequency_types.py
@@ -4,5 +4,6 @@ from django.utils.translation import gettext as _
 def get_schedule_item_frequency_types():
     return (
         ('SPECIFIC', _("Specific")),
-        ('WEEKLY', _("Weekly"))
+        ('WEEKLY', _("Weekly")),
+        ('LAST_WEEKDAY_OF_MONTH', _("Last weekday of month"))
     )
