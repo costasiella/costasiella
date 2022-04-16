86b48ccac6272ed5a15b1cccdbac4d74b2d54b91
Edwin van de Ven <edwin@simplycode.nl>
2022-04-16 02:14:56 +0200

    Set csrf cookie to secure

--- app/app/settings/production.py
+++ app/app/settings/production.py
@@ -7,6 +7,7 @@ from app.settings.common import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+CSRF_COOKIE_SECURE = True
 
 # Email configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
