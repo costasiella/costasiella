9b713733aa06caf72ea161f7a505467a7b0fa17d
Edwin van de Ven <edwin@simplycode.nl>
2022-04-18 09:11:53 +0200

    Set csrf token in cookie

--- app/costasiella/views/csrf_token.py
+++ app/costasiella/views/csrf_token.py
@@ -1,6 +1,12 @@
 from django.http import JsonResponse
 from django.middleware.csrf import get_token
+from django.views.decorators.csrf import ensure_csrf_cookie
 
+# Return csrf token in JSON data
+# def csrf(request):
+#     return JsonResponse({'csrfToken': get_token(request)})
 
+# Set csrf token in Cookie
+@ensure_csrf_cookie
 def csrf(request):
-    return JsonResponse({'csrfToken': get_token(request)})
+    return JsonResponse({'success': 'CSRF cookie set'})
