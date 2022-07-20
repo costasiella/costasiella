790d95fd4d67609b1a0c0b66c7d497538eaa19bc
Edwin van de Ven <edwin@simplycode.nl>
2022-07-20 23:47:28 +0200

    Add subtotal, tax and total fields to event ticket model
    To have a reliable total when an EX tax rate is set. This total can be used to calculate the ticket price with discounts

--- app/costasiella/models/schedule_event_ticket.py
+++ app/costasiella/models/schedule_event_ticket.py
@@ -20,6 +20,9 @@ class ScheduleEventTicket(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(default="")
     price = models.DecimalField(max_digits=20, decimal_places=2)
+    subtotal = models.DecimalField(max_digits=20, decimal_places=2, default=0)
+    tax = models.DecimalField(max_digits=20, decimal_places=2, default=0)
+    total = models.DecimalField(max_digits=20, decimal_places=2, default=0)
     schedule_items = models.ManyToManyField(
         'ScheduleItem',
         through='ScheduleEventTicketScheduleItem',
@@ -33,6 +36,37 @@ class ScheduleEventTicket(models.Model):
 
     def __str__(self):
         return model_string(self)
+
+    def save(self, *args, **kwargs):
+        self.subtotal = self._calculate_subtotal()
+        self.tax = self._calculate_tax()
+        self.total = self._calculate_total()
+
+        super(ScheduleEventTicket, self).save(*args, **kwargs)
+
+    def _calculate_subtotal(self):
+        # If tax is included in price, first remove it.
+        tax_rate = self.finance_tax_rate
+        price = float(self.price)
+        if tax_rate:
+            if tax_rate.rate_type == "IN":
+                # divide price by 1.tax_percentage and then multiply by quantity
+                percentage = (float(tax_rate.percentage) / 100) + 1
+                price = price / percentage
+
+        return float(price) * float(self.quantity)
+
+    def _calculate_tax(self):
+        tax_rate = self.finance_tax_rate
+        if tax_rate:
+            percentage = (tax_rate.percentage / 100)
+
+            return float(self.subtotal) * float(percentage)
+        else:
+            return 0
+
+    def _calculate_total(self):
+        return self.subtotal + self.tax
 
     def is_sold_out(self):
         """
@@ -99,6 +133,82 @@ class ScheduleEventTicket(models.Model):
             "discount": Decimal(discount.quantize(Decimal('.01'), rounding=ROUND_HALF_UP)),
             "earlybird": earlybird
         }
+
+    def get_highest_subscription_group_discount_on_date_for_account(self, finance_order_item, date):
+        from decimal import Decimal, ROUND_HALF_UP
+        import time
+        from .account_subscription import AccountSubscription
+        from .schedule_event_subscription_group_discount import ScheduleEventSubscriptionGroupDiscount
+
+        print("Hello world")
+        account = finance_order_item.finance_order.account
+
+        discount = Decimal(0)
+        organization_subscription_group = None
+        organization_subscription_groups = self._get_earlybird_qs(date)
+
+        # Get subscriptions for account that are active on given date
+        account_subscriptions = AccountSubscription.objects.filter(
+            Q(account=account),
+            Q(date_start__lte=date),
+            (Q(date_end__gte=date) | Q(date_end__isnull=True))
+        )
+        print(account_subscriptions)
+
+        # For each account subscription, check which group(s) it belongs to
+        highest_subscription_group_discount = None
+        for account_subscription in account_subscriptions:
+            print(account_subscription)
+            organization_subscription = account_subscription.organization_subscription
+            subscription_group_subscriptions = organization_subscription.organizationsubscriptiongroupsubscription_set.all()
+            print(subscription_group_subscriptions)
+            for subscription_group_subscription in subscription_group_subscriptions:
+                print(subscription_group_subscription)
+                # For each subscription group, check if there's a discount
+                subscription_group_discounts = ScheduleEventSubscriptionGroupDiscount.objects.filter(
+                    schedule_event=self.schedule_event,
+                    organization_subscription_group=subscription_group_subscription.organization_subscription_group
+                )
+                print(subscription_group_discounts)
+                for subscription_group_discount in subscription_group_discounts:
+                    if not highest_subscription_group_discount:
+                        highest_subscription_group_discount = subscription_group_discount
+
+                    if (subscription_group_discount.discount_percentage >
+                            highest_subscription_group_discount.discount_percentage):
+                        highest_subscription_group_discount = subscription_group_discount
+
+        # time.sleep(10)
+
+        print("Highest discount:")
+        print(highest_subscription_group_discount)
+
+        if highest_subscription_group_discount:
+            discount_percentage = highest_subscription_group_discount.discount_percentage
+            discount = Decimal(Decimal(finance_order_item.total) * Decimal(discount_percentage / 100))
+
+        return {
+            "discount": Decimal(discount.quantize(Decimal('.01'), rounding=ROUND_HALF_UP)),
+            "subscription_group_discount": highest_subscription_group_discount
+        }
+
+
+
+        # Return the highest found discount
+
+
+        # # Check if discounts exit
+        # if earlybirds.exists():
+        #     # If so, get the first one (with the highest discount)
+        #     earlybird = earlybirds.first()
+        #
+        #     discount_percentage = earlybird.discount_percentage
+        #     discount = Decimal(self.price * Decimal(discount_percentage / 100))
+        #
+        # return {
+        #     "discount": Decimal(discount.quantize(Decimal('.01'), rounding=ROUND_HALF_UP)),
+        #     "earlybird": earlybird
+        # }
 
     def total_price_on_date(self, date):
         # Process earlybird discount
