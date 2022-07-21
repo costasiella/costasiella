2c2a185f03534b474ecca381a6cc55f21fcbb12e
Edwin van de Ven <edwin@simplycode.nl>
2022-07-21 16:52:25 +0200

    Add subscription discounts to orders, if applicable

--- app/costasiella/models/schedule_event_ticket.py
+++ app/costasiella/models/schedule_event_ticket.py
@@ -137,11 +137,8 @@ class ScheduleEventTicket(models.Model):
 
     def get_highest_subscription_group_discount_on_date_for_account(self, account, date):
         from decimal import Decimal, ROUND_HALF_UP
-        import time
         from .account_subscription import AccountSubscription
         from .schedule_event_subscription_group_discount import ScheduleEventSubscriptionGroupDiscount
-
-        print("Hello world")
 
         discount = Decimal(0)
         organization_subscription_group = None
@@ -153,23 +150,18 @@ class ScheduleEventTicket(models.Model):
             Q(date_start__lte=date),
             (Q(date_end__gte=date) | Q(date_end__isnull=True))
         )
-        print(account_subscriptions)
 
         # For each account subscription, check which group(s) it belongs to
         highest_subscription_group_discount = None
         for account_subscription in account_subscriptions:
-            print(account_subscription)
             organization_subscription = account_subscription.organization_subscription
             subscription_group_subscriptions = organization_subscription.organizationsubscriptiongroupsubscription_set.all()
-            print(subscription_group_subscriptions)
             for subscription_group_subscription in subscription_group_subscriptions:
-                print(subscription_group_subscription)
                 # For each subscription group, check if there's a discount
                 subscription_group_discounts = ScheduleEventSubscriptionGroupDiscount.objects.filter(
                     schedule_event=self.schedule_event,
                     organization_subscription_group=subscription_group_subscription.organization_subscription_group
                 )
-                print(subscription_group_discounts)
                 for subscription_group_discount in subscription_group_discounts:
                     if not highest_subscription_group_discount:
                         highest_subscription_group_discount = subscription_group_discount
@@ -177,11 +169,6 @@ class ScheduleEventTicket(models.Model):
                     if (subscription_group_discount.discount_percentage >
                             highest_subscription_group_discount.discount_percentage):
                         highest_subscription_group_discount = subscription_group_discount
-
-        # time.sleep(10)
-
-        print("Highest discount:")
-        print(highest_subscription_group_discount)
 
         if highest_subscription_group_discount:
             discount_percentage = highest_subscription_group_discount.discount_percentage
@@ -191,24 +178,6 @@ class ScheduleEventTicket(models.Model):
             "discount": Decimal(discount.quantize(Decimal('.01'), rounding=ROUND_HALF_UP)),
             "subscription_group_discount": highest_subscription_group_discount
         }
-
-
-
-        # Return the highest found discount
-
-
-        # # Check if discounts exit
-        # if earlybirds.exists():
-        #     # If so, get the first one (with the highest discount)
-        #     earlybird = earlybirds.first()
-        #
-        #     discount_percentage = earlybird.discount_percentage
-        #     discount = Decimal(self.price * Decimal(discount_percentage / 100))
-        #
-        # return {
-        #     "discount": Decimal(discount.quantize(Decimal('.01'), rounding=ROUND_HALF_UP)),
-        #     "earlybird": earlybird
-        # }
 
     def total_price_on_date(self, date):
         # Process earlybird discount
