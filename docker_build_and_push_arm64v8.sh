a2ea1aecc74995e51a53adc929b8696d86beba8e
Edwin van de Ven <edwin@simplycode.nl>
2022-10-24 14:29:01 +0200

    Build containers for docker hub without cache

--- docker_build_and_push_arm64v8.sh
+++ docker_build_and_push_arm64v8.sh
@@ -5,7 +5,7 @@
 export COSTASIELLA_VERSION=$1
 export BUILD_ARCH=arm64v8
 
-docker compose -f docker-compose-build.yml build --build-arg ARCH=$BUILD_ARCH/
+docker compose -f docker-compose-build.yml build --no-cache --build-arg ARCH=$BUILD_ARCH/
 
 docker push simplycode/costasiella_backend:$COSTASIELLA_VERSION-$BUILD_ARCH
 docker push simplycode/costasiella_celery_beat:$COSTASIELLA_VERSION-$BUILD_ARCH
