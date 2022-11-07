23d1855bf37482e6f63ff0dadbb8db6bef7198b7
Edwin van de Ven <edwin@simplycode.nl>
2022-11-07 13:28:53 +0100

    Change GL Account & Cost Center code fields to int

--- app/costasiella/models/finance_costcenter.py
+++ app/costasiella/models/finance_costcenter.py
@@ -2,10 +2,11 @@ from django.utils.translation import gettext as _
 
 from django.db import models
 
+
 class FinanceCostCenter(models.Model):
     archived = models.BooleanField(default=False)
     name = models.CharField(max_length=255)
-    code = models.CharField(max_length=255, default="", help_text=_("Cost center code in your accounting software."))
+    code = models.IntegerField(default=0)
 
     def __str__(self):
         return self.name
--- app/costasiella/models/finance_glaccount.py
+++ app/costasiella/models/finance_glaccount.py
@@ -4,7 +4,7 @@ from django.db import models
 class FinanceGLAccount(models.Model):
     archived = models.BooleanField(default=False)
     name = models.CharField(max_length=255)
-    code = models.CharField(max_length=255, default="")
+    code = models.IntegerField(default=0)
 
     def __str__(self):
         return self.name
