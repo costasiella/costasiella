d21aec5cda3c727d117f4551d03c1f49a472306a
Edwin van de Ven <edwin@simplycode.nl>
2023-07-11 13:01:14 +0200

    Bump version to 2023.03.14

--- app/costasiella/dudes/version_dude.py
+++ app/costasiella/dudes/version_dude.py
@@ -32,7 +32,7 @@ class VersionDude:
     def get_latest_version():
         return {
             "version": "2023.03",
-            "version_patch": "13"
+            "version_patch": "14"
         }
 
     def update_version(self):
