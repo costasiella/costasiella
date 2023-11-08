0679628fd17ff4e4f8831e42f2dc7ba0fb3e6fd0
Edwin van de Ven <edwin@simplycode.nl>
2023-11-08 19:53:57 +0100

    Clean up unused code & import

--- app/costasiella/schema/insight_finance_open_invoices.py
+++ app/costasiella/schema/insight_finance_open_invoices.py
@@ -1,22 +1,10 @@
-import datetime
-from decimal import Decimal
-
-from django.utils.translation import gettext as _
-from django.utils import timezone
-from django.db.models import Q, FilteredRelation, OuterRef, Subquery
-
 import graphene
-from graphene_django import DjangoObjectType
-from graphene_django.filter import DjangoFilterConnectionField
-from graphql import GraphQLError
-from graphql_relay import to_global_id
 
 from ..dudes import InsightFinanceOpenInvoicesDude
-from ..models import ScheduleItem
 from ..modules.finance_tools import display_float_as_amount
 from .finance_invoice import FinanceInvoiceNode
 
-from ..modules.gql_tools import get_rid, require_login_and_permission
+from ..modules.gql_tools import require_login_and_permission
 
 
 class FinanceOpenInvoicesType(graphene.ObjectType):
@@ -29,16 +17,6 @@ class FinanceOpenInvoicesType(graphene.ObjectType):
         insight_finance_open_invoices_dude = InsightFinanceOpenInvoicesDude()
         open_invoices = insight_finance_open_invoices_dude.get_open_invoices_on_date(self.date)
 
-        # data = []
-        # for finance_tax_rate in result:
-        #     data.append(
-        #         FinanceTaxRateSummaryDataType(
-        #             finance_tax_rate=finance_tax_rate,
-        #             subtotal=finance_tax_rate.sum_subtotal or Decimal(0),
-        #             tax=finance_tax_rate.sum_tax or Decimal(0)
-        #         )
-        #     )
-
         return open_invoices
 
     def resolve_total_open_on_date(self, info):
@@ -50,18 +28,6 @@ class FinanceOpenInvoicesType(graphene.ObjectType):
 
     def resolve_total_open_on_date_display(self, info):
         return display_float_as_amount(self.resolve_total_open_on_date(info))
-
-
-# def validate_input(date_start, date_end):
-#     """
-#     Validate input
-#     """
-#     result = {}
-#
-#     if date_end < date_start:
-#         raise Exception(_('dateEnd should be >= dateStart'))
-#
-#     return result
 
 
 class InsightFinanceOpenInvoicesQuery(graphene.ObjectType):
