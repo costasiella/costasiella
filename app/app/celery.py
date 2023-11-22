7e9cb406ad35b40dd4fb26286a1aab867c84871b
Edwin van de Ven <edwin@simplycode.nl>
2023-11-22 12:51:28 +0100

    Bump version to 2023.05.02

--- app/app/celery.py
+++ app/app/celery.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
+# from celery.signals import setup_logging  # noqa
 
 # Set the default Django settings module for the 'celery' program.
 if 'DJANGO_SETTINGS_MODULE' not in os.environ:
@@ -13,6 +14,13 @@ celery_app = Celery('app')
 # - namespace='CELERY' means all celery-related configuration keys
 #   should have a `CELERY_` prefix.
 celery_app.config_from_object('django.conf:settings', namespace='CELERY')
+#
+# @setup_logging.connect
+# def config_loggers(*args, **kwargs):
+#     from logging.config import dictConfig  # noqa
+#     from django.conf import settings  # noqa
+#
+#     dictConfig(settings.LOGGING)
 
 # Load task modules from all registered Django app configs.
 celery_app.autodiscover_tasks()
