249836cd394e62cfc1198dca5e4f2c96f6561982
Edwin van de Ven <edwin@simplycode.nl>
2023-12-06 20:23:59 +0100

    Create management command for importing os invoice item descriptions

---
+++ app/costasiella/management/commands/fix_openstudio_import_invoice_item_descriptions.py
@@ -0,0 +1,221 @@
+import logging
+import sys
+
+import MySQLdb
+from MySQLdb._exceptions import OperationalError
+import MySQLdb.converters
+
+from django.core.management.base import BaseCommand, CommandError, no_translations
+
+from costasiella.models import FinanceInvoiceItem
+
+logger = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.help = 'Import from OpenStudio. Provide at least --db_name, --db_user and --db_password.'
+        self.cursor = None  # Set later on from handle
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--db_name',
+            type=str,
+            required=True,
+            help="OpenStudio database name"
+        )
+        parser.add_argument(
+            '--db_user',
+            type=str,
+            required=True,
+            help="OpenStudio database username"
+        )
+        parser.add_argument(
+            '--db_password',
+            type=str,
+            required=True,
+            help="OpenStudio database password for db_user"
+        )
+        parser.add_argument(
+            '--db_host',
+            type=str,
+            default="localhost",
+            help="OpenStudio database host (default: localhost)"
+        )
+        parser.add_argument(
+            '--db_port',
+            type=int,
+            default=3306,
+            help="OpenStudio database port (default: 3306)"
+        )
+
+    def _yes_or_no(self, question, exit_on_no=True):
+        """
+        A simple yes or no confirmation question
+        :param question: String - question text
+        :return: input asking a y/n question
+        """
+        reply = str(input(question + ' (y/n): ')).lower().strip()
+        if reply[0] == 'y':
+            return True
+        if reply[0] == 'n':
+            if exit_on_no:
+                self.stdout.write("Fix OpenStudio import invoice item descriptions stopped.")
+                sys.exit(1)
+            else:
+                return False
+        else:
+            return self._yes_or_no("Uhhhh... please enter y or n")
+
+    def _confirm_args(self, **options):
+        """
+        Confirm passed options
+        :param options: parser options
+        :return: result of _yes_or_no
+        """
+        self.stdout.write("")
+        self.stdout.write("OpenStudio DB connection info:")
+        self.stdout.write("-----")
+        self.stdout.write("db: %s" % options['db_name'])
+        self.stdout.write("user: %s" % options['db_user'])
+        self.stdout.write("password: %s" % options['db_password'])
+        self.stdout.write("host: %s" % options['db_host'])
+        self.stdout.write("port: %s" % options['db_port'])
+        self.stdout.write("-----")
+        self.stdout.write("")
+
+        return self._yes_or_no("Test connection & start fix using the settings above?")
+
+    def _connect_to_db_and_set_cursor(self, host, user, password, db, port):
+        """
+
+        :return:
+        """
+        # Conversion mapping
+        # https://mysqlclient.readthedocs.io/user_guide.html#some-examples
+        # os_db_conv = {
+        #     FIELD_TYPE.: int,
+        #     FIELD_TYPE.LONG: int,
+        #     FIELD_TYPE.VARCHAR: str,
+        #     FIELD_TYPE.CHAR: str,
+        #     FIELD_TYPE.FLOAT: float,
+        #     FIELD_TYPE.DOUBLE: float,
+        #     # FIELD_TYPE.DATE: datetime.date,
+        #     # FIELD_TYPE.DATETIME: datetime.datetime,
+        # }
+        orig_conv = MySQLdb.converters.conversions
+        conv_iter = iter(orig_conv)
+        convert = dict(zip(conv_iter, [str, ] * len(orig_conv.keys())))
+
+        try:
+            conn = MySQLdb.connect(
+                host=host,
+                user=user,
+                passwd=password,
+                db=db,
+                port=port,
+                charset="latin1",
+                # conv=convert
+            )
+        except OperationalError as e:
+            self.stdout.write("OpenStudio MySQL connection: " + self.style.ERROR("FAILED"))
+            self.stdout.write("The following error message was received:")
+            self.stdout.write(str(e))
+            self.stdout.write("")
+            self.stdout.write("Exiting...")
+            sys.exit(1)
+
+        return conn.cursor(MySQLdb.cursors.DictCursor)
+
+    @no_translations
+    def handle(self, *args, **options):
+        """
+        Main command function (options defined in add_arguments)
+        :param args: command args
+        :param options: command options
+        :return:
+        """
+        options_confirmation = self._confirm_args(**options)
+        if options_confirmation:
+            # Setup db connection
+            self.stdout.write("")
+            self.stdout.write("Testing OpenStudio MySQL connection...")
+            self.cursor = self._connect_to_db_and_set_cursor(
+                host=options['db_host'],
+                user=options['db_user'],
+                password=options['db_password'],
+                db=options['db_name'],
+                port=options['db_port']
+            )
+            self.stdout.write("OpenStudio MySQL connection: " + self.style.SUCCESS("SUCCESS"))
+            self.stdout.write("Starting import...")
+
+        if not self.cursor:
+            self.stdout.write("Error setting up MySQL connection, exiting...")
+            sys.exit(1)
+
+        self._import()
+
+    def get_records_import_status_display(self, records_imported, records_total, raw=False):
+        records_display = "%s/%s" % (records_imported, records_total)
+
+        if not raw:
+            if records_imported == records_total:
+                records_display = self.style.SUCCESS(records_display)
+            else:
+                records_display = self.style.ERROR(records_display)
+
+        return records_display
+
+    def _import(self):
+        """
+        Main import fix function
+        :param self:
+        :return:
+        """
+        # Query invoice items with invoice number
+        query = """
+SELECT 
+ii.id,
+ii.Sorting,
+ii.ProductName,
+ii.Description,
+i.invoiceid
+FROM invoices_items ii 
+LEFT JOIN invoices i ON ii.invoices_id = i.id
+"""
+        self.cursor.execute(query)
+        records = self.cursor.fetchall()
+
+        records_imported = 0
+        for record in records:
+            record = {k.lower(): v for k, v in record.items()}
+
+            # Match by invoice number and line number
+            finance_invoice_item = FinanceInvoiceItem.objects.filter(
+                line_number=record['sorting'],
+                finance_invoice__invoice_number=record['invoiceid']
+            ).first()
+
+            print("----------")
+            print(f"cs description: {finance_invoice_item.description}")
+            print(f"os description: {record['description']}")
+
+            # finance_tax_rate = m.FinanceTaxRate(
+            #     archived=self._web2py_bool_to_python(record['archived']),
+            #     name=record['name'],
+            #     percentage=record['percentage'],
+            #     rate_type="IN",
+            #     code=record['vatcodeid'] or ""
+            # )
+            # finance_tax_rate.save()
+            records_imported += 1
+
+
+        log_message = "Fix invoice item descriptions: "
+        self.stdout.write(log_message + self.get_records_import_status_display(records_imported, len(records)))
+        logger.info(log_message + self.get_records_import_status_display(records_imported, len(records), raw=True))
+
+
+
